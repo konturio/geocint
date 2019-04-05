6491d57da79a79b065ed7d2e141c98605c315c99
Darafei Praliaskouski <me@komzpa.net>
2019-04-05 18:38:03 +0300

    stop at z8

--- scripts/generate_bivariate_class_tiles.sh
+++ scripts/generate_bivariate_class_tiles.sh
@@ -4,13 +4,13 @@ x0=1
 y0=1
 zoom=0
 
-for (( z=$zoom; z<=9; z++ )); do
+for (( z=$zoom; z<=8; z++ )); do
   let "x0 = 2**$z"
   let "y0 = 2**$z"
   for (( x=0; x<$x0; x++ )); do
     mkdir -p data/tiles/osm_quality_bivariate/tiles/$z/$x
     for (( y=0; y<$y0; y++ )); do
-      file="data/tiles/osm_quality_bivariate/tiles/$z/$x/$y.mvt"
+      file="data/tiles/osm_quality_bivariate/$z/$x/$y.mvt"
       echo "psql -q -X -f scripts/bivariate_class_tile.sql -v x=$x -v y=$y -v z=$z | xxd -r -p > $file"
     done
   done

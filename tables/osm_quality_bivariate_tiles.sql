aa2a1488241c821ba03bfddc54dc75d45fc1446e
Darafei Praliaskouski <me@komzpa.net>
2019-04-05 11:14:20 +0300

    clip by z8 grid

--- tables/osm_quality_bivariate_tiles.sql
+++ tables/osm_quality_bivariate_tiles.sql
@@ -1,17 +1,27 @@
+drop table if exists z8_grid;
+create table z8_grid as (
+    select
+        z,
+        x,
+        y,
+        TileBBOX(z, x, y) as geom
+    from
+            (select 8::integer as z) as zoom,
+            generate_series(0, (2 ^ z)::integer) as x,
+            generate_series(0, (2 ^ z)::integer) as y
+);
+
 drop table if exists osm_quality_bivariate_tiles;
 create table osm_quality_bivariate_tiles as (
     select
         z,
         x,
         y,
-        --ST_Intersection(ST_Union(geom), TileBBOX(z, x, y)) as geom, -- slow and unnecessary, will be clipped anyway
-        ST_Simplify(ST_Union(geom), 0) as geom,
+        ST_Intersection(ST_Simplify(ST_Union(q.geom), 0), g.geom) as geom,
         bivariate_class
     from
-            (select 8::integer as z) as zoom,
-            generate_series(0, (2 ^ z)::integer) x,
-            generate_series(0, (2 ^ z)::integer) y,
-            lateral (select * from osm_quality_bivariate_grid_1000 where geom && TileBBOX(z, x, y)) g
-    group by z, x, y, bivariate_class
+        z8_grid g
+            join osm_quality_bivariate_grid_1000 q on g.geom && q.geom and g.geom && q.geom
+    group by z, x, y, bivariate_class, g.geom
 );
-create index on osm_quality_bivariate_tiles using gist(geom);
+create index on osm_quality_bivariate_tiles using gist (geom);

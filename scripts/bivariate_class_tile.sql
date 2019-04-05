6b6833f58ee0992125443c51a57ad7dae07968bb
Darafei Praliaskouski <me@komzpa.net>
2019-04-05 13:04:34 +0300

    match extent

--- scripts/bivariate_class_tile.sql
+++ scripts/bivariate_class_tile.sql
@@ -1,9 +1,8 @@
 COPY (
-  SELECT encode(ST_AsMVT(q, 'bivariate_class', 4096, 'geom'), 'hex')
+  SELECT encode(ST_AsMVT(q, 'bivariate_class', 512, 'geom'), 'hex')
   FROM (
     SELECT bivariate_class, ST_AsMvtGeom(geom, TileBBox(:z, :x, :y), 512, 0, false) AS geom
     FROM osm_quality_bivariate_tiles
     WHERE geom && TileBBox(:z, :x, :y)
-    AND ST_Intersects(geom, TileBBox(:z, :x, :y))
   ) AS q
 ) TO STDOUT;

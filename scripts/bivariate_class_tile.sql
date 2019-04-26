48b435e4295900022292b59687dcdfbdc1062d08
Elena Bondareva <e.vic.bondareva@gmail.com>
2019-04-26 17:04:35 +0300

    Perform bivariate tiles export into mvt directly skipping  osm_quality_bivariate_tiles table generation

--- scripts/bivariate_class_tile.sql
+++ scripts/bivariate_class_tile.sql
@@ -1,8 +1,12 @@
 COPY (
-  SELECT encode(ST_AsMVT(q, 'bivariate_class', 512, 'geom'), 'hex')
-  FROM (
-    SELECT bivariate_class, ST_AsMvtGeom(geom, TileBBox(:z, :x, :y), 512, 0, false) AS geom
-    FROM osm_quality_bivariate_tiles
-    WHERE geom && TileBBox(:z, :x, :y) and z=:z and x=:x and y=:y
-  ) AS q
-) TO STDOUT;
+    SELECT encode(ST_AsMVT(q, 'bivariate_class', 512, 'geom'), 'hex')
+    FROM (
+             SELECT bivariate_class,
+                    ST_AsMvtGeom(ST_Intersection(ST_Simplify(ST_Union(geom), 0), TileBBox(:z, :x, :y)),
+                                 TileBBox(:z, :x, :y), 512, 0, false) AS geom
+             FROM osm_quality_bivariate_grid_1000
+             WHERE zoom = :z
+               AND geom && TileBBox(:z, :x, :y)
+             GROUP BY bivariate_class, geom
+         ) AS q
+    ) TO STDOUT;

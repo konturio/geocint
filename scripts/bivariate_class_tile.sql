0b5778a9a49ae1381a530ada77ef6c5e8473bf26
Geocint GIS User <darafei@kontur.io>
2019-07-07 11:55:03 +0300

    higher resolution MVT

--- scripts/bivariate_class_tile.sql
+++ scripts/bivariate_class_tile.sql
@@ -1,8 +1,8 @@
 COPY (
-    SELECT encode(ST_AsMVT(q, 'bivariate_class', 512, 'geom'), 'hex')
+    SELECT encode(ST_AsMVT(q, 'bivariate_class', 4096, 'geom'), 'hex')
     FROM (
              SELECT bivariate_class,
-                    ST_AsMvtGeom(ST_Union(geom), TileBBox(:z, :x, :y), 512, 0, false) AS geom
+                    ST_AsMvtGeom(ST_Union(geom), TileBBox(:z, :x, :y), 4096, 0, false) AS geom
              FROM osm_quality_bivariate_grid_1000
              WHERE zoom = :z
                AND geom && TileBBox(:z, :x, :y)

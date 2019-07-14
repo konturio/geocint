734994504495d44921e04b42bc4d65aa0d34aadf
Geocint GIS User <darafei@kontur.io>
2019-07-14 23:39:44 +0300

    enable more parallel

--- procedures/trim_water_in_osm_population_raw.sql
+++ procedures/trim_water_in_osm_population_raw.sql
@@ -1,20 +1,54 @@
 alter table osm_population_raw set (parallel_workers=32);
 drop table if exists osm_population_raw_nowater;
 
-create table osm_population_raw_nowater as (
-    select
-        coalesce(ST_Difference(p.geom, ST_Union(w.geom)), p.geom) as geom,
-        p.osm_type,
-        p.osm_id,
-        null::float                                               as area,
-        p.population,
-        null::float                                               as people_per_sq_km,
-        p.admin_level
-    from
-        osm_population_raw p
-            left join osm_water_polygons w on (ST_Intersects(w.geom, p.geom))
-    group by p.geom, p.osm_type,  p.osm_id, p.population, p.admin_level
+
+
+drop table if exists tmp_osm_pop_split_for_water_trim;
+create table tmp_osm_pop_split_for_water_trim as (
+    select osm_type, osm_id, population, admin_level, ST_Subdivide(geom, 100) as geom
+    from osm_population_raw
+    order by geom );
+alter table tmp_osm_pop_split_for_water_trim
+    set (parallel_workers=32);
+drop table if exists tmp_osm_population_raw_nowater;
+create table tmp_osm_population_raw_nowater as (
+    select p.geom as p_geom,
+           ST_Collect(w.geom) as w_geom,
+           p.osm_type,
+           p.osm_id,
+           p.population,
+           p.admin_level
+    from tmp_osm_pop_split_for_water_trim p
+         left join osm_water_polygons w on ST_Intersects(w.geom, p.geom)
+    group by p.osm_type, p.osm_id, p.population, p.admin_level, p.geom
 );
+drop table tmp_osm_pop_split_for_water_trim;
+drop table if exists osm_population_raw_nowater;
+
+alter table tmp_osm_population_raw_nowater
+    set (parallel_workers=32);
+
+create table osm_population_raw_nowater as (
+    select population,
+           osm_type,
+           osm_id,
+           admin_level,
+           null::float as area,
+           null::float as people_per_sq_km,
+           ST_Collect(geom) as geom
+    from (
+             select population,
+                    osm_type,
+                    osm_id,
+                    admin_level,
+                    ST_Difference(p_geom, ST_UnaryUnion(w_geom)) as geom
+             from tmp_osm_population_raw_nowater offset 0
+         ) z
+    group by 1, 2, 3, 4
+);
+
+
+
 drop table osm_population_raw;
 alter table osm_population_raw_nowater rename to osm_population_raw;
 delete from osm_population_raw where geom is null or ST_IsEmpty(geom);
--- tables/osm_water_polygons.sql
+++ tables/osm_water_polygons.sql
@@ -5,7 +5,7 @@ create table osm_water_polygons as (
     osm_type,
     osm_id,
     ST_Area(geog)                      as area,
-    ST_Transform(geog::geometry, 3857) as geom
+    ST_Subdivide(ST_Transform(geog::geometry, 3857)) as geom
   from
     osm
   where
@@ -15,7 +15,7 @@ create table osm_water_polygons as (
 );
 
 insert into osm_water_polygons (osm_type, osm_id, geom) 
-select 'oceans' as osm_type, gid as osm_id, geom as geom from water_polygons_vector;
+select 'oceans' as osm_type, gid as osm_id, ST_Subdivide(geom) as geom from water_polygons_vector;
 
 create index on osm_water_polygons using gist (geom);
 

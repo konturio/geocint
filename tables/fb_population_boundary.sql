f37875befe0406517db35afc494b5106465b7bc8
Konstantin <kshmidman@objectstyle.com>
2019-09-25 18:24:17 +0300

    FB population. Temp commit

---
+++ tables/fb_population_boundary.sql
@@ -0,0 +1,8 @@
+drop table if exists fb_all_osm_countries;
+create table fb_all_osm_countries as (select ST_Subdivide(ST_Transform(geog::geometry,3857)) as geom, tags->>'name' as name, tags->>'ISO3166-1:alpha3' as iso from osm where tags @> '{"admin_level":"2", "boundary":"administrative"}' 
+	and osm_type='relation'); 
+
+drop table if exists fb_population_boundary;
+create table fb_population_boundary as (select * from fb_all_osm_countries c where exists(select from fb_population_vector p where ST_Intersects(p.geom, c.geom)));
+
+drop table fb_all_osm_countries;

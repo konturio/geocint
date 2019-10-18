3b55c50ac6623923fe08961925537230f792455b
Konstantin <kshmidman@objectstyle.com>
2019-10-18 18:03:43 +0300

    Gets country for FB population from country codes

--- tables/fb_population_boundary.sql
+++ tables/fb_population_boundary.sql
@@ -1,3 +1,2 @@
 drop table if exists fb_population_boundary;
-create table fb_population_boundary as (select gid, gid_0 as iso, name_0 as name, ST_Subdivide(geom) as geom from gadm_countries_boundary c where exists(select from fb_population_vector p, ST_Subdivide(c.geom) c_sub_geom where ST_Intersects(p.geom, c_sub_geom)));
-
+create table fb_population_boundary as (select g.gid, g.gid_0 as iso, g.name_0 as name, ST_Subdivide(geom) as geom from gadm_countries_boundary g inner join fb_country_codes c on c.code = g.gid_0);

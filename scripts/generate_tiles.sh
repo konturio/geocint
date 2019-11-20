83828970d4a6bbb52a568e3139c81c056aab1287
Darafei Praliaskouski <me@komzpa.net>
2019-11-20 15:19:23 +0300

    Create directory too

--- scripts/generate_tiles.sh
+++ scripts/generate_tiles.sh
@@ -8,7 +8,7 @@ for (( z=$zoom; z<=7; z++ )); do
   let "x0 = 2**$z"
   let "y0 = 2**$z"
   for (( x=0; x<$x0; x++ )); do
-    mkdir -p data/tiles/osm_quality_bivariate/$z/$x
+    mkdir -p data/tiles/$1/$z/$x
     for (( y=0; y<$y0; y++ )); do
       file="data/tiles/$1/$z/$x/$y.mvt"
       echo "psql -q -X -f scripts/$1.sql -v x=$x -v y=$y -v z=$z | xxd -r -p > $file"

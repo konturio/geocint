e43f4bdd10a7e3d3b49bfee89850f46191a5eef4
Alexander Zapasnik <sanych27@gmail.com>
2019-12-05 13:53:06 +0300

    #1288 Update user tiles. Add layer with hexagons

--- scripts/users.sql
+++ scripts/users.sql
@@ -1,9 +1,9 @@
 copy (
-    select encode(ST_AsMVT(q, 'users', 8192, 'geom'), 'hex')
+    select encode(ST_AsMVT(q, 'users', 8192, 'centroid') || ST_AsMVT(q, 'hexagon', 8192, 'geom'), 'hex')
     from (
              select osm_user as top_user,
---                     ST_AsMVTGeom(geom, ST_TileEnvelope(:z, :x, :y), 8192, 64, true) as geom
-                    st_centroid(geom) as geom
+                    st_centroid(geom) as centroid,
+                    ST_AsMVTGeom(geom, ST_TileEnvelope(:z, :x, :y), 8192, 64, true) as geom
              from osm_users_hex
              where resolution = LEAST(calculate_h3_res(:z), 8)
                and geom && ST_TileEnvelope(:z, :x, :y)

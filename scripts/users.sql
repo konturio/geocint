3da029fa9bbd5c104b7bb6ad873edfbf9a24ca99
Alexander Zapasnik <sanych27@gmail.com>
2019-12-06 11:09:13 +0300

    #1288 User tiles. Fix missed centroid layer

--- scripts/users.sql
+++ scripts/users.sql
@@ -2,7 +2,7 @@ copy (
     select encode(ST_AsMVT(q, 'users', 8192, 'centroid') || ST_AsMVT(q, 'hexagon', 8192, 'geom'), 'hex')
     from (
              select osm_user as top_user,
-                    st_centroid(geom) as centroid,
+                    ST_AsMVTGeom(st_centroid(geom), ST_TileEnvelope(:z, :x, :y), 8192, 64, true) as centroid,
                     ST_AsMVTGeom(geom, ST_TileEnvelope(:z, :x, :y), 8192, 64, true) as geom
              from osm_users_hex
              where resolution = LEAST(calculate_h3_res(:z), 8)

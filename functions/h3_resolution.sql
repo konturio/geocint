982e4085385235fcdee3a9a6e63a7e8100a29380
Alexander Zapasnik <sanych27@gmail.com>
2019-12-09 12:26:29 +0300

    #1288 User tiles. Simplify h3 resolutions calculation

--- functions/h3_resolution.sql
+++ functions/h3_resolution.sql
@@ -13,22 +13,17 @@ drop table if exists h3_resolutions;
 
 create table h3_resolutions (id integer, edge_length float);
 
-insert into h3_resolutions (id, edge_length) values (0, 1107.712591000);
-insert into h3_resolutions (id, edge_length) values (1, 418.6760055);
-insert into h3_resolutions (id, edge_length) values (2, 158.244655800);
-insert into h3_resolutions (id, edge_length) values (3, 59.810857940);
-insert into h3_resolutions (id, edge_length) values (4, 59.810857940);
-insert into h3_resolutions (id, edge_length) values (5, 8.544408276);
-insert into h3_resolutions (id, edge_length) values (6, 3.229482772);
-insert into h3_resolutions (id, edge_length) values (7, 1.220629759);
-insert into h3_resolutions (id, edge_length) values (8, 0.461354684);
-insert into h3_resolutions (id, edge_length) values (9, 0.174375668);
-insert into h3_resolutions (id, edge_length) values (10, 0.065907807);
-insert into h3_resolutions (id, edge_length) values (11, 0.024910561);
-insert into h3_resolutions (id, edge_length) values (12, 0.009415526);
-insert into h3_resolutions (id, edge_length) values (13, 0.003559893);
-insert into h3_resolutions (id, edge_length) values (14, 0.001348575);
-insert into h3_resolutions (id, edge_length) values (15, 0.000509713);
+do
+$$
+    declare
+        i integer;
+    begin
+        for i in 0..15
+            loop
+                insert into h3_resolutions (id, edge_length) values (i, h3_edge_length(i));
+            end loop;
+    end;
+$$;
 
 
 drop function if exists calculate_h3_res(z integer);
@@ -40,10 +35,10 @@ create or replace function calculate_h3_res(z integer)
 $func$
 SELECT id FROM
     (
-        (SELECT id, edge_length FROM h3_resolutions WHERE edge_length >= ZRES(z) * 15 / 1000 ORDER BY edge_length LIMIT 1)
+        (SELECT id, edge_length FROM h3_resolutions WHERE edge_length >= ZRES(z) * 15  ORDER BY edge_length LIMIT 1)
         UNION ALL
-        (SELECT id, edge_length FROM h3_resolutions WHERE edge_length < ZRES(z) * 15 / 1000 ORDER BY edge_length DESC LIMIT 1)
+        (SELECT id, edge_length FROM h3_resolutions WHERE edge_length < ZRES(z) * 15  ORDER BY edge_length DESC LIMIT 1)
     ) as foo
-ORDER BY abs(ZRES(z) * 15 / 1000 - edge_length) LIMIT 1;
+ORDER BY abs(ZRES(z) * 15 - edge_length) LIMIT 1;
 $func$;
 

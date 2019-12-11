2f29efeb99c08869900d36fc0f683a10bd4520df
Alexander Zapasnik <sanych27@gmail.com>
2019-12-11 11:33:54 +0300

    #1288 User tiles. PR fixes

--- functions/calculate_h3_res.sql
+++ functions/calculate_h3_res.sql
@@ -1,13 +1,3 @@
-drop function if exists ZRes(z integer);
-
-create or replace function ZRes(z integer)
-    returns float
-    returns null on null input
-    language sql immutable as
-$func$
-select (40075016.6855785/(256*2^z));
-$func$;
-
 drop function if exists calculate_h3_res(z integer);
 
 create or replace function calculate_h3_res(z integer)
@@ -18,6 +8,6 @@ $func$
     with h3_resolutions as (
         select i as id, h3_edge_length(i) as edge_length from generate_series(0,15) i
     )
-    SELECT id FROM h3_resolutions ORDER BY abs(ZRES(z) * 15 - edge_length) LIMIT 1;
+    SELECT id FROM h3_resolutions ORDER BY abs(40075016.6855785/(256*2^z) * 15 - edge_length) LIMIT 1;
 $func$;
 

eb938f3304f775a8241c3fe35b4ee9ffdabd2b78
Alexander Zapasnik <sanych27@gmail.com>
2019-12-19 14:47:20 +0300

    #1557 calculate_h3_res.sql fix code format

--- functions/calculate_h3_res.sql
+++ functions/calculate_h3_res.sql
@@ -4,7 +4,7 @@ create or replace function calculate_h3_res(z integer)
     returns table
             (
                 tile_resolution integer,
-                zoom_lvl integer
+                zoom_lvl        integer
             )
     returns null on null input
     language plpgsql immutable
@@ -16,10 +16,11 @@ begin
             with h3_resolutions as (
                 select i as id, h3_edge_length(i) as edge_length from generate_series(0, 15) i
             )
-            SELECT LEAST((SELECT id
-                          FROM h3_resolutions
-                          ORDER BY abs(40075016.6855785 / (256 * 2 ^ z) * 15 - edge_length)
-                          LIMIT 1),
+            SELECT LEAST((
+                             SELECT id
+                             FROM h3_resolutions
+                             ORDER BY abs(40075016.6855785 / (256 * 2 ^ z) * 15 - edge_length)
+                             LIMIT 1),
                          8) as tile_resolution,
                    z        as zoom_lvl;
     ELSE
@@ -27,15 +28,13 @@ begin
             with h3_resolutions as (
                 select i as id, h3_edge_length(i) as edge_length from generate_series(0, 15) i
             )
-            SELECT LEAST(
-                                    (
-                                        SELECT id
-                                        FROM h3_resolutions
-                                        ORDER BY abs(40075016.6855785 / (256 * 2 ^ i) * 15 - edge_length)
-                                        LIMIT 1
-                                    ),
-                                    8) as tile_resolution,
-                            i          as zoom_lvl
+            SELECT LEAST((
+                             SELECT id
+                             FROM h3_resolutions
+                             ORDER BY abs(40075016.6855785 / (256 * 2 ^ i) * 15 - edge_length)
+                             LIMIT 1
+                         ), 8) as tile_resolution,
+                   i           as zoom_lvl
             from generate_series(8, 10) i;
     end if;
 end;

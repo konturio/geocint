21c71f8b77d8503fef2814d48a3f884abbd395f1
Geocint GIS User <darafei@kontur.io>
2020-01-04 22:33:15 +0300

    fix column name

--- tables/countries_info.sql
+++ tables/countries_info.sql
@@ -15,6 +15,6 @@ alter table countries_info add column population float;
 
 update countries_info c set year = (select a.year from wb_gdp a where a.year = (select max(b.year) from wb_gdp b where b.code= a.code) and a.code = c.code);
 update countries_info c set gdp = (select a.gdp from wb_gdp a where a.year = (select max(b.year) from wb_gdp b where b.code= a.code) and a.code = c.code);
-update countries_info c set population = (select sum(a.people) from tmp_countries_info a where c.code = a.code);
+update countries_info c set population = (select sum(a.population) from tmp_countries_info a where c.code = a.code);
 
 drop table if exists tmp_countries_info;

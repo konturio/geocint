e9132f3aa54429c193d063d28a8294ef7f8bcc63
Geocint GIS User <darafei@kontur.io>
2020-01-22 15:55:46 +0300

    1295 generate hexagons only for actually local users

--- tables/user_hours_h3.sql
+++ tables/user_hours_h3.sql
@@ -1,17 +1,23 @@
 drop table if exists user_hours_h3;
 
 create table user_hours_h3 as (
-    select h3,
-        sum(uc.hours) FILTER (
-            WHERE exists(select
-                         from osm_local_active_users au
-                         where au.osm_user = uc.osm_user
-                           and ST_DWithin(geog, ST_Transform(hex.geom, 4326)::geography, 50000))
-            )               as local_hours,
-        sum(uc.hours)     as total_hours
-    from osm_user_count_grid_h3 uc,
-        ST_HexagonFromH3(uc.h3) hex
-    group by h3
+select h3,
+       sum(uc.hours) FILTER (
+           WHERE exists(
+                   select
+                   from osm_local_active_users au
+                   where au.osm_user = uc.osm_user
+                     and ST_Distance(
+                                 geog,
+                                 uc.h3::geography
+                             ) <= (
+                             50000 + h3_edge_length(uc.resolution)
+                             )
+               )
+           )         as local_hours,
+       sum(uc.hours) as total_hours
+from osm_user_count_grid_h3 uc
+group by h3
 );
 
 create index on user_hours_h3(h3);

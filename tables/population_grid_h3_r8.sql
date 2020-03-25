b8fa74bebab4fec5979be59078e98753993b5fa3
Darafei Praliaskouski <me@komzpa.net>
2020-03-25 16:56:20 +0300

    Fix population grid projection bug

--- tables/population_grid_h3_r8.sql
+++ tables/population_grid_h3_r8.sql
@@ -2,11 +2,9 @@ drop table if exists population_grid_h3_r8;
 create table population_grid_h3_r8 as (
     select
         8 as resolution,
-        h3_geo_to_h3(centroid::point, 8) as h3,
+        h3_geo_to_h3(ST_Transform(centroid, 4326)::point, 8) as h3,
         sum(people) as population
     from
         population_vector
     group by 2
 );
-
-create index on population_grid_h3_r8 (h3);

12dd80a3b432d2b685f6cf240312f70eea22ebd9
Darafei Praliaskouski <me@komzpa.net>
2020-04-11 19:00:28 +0300

    Do not generate segments from last point to nowhere

--- functions/osm_way_nodes_to_segments.sql
+++ functions/osm_way_nodes_to_segments.sql
@@ -3,29 +3,36 @@ drop function if exists osm_way_nodes_to_segments;
 create or replace function osm_way_nodes_to_segments(geom geometry,
                                                      way_nodes bigint[],
                                                      osm_id bigint)
-  returns table
-          (
-            seg_id   bigint,
-            node_from bigint,
-            node_to   bigint,
-            seg_geom  geometry
-          )
-  language sql
+    returns table
+    (
+        seg_id    bigint,
+        node_from bigint,
+        node_to   bigint,
+        seg_geom  geometry
+    )
+    language sql
 as
 $$
-select
-  osm_id * 10000 + ROW_NUMBER() over()     as seg_id,
-  n                                        as node_from,
-  lead(n) over ()                          as node_to,
-  ST_MakeLine(pt, lead(pt) over ())        as seg_geom
+select *
 from
-  (
-    select
-      (ST_DumpPoints(geom)).geom as pt,
-      unnest(way_nodes)          as n
-  ) z
+    (
+        select
+            osm_id * 10000 + ROW_NUMBER() over () as seg_id,
+            n as node_from,
+            lead(n) over () as node_to,
+            ST_MakeLine(pt, lead(pt) over ()) as seg_geom
+        from
+            (
+                select
+                    (ST_DumpPoints(geom)).geom as pt,
+                    unnest(way_nodes) as n
+            ) z
+    ) z
+where
+    node_to is not null
+
 $$
-  immutable
-  strict
-  parallel safe
-  cost 10000;
+    immutable
+    strict
+    parallel safe
+    cost 10000;

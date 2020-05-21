4b6450a966810abb89b376144d91b0b22d145d85
Geocint GIS User <darafei@kontur.io>
2020-05-22 00:22:24 +0300

    fix PATH in autostart script

--- autostart_geocint.sh
+++ autostart_geocint.sh
@@ -1,7 +1,8 @@
 #!/bin/bash 
 
 set -e
-
-cd ~/geocint; git pull
+PATH="/home/gis/.local/bin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/games:/usr/local/games:/snap/bin"
+cd ~/geocint
+git pull
 profile_make clean
 profile_make -j -k all

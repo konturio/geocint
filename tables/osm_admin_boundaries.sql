0f52e8b3832462a8f0f8a7fc429fe103099b8726
trylinka <tatyanakarvat@gmail.com>
2020-06-19 15:16:43 +0300

    Removed extra tags

--- tables/osm_admin_boundaries.sql
+++ tables/osm_admin_boundaries.sql
@@ -11,5 +11,4 @@ create table osm_admin_boundaries as (
     where tags ? 'admin_level'
       and tags @>
           '{"boundary":"administrative"}'
-      and (tags ->> 'admin_level') in ('1', '2', '3', '4', '5', '6', '7', '8', '9', '10', '11')
 );

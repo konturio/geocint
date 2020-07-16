155c37613b4e5e9e9d2298b14144f3df0af6c6b5
trylinka <tatyanakarvat@gmail.com>
2020-07-16 19:02:03 +0300

    #1755 Return osm_population_raw code.

---
+++ tables/osm_population_raw.sql
@@ -0,0 +1,36 @@
+drop table if exists osm_population_raw;
+create table osm_population_raw as (
+    select ST_Transform(geog::geometry, 3857) as geom,
+           osm_type,
+           osm_id,
+           ST_Area(geog)                      as area,
+           (case
+               -- TODO: validator for values
+                when (tags ->> 'population') ~ E'^[[:digit:]]+([.][[:digit:]]+)?$'
+                    then (tags ->> 'population')::float
+                else null
+               end)                           as population,
+           1000000
+               * (case
+                      when (tags ->> 'population') ~ E'^[[:digit:]]+([.][[:digit:]]+)?$'
+                          then (tags ->> 'population')::float
+                      else null
+               end) / ST_Area(geog)           as people_per_sq_km,
+           -- TODO: validator for non-numerics
+           (case
+                when (tags ->> 'admin_level') ~ E'^[[:digit:]]+([.][[:digit:]]+)?$'
+                    then (tags ->> 'admin_level')::float
+                else null
+               end)                           as admin_level
+    from osm
+    where
+      -- TODO: validator for lines and points
+        ST_Dimension(geog::geometry) = 2
+      and tags ? 'population'
+      -- TODO: handle null admin level
+      and tags ->> 'admin_level' is not null
+    order by 1
+);
+
+create index on osm_population_raw using gist (geom);
+create index on osm_population_raw using gist (ST_PointOnSurface(geom));

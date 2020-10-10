103154d80d63b3c5d4781d6951bf76ecdd6c918b
Geocint GIS User <darafei@kontur.io>
2020-10-10 20:44:05 +0300

    Ask h3_edge_legth for meters explicitly

--- functions/calculate_h3_res.sql
+++ functions/calculate_h3_res.sql
@@ -1,11 +1,14 @@
-drop function if exists calculate_h3_res(z integer);
+drop function if exists calculate_h3_res(z integer
+);
 
-create or replace function calculate_h3_res(z integer)
-    returns table
-            (
-                tile_resolution integer,
-                zoom_lvl        integer
-            )
+create or replace function calculate_h3_res
+(
+    z integer
+)
+    returns table (
+        tile_resolution integer,
+        zoom_lvl        integer
+    )
     returns null on null input
     language plpgsql immutable
 as
@@ -14,27 +17,27 @@ begin
     if z < 8 then
         return query
             with h3_resolutions as (
-                select i as id, h3_edge_length(i) as edge_length from generate_series(0, 15) i
+                select i as id, h3_edge_length(i, 'm') as edge_length from generate_series(0, 15) i
             )
-            SELECT LEAST((
-                             SELECT id
-                             FROM h3_resolutions
-                             ORDER BY abs(40075016.6855785 / (256 * 2 ^ z) * 15 - edge_length)
-                             LIMIT 1),
+            select least((
+                             select id
+                             from h3_resolutions
+                             order by abs(40075016.6855785 / (256 * 2 ^ z) * 15 - edge_length)
+                             limit 1 ),
                          8) as tile_resolution,
-                   z        as zoom_lvl;
-    ELSE
+                   z as zoom_lvl;
+    else
         return query
             with h3_resolutions as (
-                select i as id, h3_edge_length(i) as edge_length from generate_series(0, 15) i
+                select i as id, h3_edge_length(i, 'm') as edge_length from generate_series(0, 15) i
             )
-            SELECT LEAST((
-                             SELECT id
-                             FROM h3_resolutions
-                             ORDER BY abs(40075016.6855785 / (256 * 2 ^ i) * 15 - edge_length)
-                             LIMIT 1
+            select least((
+                             select id
+                             from h3_resolutions
+                             order by abs(40075016.6855785 / (256 * 2 ^ i) * 15 - edge_length)
+                             limit 1
                          ), 8) as tile_resolution,
-                   i           as zoom_lvl
+                   i as zoom_lvl
             from generate_series(8, 12) i;
     end if;
 end;

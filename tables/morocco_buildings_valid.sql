87bc60a6f3f704eb5c154724b2c7266338b98071
trylinka <tatyanakarvat@gmail.com>
2020-10-15 12:01:21 +0300

    3460 Remove unnecessary files.

--- tables/morocco_buildings_valid.sql
+++
@@ -1,5 +0,0 @@
-drop table if exists morocco_buildings_valid;
-create table morocco_buildings_valid as (
-    select building_height,
-           ST_Transform(ST_CollectionExtract(ST_MakeValid(geom), 3), 3857) as geom
-       from morocco_buildings);

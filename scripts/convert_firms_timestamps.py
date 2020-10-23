137929c6f8f61ee689893438591f6f92f1b81e61
trylinka <tatyanakarvat@gmail.com>
2020-10-23 17:12:56 +0300

    3629 Change preprocess.

--- scripts/convert_firms_timestamps.py
+++ scripts/convert_firms_timestamps.py
@@ -2,10 +2,12 @@ import pandas as pd
 import sys
 import datetime
 
-csv_path = sys.argv[1]
+paths = sys.stdin.read().split('\n')
 
-df = pd.read_csv(csv_path, dtype={'acq_time': 'str'})
-df['acq_datetime'] = df['acq_date'] + ":" + df["acq_time"]
-df['acq_datetime'] = df['acq_datetime'].apply(lambda t: str(datetime.datetime.strptime(t, '%Y-%m-%d:%H%M')))
-df.drop(['acq_date', 'acq_time'], axis=1, inplace=True)
-df.to_csv(csv_path.replace('.csv', '_proc.csv'), index=False)
+for csv_path in paths:
+    if csv_path:
+        df = pd.read_csv(csv_path, dtype={'acq_time': 'str'})
+        df['acq_datetime'] = df['acq_date'] + ":" + df["acq_time"]
+        df['acq_datetime'] = df['acq_datetime'].apply(lambda t: str(datetime.datetime.strptime(t, '%Y-%m-%d:%H%M')))
+        df.drop(['acq_date', 'acq_time'], axis=1, inplace=True)
+        df.to_csv(csv_path.replace('.csv', '_proc.csv'), index=False)

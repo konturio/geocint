c5ce30eb7c6ee1ff43e49df77909c0780e91f090
trylinka <tatyanakarvat@gmail.com>
2020-11-11 15:20:40 +0200

    Generated all h3 resolutions for firms_fires_h3.

--- tables/firms_fires_h3.sql
+++ tables/firms_fires_h3.sql
@@ -1,21 +1,32 @@
-drop table if exists firms_fires_h3;
+alter table firms_fires
+    set (parallel_workers = 32);
+
+drop table firms_fires_h3;
 create table firms_fires_h3 as (
-    select distinct acq_datetime,
-                    h3_geo_to_h3(ST_SetSrid(ST_Point(longitude, latitude), 4326), 8) as h3
+    select h3_geo_to_h3(ST_SetSrid(ST_Point(longitude, latitude), 4326), 8) as h3,
+           count(distinct acq_datetime)                                     as wildfires,
+           8                                                                as resolution
     from firms_fires
+    group by 1
 );
 
-drop table firms_fires_h3_r8;
-create table firms_fires_h3_r8 as (
-    select count(*) as wildfires,
-           h3_geo_to_h3(ST_SetSrid(ST_Point(longitude, latitude), 4326), 6) as h3
-    from firms_fires
-    group by 2
-);
+alter table firms_fires_h3
+    set (parallel_workers = 32);
 
-drop table if exists firms_fires_h3_r6_geom;
-create table firms_fires_h3_r6_geom as (
-    select h3_to_geo_boundary_geometry(h3) as geom,
-           fires
-    from firms_fires_h3_r6
-);
+do
+$$
+    declare
+        res integer;
+    begin
+        res = 8;
+        while res > 0
+            loop
+                insert into firms_fires_h3 (h3, wildfires, resolution)
+                select h3_to_parent(h3) as h3, sum(wildfires) as wildfires, (res - 1) as resolution
+                from firms_fires_h3
+                where resolution = res
+                group by 1;
+                res = res - 1;
+            end loop;
+    end;
+$$;

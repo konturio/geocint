0633f99c00d61a52bc75c3afd72942a837146740
trylinka <tatyanakarvat@gmail.com>
2020-11-13 20:02:53 +0300

    Added wildfires for generating in stat.json.

--- tables/bivariate_axis.sql
+++ tables/bivariate_axis.sql
@@ -52,7 +52,8 @@ create table stat_h3_quality as (
                 avg(a.view_count) as agg_view_count,
                 avg(a.area_km2) as agg_area_km2,
                 avg(a.one) as agg_one,
-                avg(a.total_building_count) as agg_total_building_count
+                avg(a.total_building_count) as agg_total_building_count,
+                avg(a.wildfires) as agg_wildfires
             from
                 stat_h3 a
             where

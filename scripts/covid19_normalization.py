3edb40367c317dcfb463383e662c1e97acd502a7
trylinka <tatyanakarvat@gmail.com>
2020-11-20 00:15:07 +0300

    Changed links for covid19 datasets and added preprocessing them before coping.

---
+++ scripts/covid19_normalization.py
@@ -0,0 +1,8 @@
+import pandas as pd
+import sys
+
+csv_path = sys.argv[1]
+df = pd.read_csv(csv_path)
+df = df.melt(id_vars=['Province/State', 'Country/Region', 'Lat', 'Long'], var_name='date', value_name='value')
+df['date'] = pd.to_datetime(df.date)
+df.to_csv(csv_path.replace('.csv', '_normalized.csv'), index=False, header=True)

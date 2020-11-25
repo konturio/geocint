84d8d282b98c83b0d11f6da2168731acfdb003ac
trylinka <tatyanakarvat@gmail.com>
2020-11-25 09:24:39 +0300

    Renamed table and files in targets.

--- tables/morocco_buildings_benchmark_aoi.sql
+++ tables/morocco_buildings_benchmark_aoi.sql
@@ -1,7 +1,7 @@
 drop table if exists morocco_buildings_benchmark_aoi;
 create table morocco_buildings_benchmark_aoi as (
     select city,
-           ST_Convexhull(ST_Collect(ST_Transform(footprint, 3857))) as geom
+           ST_Convexhull(ST_Collect(ST_Transform(wkb_geometry, 3857))) as geom
     from morocco_buildings_manual
     group by city
 );

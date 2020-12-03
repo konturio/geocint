0e8bfa6de6d5a12fb70c66d683d597ee5b85e4f3
pkokush <pkokush@kontur.io>
2020-12-03 10:17:27 +0300

    4099: daily export of fires for last 13 months

--- tables/firms_fires2_h3.sql
+++ tables/firms_fires2_h3.sql
@@ -6,5 +6,5 @@ create table firms_fires2_h3 as (
     select h3_geo_to_h3(ST_SetSrid(ST_Point(longitude, latitude), 4326), 8) as h3,
            acq_datetime                                                     as datetime
     from firms_fires
-    where acq_datetime < now() - interval '13 months'
+    where acq_datetime > now() - interval '13 months'
 );

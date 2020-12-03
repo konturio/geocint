7faf71bd35a40c840119773ddb196863eeb85aef
pkokush <pkokush@kontur.io>
2020-12-03 12:06:03 +0300

    4099: daily export of fires for last 13 months

--- scripts/normilize_firms_fires.py
+++ scripts/normilize_firms_fires.py
@@ -3,12 +3,15 @@ import sys
 import datetime
 import hashlib
 
+
 def create_empty_if_not_exists(df, column):
     if column not in df: df[column] = ""
+
 
 def create_md5_hash_column(df):
     return pd.Series(
         (hashlib.md5((''.join(map(str, tuple(row)))).encode("utf-8")).hexdigest() for _, row in df.iterrows()))
+
 
 csv_path = sys.argv[1]
 
@@ -21,5 +24,8 @@ create_empty_if_not_exists(df, 'bright_ti4')
 create_empty_if_not_exists(df, 'bright_t31')
 create_empty_if_not_exists(df, 'bright_ti5')
 
-df.drop(['acq_date', 'acq_time'], axis=1, inplace=True)
+df = df[['latitude', 'longitude', 'brightness', 'bright_ti4', 'scan',
+         'track', 'satellite', 'confidence', 'version', 'bright_t31',
+         'bright_ti5', 'frp', 'daynight', 'acq_datetime', 'hash']]
+
 df.to_csv(csv_path.replace('.csv', '_proc.csv'), index=False)

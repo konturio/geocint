a26be2f461bdf4a451349fa119769e598d26d946
pkokush <pkokush@kontur.io>
2020-12-09 18:29:49 +0300

    4099: daily export of fires for last 13 months

--- tables/global_fires_stat_h3.sql
+++ tables/global_fires_stat_h3.sql
@@ -5,6 +5,7 @@ drop table if exists global_fires_stat_h3;
 create table global_fires_stat_h3 as (
     select h3_geo_to_h3(ST_SetSrid(ST_Point(longitude, latitude), 4326), 8) as h3,
            count(distinct date_trunc('day', acq_datetime))                  as wildfires,
+           array_agg(distinct date_trunc('day', acq_datetime))              as days_array,
            8                                                                as resolution
     from global_fires
     where acq_datetime > now() - interval '13 months'
@@ -22,9 +23,12 @@ $$
         res = 8;
         while res > 0
             loop
-                insert into global_fires_stat_h3 (h3, wildfires, resolution)
-                select h3_to_parent(h3) as h3, sum(wildfires) as wildfires, (res - 1) as resolution
-                from global_fires_stat_h3
+                insert into global_fires_stat_h3 (h3, wildfires,  days_array, resolution)
+                select h3_to_parent(h3) as h3,
+ 	                count(distinct days)  as wildfires,
+                    array_agg(distinct days) as days_array,
+                    (res - 1) as resolution
+                from global_fires_stat_h3, unnest(days_array) as days
                 where resolution = res
                 group by 1;
                 res = res - 1;

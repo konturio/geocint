77d419b12e4bc134d56c736d4cd037caf4f449e1
trylinka <tatyanakarvat@gmail.com>
2020-12-15 21:29:45 +0300

    4420: Update night bash script to prevent deploy on prod from wrong git branch.

--- autostart_geocint.sh
+++ autostart_geocint.sh
@@ -1,4 +1,4 @@
-#!/bin/bash 
+#!/bin/bash
 
 set -e
 PATH="/home/gis/.local/bin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/games:/usr/local/games:/snap/bin"
@@ -6,3 +6,10 @@ cd ~/geocint
 git pull
 profile_make clean
 profile_make -j -k all
+BRANCH = $(git rev-parse --abbrev-ref HEAD)
+if [[ "$BRANCH" = "master" ]];
+then
+  profile_make deploy/test deploy/production
+else
+  profile_make deploy/test
+fi

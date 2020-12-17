648f8e7a42963fe1c3165fa6b40df257aa28705d
trylinka <tatyanakarvat@gmail.com>
2020-12-17 17:03:57 +0300

    4420: Notification about current running target. Fix for $branch variable.

--- autostart_geocint.sh
+++ autostart_geocint.sh
@@ -4,13 +4,13 @@ set -e
 PATH="/home/gis/.local/bin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/games:/usr/local/games:/snap/bin"
 cd ~/geocint
 git pull
-#profile_make clean
+profile_make clean
 branch="$(git rev-parse --abbrev-ref HEAD)"
 if [[ "$branch" = "master" ]];
 then
   echo "Current branch is $branch. Running dev and prod targets."
-  echo profile_make -j -k dev prod
+  profile_make -j -k dev prod
 else
   echo "Current branch is $branch (not master). Running dev target."
-  echo profile_make -j -k dev
+  profile_make -j -k dev
 fi

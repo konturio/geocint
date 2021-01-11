89ce25ee56d14b3cc34f496b8b02b05a36769e40
trylinka <tatyanakarvat@gmail.com>
2021-01-12 00:37:55 +0300

    4676: Merged statPatch.json into stat.json.

---
+++ tables/bivariate_colors.sql
@@ -0,0 +1,168 @@
+drop table if exists bivariate_colors;
+create table bivariate_colors (
+    color text,
+    color_comment text,
+    corner json
+);
+
+insert into bivariate_colors (color, color_comment, corner)
+values ('#58b075', 'green', jsonb_build_array('good'));
+
+insert into bivariate_colors (color, color_comment, corner)
+values ('#e8e89d', 'yellow', jsonb_build_array('bad', 'unimportant'));
+
+insert into bivariate_colors (color, color_comment, corner)
+values ('#c85ac8', 'alternate_color_1_magenta?', jsonb_build_array('bad', 'good'));
+
+insert into bivariate_colors (color, color_comment, corner)
+values ('#5ac8c8', 'alternate_color_2_cyan?', jsonb_build_array('good', 'bad'));
+
+insert into bivariate_colors (color, color_comment, corner)
+values ('#c85a5a', 'red', jsonb_build_array('bad'));
+
+insert into bivariate_colors (color, color_comment, corner)
+values ('#e8e89d', 'yellow', jsonb_build_array('bad', 'unimportant', 'good'));
+
+insert into bivariate_colors (color, color_comment, corner)
+values ('#5ac87f', 'pale green', jsonb_build_array('good', 'unimportant'));
+
+insert into bivariate_colors (color, color_comment, corner)
+values ('#53986a', 'dark green', jsonb_build_array('good', 'important'));
+
+insert into bivariate_colors (color, color_comment, corner)
+values ('#e8e89d', 'yellow', jsonb_build_array('good', 'bad', 'unimportant'));
+
+insert into bivariate_colors (color, color_comment, corner)
+values ('#58b075', 'green', jsonb_build_array('good', 'neutral'));
+
+insert into bivariate_colors (color, color_comment, corner)
+values ('#58b075', 'green', jsonb_build_array('neutral', 'good'));
+
+insert into bivariate_colors (color, color_comment, corner)
+values ('#e41a1c', 'bloody red', jsonb_build_array('bad', 'important'));
+
+insert into bivariate_colors (color, color_comment, corner)
+values ('#f37e7e', 'light red', jsonb_build_array('neutral', 'bad'));
+
+insert into bivariate_colors (color, color_comment, corner)
+values ('#f37e7e', 'light red', jsonb_build_array('neutral', 'bad'));
+
+insert into bivariate_colors (color, color_comment, corner)
+values ('#f37e7e', 'light red', jsonb_build_array('bad', 'neutral'));
+
+insert into bivariate_colors (color, color_comment, corner)
+values ('#5ac87f', 'pale green', jsonb_build_array('unimportant', 'good'));
+
+insert into bivariate_colors (color, color_comment, corner)
+values ('#53986a', 'dark green', jsonb_build_array('important', 'good'));
+
+insert into bivariate_colors (color, color_comment, corner)
+values ('#f37e7e', 'light red',jsonb_build_array('unimportant', 'bad'));
+
+insert into bivariate_colors (color, color_comment, corner)
+values ('#e41a1c', 'bloody red', jsonb_build_array('important', 'bad'));
+
+insert into bivariate_colors (color, color_comment, corner)
+values ('#f37e7e', 'light red', jsonb_build_array('neutral', 'bad', 'unimportant'));
+
+insert into bivariate_colors (color, color_comment, corner)
+values ('#f37e7e', 'light red', jsonb_build_array('bad', 'unimportant', 'neutral'));
+
+insert into bivariate_colors (color, color_comment, corner)
+values ('#cccccc', '#cccccc', jsonb_build_array('neutral'));
+
+insert into bivariate_colors (color, color_comment, corner)
+values ('#c85a5a', 'red', jsonb_build_array('bad', 'unimportant', 'important'));
+
+insert into bivariate_colors (color, color_comment, corner)
+values ('#999999', 'dark grey', jsonb_build_array('bad', 'important', 'good'));
+
+insert into bivariate_colors (color, color_comment, corner)
+values ('#e8e89d', 'yellow', jsonb_build_array('good', 'unimportant', 'bad'));
+
+insert into bivariate_colors (color, color_comment, corner)
+values ('#999999', 'dark grey', jsonb_build_array('good', 'bad', 'important'));
+
+insert into bivariate_colors (color, color_comment, corner)
+values ('#c85a5a', 'red', jsonb_build_array('important', 'bad', 'unimportant'));
+
+insert into bivariate_colors (color, color_comment, corner)
+values ('#e8e89d', 'yellow', jsonb_build_array('neutral', 'unimportant'));
+
+insert into bivariate_colors (color, color_comment, corner)
+values ('#e8e89d', 'yellow', jsonb_build_array('important', 'neutral'));
+
+insert into bivariate_colors (color, color_comment, corner)
+values ('#e8e89d', 'yellow', jsonb_build_array('neutral', 'important'));
+
+insert into bivariate_colors (color, color_comment, corner)
+values ('#999999', 'dark grey', jsonb_build_array('good', 'important', 'bad'));
+
+insert into bivariate_colors (color, color_comment, corner)
+values ('#e8e89d',  'yellow', jsonb_build_array('unimportant', 'neutral'));
+
+insert into bivariate_colors (color, color_comment, corner)
+values ('#e8e89d',  'yellow', jsonb_build_array('bad', 'good', 'unimportant'));
+
+insert into bivariate_colors (color, color_comment, corner)
+values ('#999999', 'dark grey', jsonb_build_array('bad', 'good', 'important'));
+
+insert into bivariate_colors (color, color_comment, corner)
+values ('#c85a5a', 'red', jsonb_build_array('bad', 'important', 'unimportant'));
+
+insert into bivariate_colors (color, color_comment, corner)
+values ('#5ac87f', 'pale green', jsonb_build_array('neutral', 'good', 'unimportant'));
+
+insert into bivariate_colors (color, color_comment, corner)
+values ('#5ac87f', 'pale green', jsonb_build_array('good', 'unimportant', 'neutral'));
+
+insert into bivariate_colors (color, color_comment, corner)
+values ('#cccccc',  '#cccccc', jsonb_build_array('bad', 'unimportant', 'good', 'important'));
+
+insert into bivariate_colors (color, color_comment, corner)
+values ('#53986a', 'dark green', jsonb_build_array('neutral', 'good', 'important'));
+
+insert into bivariate_colors (color, color_comment, corner)
+values ('#c85ac8', 'alternate_ _1_magenta?', jsonb_build_array('unimportant', 'important'));
+
+insert into bivariate_colors (color, color_comment, corner)
+values ('#e41a1c', 'bloody red', jsonb_build_array('neutral', 'bad', 'important'));
+
+insert into bivariate_colors (color, color_comment, corner)
+values ('#53986a', 'dark green', jsonb_build_array('good', 'important', 'neutral'));
+
+insert into bivariate_colors (color, color_comment, corner)
+values ('#e8e89d', 'yellow', jsonb_build_array('unimportant'));
+
+insert into bivariate_colors (color, color_comment, corner)
+values ('#999999', 'dark grey', jsonb_build_array('important'));
+
+insert into bivariate_colors (color, color_comment, corner)
+values ('#cccccc', '#cccccc', jsonb_build_array('good', 'important', 'bad', 'unimportant'));
+
+insert into bivariate_colors (color, color_comment, corner)
+values ('#e41a1c', 'bloody red', jsonb_build_array('bad', 'important', 'neutral'));
+
+insert into bivariate_colors (color, color_comment, corner)
+values ('#5ac8c8', 'alternate_ _2_cyan?', jsonb_build_array('important', 'unimportant'));
+
+insert into bivariate_colors (color, color_comment, corner)
+values ('#58b075', 'green', jsonb_build_array('good', 'unimportant', 'important'));
+
+insert into bivariate_colors (color, color_comment, corner)
+values ('#58b075', 'green', jsonb_build_array('good', 'important', 'unimportant'));
+
+insert into bivariate_colors (color, color_comment, corner)
+values ('#c85a5a', 'red', jsonb_build_array('unimportant', 'bad', 'important'));
+
+insert into bivariate_colors (color, color_comment, corner)
+values ('#58b075',  'green', jsonb_build_array('important', 'good', 'unimportant'));
+
+insert into bivariate_colors (color, color_comment, corner)
+values ('#58b075', 'green', jsonb_build_array('unimportant', 'good', 'important'));
+
+insert into bivariate_colors (color, color_comment, corner)
+values ('#cccccc', '#cccccc', jsonb_build_array('good', 'unimportant', 'bad', 'important'));
+
+insert into bivariate_colors (color, color_comment, corner)
+values ('#cccccc', '#cccccc', jsonb_build_array('bad', 'important', 'good', 'unimportant'));
--- tables/bivariate_indicators.sql
+++ tables/bivariate_indicators.sql
@@ -1,57 +1,55 @@
-drop table if exists bivariate_copyrights;
-
 drop table if exists bivariate_indicators;
-
 create table bivariate_indicators
 (
     param_id   text,
     param_label text,
     copyrights json,
+    direction json,
     is_base boolean not null default false
 );
 
 alter table bivariate_indicators
     set (parallel_workers = 32);
 
-insert into bivariate_indicators (param_id, param_label, copyrights)
-values ('one', '1', '["Numbers © Muḥammad ibn Mūsā al-Khwārizmī"]'::json);
+insert into bivariate_indicators (param_id, param_label, copyrights, direction)
+values ('one', '1', '["Numbers © Muḥammad ibn Mūsā al-Khwārizmī"]'::json, jsonb_build_array(jsonb_build_array('neutral'), jsonb_build_array('neutral')));
 
-insert into bivariate_indicators (param_id, param_label, copyrights)
-values ('area_km2', 'Area', '["Concept of areas © Brahmagupta, René Descartes"]'::json);
+insert into bivariate_indicators (param_id, param_label, copyrights, direction)
+values ('area_km2', 'Area', '["Concept of areas © Brahmagupta, René Descartes"]'::json, jsonb_build_array(jsonb_build_array('neutral'), jsonb_build_array('neutral')));
 
-insert into bivariate_indicators (param_id, param_label, copyrights)
-values ('count', 'OSM objects', jsonb_build_array('© OpenStreetMap contributors https://www.openstreetmap.org/copyright'));
+insert into bivariate_indicators (param_id, param_label, copyrights, direction)
+values ('count', 'OSM objects', jsonb_build_array('© OpenStreetMap contributors https://www.openstreetmap.org/copyright'), jsonb_build_array(jsonb_build_array('bad'), jsonb_build_array('good')));
 
-insert into bivariate_indicators (param_id, param_label, copyrights)
-values ('view_count', 'OSM Map views', jsonb_build_array('© OpenStreetMap contributors https://www.openstreetmap.org/copyright'));
+insert into bivariate_indicators (param_id, param_label, copyrights, direction)
+values ('view_count', 'OSM Map views', jsonb_build_array('© OpenStreetMap contributors https://www.openstreetmap.org/copyright'),jsonb_build_array(jsonb_build_array('bad', 'unimportant'), jsonb_build_array('good', 'important')));
 
-insert into bivariate_indicators (param_id, param_label, copyrights)
-values ('avg_ts', 'OSM Last Edit Date (avg)', jsonb_build_array('© OpenStreetMap contributors https://www.openstreetmap.org/copyright'));
+insert into bivariate_indicators (param_id, param_label, copyrights, direction)
+values ('avg_ts', 'OSM Last Edit Date (avg)', jsonb_build_array('© OpenStreetMap contributors https://www.openstreetmap.org/copyright'), jsonb_build_array(jsonb_build_array('bad', 'unimportant'), jsonb_build_array('good')));
 
-insert into bivariate_indicators (param_id, param_label, copyrights)
-values ('max_ts', 'OSM Last Edit Date (max)', jsonb_build_array('© OpenStreetMap contributors https://www.openstreetmap.org/copyright'));
+insert into bivariate_indicators (param_id, param_label, copyrights, direction)
+values ('max_ts', 'OSM Last Edit Date (max)', jsonb_build_array('© OpenStreetMap contributors https://www.openstreetmap.org/copyright'), jsonb_build_array(jsonb_build_array('bad', 'unimportant'), jsonb_build_array('good')));
 
-insert into bivariate_indicators (param_id, param_label, copyrights)
-values ('p90_ts', 'OSM 90% Mapped Before', jsonb_build_array('© OpenStreetMap contributors https://www.openstreetmap.org/copyright'));
+insert into bivariate_indicators (param_id, param_label, copyrights, direction)
+values ('p90_ts', 'OSM 90% Mapped Before', jsonb_build_array('© OpenStreetMap contributors https://www.openstreetmap.org/copyright'), jsonb_build_array(jsonb_build_array('bad', 'unimportant'), jsonb_build_array('good')));
 
-insert into bivariate_indicators (param_id, param_label, copyrights)
-values ('osm_users', 'OSM Mappers Edited Here', jsonb_build_array('© OpenStreetMap contributors https://www.openstreetmap.org/copyright'));
+insert into bivariate_indicators (param_id, param_label, copyrights, direction)
+values ('osm_users', 'OSM Mappers Edited Here', jsonb_build_array('© OpenStreetMap contributors https://www.openstreetmap.org/copyright'), jsonb_build_array(jsonb_build_array('bad'), jsonb_build_array('good')));
 
-insert into bivariate_indicators (param_id, param_label, copyrights)
-values ('building_count', 'OSM Buildings', jsonb_build_array('© OpenStreetMap contributors https://www.openstreetmap.org/copyright'));
+insert into bivariate_indicators (param_id, param_label, copyrights, direction)
+values ('building_count', 'OSM Buildings', jsonb_build_array('© OpenStreetMap contributors https://www.openstreetmap.org/copyright'), jsonb_build_array(jsonb_build_array('bad'), jsonb_build_array('good')));
 
-insert into bivariate_indicators (param_id, param_label, copyrights)
-values ('highway_length', 'OSM Road Length', jsonb_build_array('© OpenStreetMap contributors https://www.openstreetmap.org/copyright'));
+insert into bivariate_indicators (param_id, param_label, copyrights, direction)
+values ('highway_length', 'OSM Road Length', jsonb_build_array('© OpenStreetMap contributors https://www.openstreetmap.org/copyright'), jsonb_build_array(jsonb_build_array('bad'), jsonb_build_array('good')));
 
-insert into bivariate_indicators (param_id, param_label, copyrights)
+insert into bivariate_indicators (param_id, param_label, copyrights, direction)
 values ('local_hours', 'OSM Mapping Hours by Local Mappers', jsonb_build_array('© Kontur https://kontur.io/',
-'© OpenStreetMap contributors https://www.openstreetmap.org/copyright'));
+'© OpenStreetMap contributors https://www.openstreetmap.org/copyright'), jsonb_build_array(jsonb_build_array('bad'), jsonb_build_array('good')));
 
-insert into bivariate_indicators (param_id, param_label, copyrights)
+insert into bivariate_indicators (param_id, param_label, copyrights, direction)
 values ('total_hours', 'OSM Mapping Hours by All Mappers', jsonb_build_array('© Kontur https://kontur.io/',
-'© OpenStreetMap contributors https://www.openstreetmap.org/copyright'));
+'© OpenStreetMap contributors https://www.openstreetmap.org/copyright'), jsonb_build_array(jsonb_build_array('bad'), jsonb_build_array('good')));
 
-insert into bivariate_indicators (param_id, param_label, copyrights)
+insert into bivariate_indicators (param_id, param_label, copyrights, direction)
 values ('gdp', 'Gross Domestic Product', jsonb_build_array(
 '© Kontur https://kontur.io/',
 '© 2019 The World Bank Group, CC-BY 4.0',
@@ -59,33 +57,35 @@ values ('gdp', 'Gross Domestic Product', jsonb_build_array(
                                  'Dataset: Schiavina, Marcello; Freire, Sergio; MacManus, Kytt (2019): GHS population grid multitemporal (1975, 1990, 2000, 2015) R2019A. European Commission, Joint Research Centre (JRC) DOI: 10.2905/42E8BE89-54FF-464E-BE7B-BF9E64DA5218 PID: http://data.europa.eu/89h/0c6b9751-a71f-4062-830b-43c9f432370f Concept & Methodology: Freire, Sergio; MacManus, Kytt; Pesaresi, Martino; Doxsey-Whitfield, Erin; Mills, Jane (2016): Development of new open and free multi-temporal global population grids at 250 m resolution. Geospatial Data in a Changing World; Association of Geographic Information Laboratories in Europe (AGILE). AGILE 2016',
                                  'Copernicus Global Land Service: Land Cover 100m: Marcel Buchhorn, Bruno Smets, Luc Bertels, Bert De Roo, Myroslava Lesiv, Nandin-Erdene Tsendbazar, … Steffen Fritz. (2020). Copernicus Global Land Service: Land Cover 100m: collection 3: epoch 2019: Globe (Version V3.0.1) Data set. Zenodo. http://doi.org/10.5281/zenodo.3939050',
                                  'Microsoft Buildings: Canada, Tanzania, Uganda, USA: This data is licensed by Microsoft under the Open Data Commons Open Database License (ODbL).',
-                                 '@ OpenStreetMap contributors https://www.openstreetmap.org/copyright'));
+                                 '@ OpenStreetMap contributors https://www.openstreetmap.org/copyright'),
+        jsonb_build_array(jsonb_build_array('bad'), jsonb_build_array('good')));
 
-insert into bivariate_indicators (param_id, param_label, copyrights)
+insert into bivariate_indicators (param_id, param_label, copyrights, direction)
 values ('population', 'Population', jsonb_build_array(
 '© Kontur https://kontur.io/',
         'Facebook Connectivity Lab and Center for International Earth Science Information Network - CIESIN - Columbia University. 2016. High Resolution Settlement Layer (HRSL). Source imagery for HRSL © 2016 DigitalGlobe. https://dataforgood.fb.com/tools/population-density-maps/',
         'Dataset: Schiavina, Marcello; Freire, Sergio; MacManus, Kytt (2019): GHS population grid multitemporal (1975, 1990, 2000, 2015) R2019A. European Commission, Joint Research Centre (JRC) DOI: 10.2905/42E8BE89-54FF-464E-BE7B-BF9E64DA5218 PID: http://data.europa.eu/89h/0c6b9751-a71f-4062-830b-43c9f432370f Concept & Methodology: Freire, Sergio; MacManus, Kytt; Pesaresi, Martino; Doxsey-Whitfield, Erin; Mills, Jane (2016): Development of new open and free multi-temporal global population grids at 250 m resolution. Geospatial Data in a Changing World; Association of Geographic Information Laboratories in Europe (AGILE). AGILE 2016',
         'Copernicus Global Land Service: Land Cover 100 m: Marcel Buchhorn, Bruno Smets, Luc Bertels, Bert De Roo, MyroslavaLesiv, Nandin - Erdene Tsendbazar, … Steffen Fritz. (2020). Copernicus Global Land Service: Land Cover 100m: collection 3: epoch 2019: Globe (Version V3.0.1) Data set. Zenodo. http://doi.org/10.5281/zenodo.3939050',
         'Microsoft Buildings: Canada, Tanzania, Uganda, USA: This data is licensed by Microsoft under the Open Data Commons Open Database License (ODbL).',
-        '© OpenStreetMap contributors https://www.openstreetmap.org/copyright'));
+        '© OpenStreetMap contributors https://www.openstreetmap.org/copyright'),
+        jsonb_build_array(jsonb_build_array('unimportant'), jsonb_build_array('important')));
 
-insert into bivariate_indicators (param_id, param_label, copyrights)
+insert into bivariate_indicators (param_id, param_label, copyrights, direction)
 values ('total_building_count', 'Total Buildings Estimate', jsonb_build_array(
 '© Kontur https://kontur.io/',
         'Copernicus Global Land Service: Land Cover 100 m: Marcel Buchhorn, Bruno Smets, Luc Bertels, Bert De Roo, MyroslavaLesiv, Nandin - Erdene Tsendbazar, … Steffen Fritz. (2020). Copernicus Global Land Service: Land Cover 100m: collection 3: epoch 2019: Globe (Version V3.0.1) Data set. Zenodo. http://doi.org/10.5281/zenodo.3939050',
         'Microsoft Buildings: Canada, Tanzania, Uganda, USA: This data is licensed by Microsoft under the Open Data Commons Open Database License (ODbL).',
-        '© OpenStreetMap contributors https://www.openstreetmap.org/copyright'));
+        '© OpenStreetMap contributors https://www.openstreetmap.org/copyright'),
+        jsonb_build_array(jsonb_build_array('unimportant'), jsonb_build_array('important')));
 
-insert into bivariate_indicators (param_id, param_label, copyrights)
+insert into bivariate_indicators (param_id, param_label, copyrights, direction)
 values ('wildfires', 'Wildfire Days Per Year', jsonb_build_array(
 '© NRT VIIRS 375 m Active Fire product VJ114IMGTDL_NRT. Available on-line [https://earthdata.nasa.gov/firms]. doi: 10.5067/FIRMS/VIIRS/VJ114IMGT_NRT.002',
     'NRT VIIRS 375 m Active Fire product VNP14IMGT. Available on-line [https://earthdata.nasa.gov/firms]. doi:10.5067/FIRMS/VIIRS/VNP14IMGT_NRT.002',
     'MODIS Collection 6 NRT Hotspot / Active Fire Detections MCD14DL. Available on-line [https://earthdata.nasa.gov/firms]. doi: 10.5067/FIRMS/MODIS/MCD14DL.NRT.006',
-    'MODIS Collection 6 NRT Hotspot / Active Fire Detections MCD14ML. Available on-line [https://earthdata.nasa.gov/firms]. doi: 10.5067/FIRMS/MODIS/MCD14ML'));
+    'MODIS Collection 6 NRT Hotspot / Active Fire Detections MCD14ML. Available on-line [https://earthdata.nasa.gov/firms]. doi: 10.5067/FIRMS/MODIS/MCD14ML'),
+    jsonb_build_array(jsonb_build_array('good', 'unimportant'), jsonb_build_array('bad', 'important')));
 
 update bivariate_indicators
-set
-    is_base = true
-where
-      param_id in ('population', 'total_building_count', 'gdp', 'area_km2', 'one');
+set is_base = true
+where param_id in ('population', 'total_building_count', 'gdp', 'area_km2', 'one');

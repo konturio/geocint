d5095784c412998510bb2036a1e1efdfbdb52c88
pashagin_p <paolo.pash@gmail.com>
2021-04-24 17:09:52 +0300

    fix errors in FIPS codes starting with 0

--- tables/covid19_us_confirmed_in.sql
+++ tables/covid19_us_confirmed_in.sql
@@ -1,8 +1,13 @@
 drop table if exists covid19_us_confirmed_in;
 create table covid19_us_confirmed_in as
-    select replace(fips, '.0', '') as fips,
-           max(date) as date,
-           max(value) as value,
-           combined_key,
-           'confirmed'::text as status
- from covid19_us_confirmed_csv_in group by 1, 4;
+(select
+        case
+            when length(fips) = 6
+            then '0'::text||(replace(fips, '.0', ''))::text
+            else replace(fips, '.0', '')
+        end as fips,
+        max(date) as date,
+        max(value) as value,
+        combined_key,
+        'confirmed'::text as status
+ from covid19_us_confirmed_csv_in group by 1, 4);
--- tables/covid19_us_deaths_in.sql
+++ tables/covid19_us_deaths_in.sql
@@ -1,8 +1,13 @@
 drop table if exists covid19_us_deaths_in;
 create table covid19_us_deaths_in as
-    select replace(fips, '.0', '') as fips,
-           max(date) as date,
-           max(value) as value,
-           combined_key,
-           'dead'::text as status
- from covid19_us_deaths_csv_in group by 1, 4;
+(select
+        case
+            when length(fips) = 6
+            then '0'::text||(replace(fips, '.0', ''))::text
+            else replace(fips, '.0', '')
+        end as fips,
+        max(date) as date,
+        max(value) as value,
+        combined_key,
+        'dead'::text as status
+ from covid19_us_deaths_csv_in group by 1, 4);

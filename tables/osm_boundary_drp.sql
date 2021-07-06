d4b72b2f511acb4a51d5f3988e7dbc74eae64597
pashagin_p <paolo.pash@gmail.com>
2021-07-06 18:23:47 +0300

    force check on null geometry

--- tables/osm_boundary_drp.sql
+++ tables/osm_boundary_drp.sql
@@ -6,4 +6,15 @@ set geom = ST_ConvexHull(b.geom)
 from osm_admin_boundaries b
 where r.osm_id = b.osm_id;
 
+-- force check on null geometry not to be null
+update drp_regions r
+set geom = ST_ConvexHull(b.geog::geometry)
+from osm b
+where b.tags ? 'admin_level'
+  and b.tags @> '{"boundary":"administrative"}'
+  and ST_Dimension(b.geog::geometry) = 2
+  and not (tags ->> 'name' is null and tags @> '{"admin_level":"2"}')
+  and r.geom is null
+  and r.osm_id = b.osm_id;
+
 create index if not exists drp_regions_geom_idx on drp_regions using gist(geom);

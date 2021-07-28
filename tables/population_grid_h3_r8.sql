3e44c0495e3b239b0aaf93dd62eb7bfbb477489a
ykyslomed <^jklop23i>
2021-07-28 18:00:00 +0300

    6344 - commented out WorldPop population dataset from pipeline

--- tables/population_grid_h3_r8.sql
+++ tables/population_grid_h3_r8.sql
@@ -1,16 +1,16 @@
--- comment out WorldPop population dataset
+-- commented out WorldPop population dataset
 drop table if exists population_grid_h3_r8;
 create table population_grid_h3_r8 as (
     select h3,
-           h3::geometry  as geom,
-           8             as resolution,
-           null::float   as population,
-           sum(ghs_pop)  as ghs_pop,
-           sum(hrsl_pop) as hrsl_pop,
-           sum(worldpop) as worldpop
+           h3::geometry               as geom,
+           8                          as resolution,
+           null::float                as population,
+           coalesce(sum(ghs_pop), 0)  as ghs_pop,
+           sum(hrsl_pop)              as hrsl_pop,
+           sum(worldpop)              as worldpop
     from (
              select h3,
-                    coalesce(population, 0)  as ghs_pop,
+                    population  as ghs_pop,
                     null::float as hrsl_pop,
                     null::float as worldpop
              from ghs_globe_population_grid_h3_r8

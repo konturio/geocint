d95c9fbb08df0a368d67d07ae94eee9970261b32
trylinka <tatyanakarvat@gmail.com>
2021-07-30 18:54:27 +0200

    4789: Fix column name pop_over_65_total for us_census_tracts_stats_h3 table

--- tables/us_census_tracts_stats_h3.sql
+++ tables/us_census_tracts_stats_h3.sql
@@ -1,13 +1,13 @@
 drop table if exists us_census_tracts_stats_h3;
 create table us_census_tracts_stats_h3 as (
-    select h3_polyfill((geom), 8)        as h3,
+    select h3_polyfill((geom), 8)      as h3,
            sum(pop_under_5_total)      as pop_under_5_total,
-           sum(pop_over_65_total)      as pop_under_65_total,
+           sum(pop_over_65_total)      as pop_over_65_total,
            sum(poverty_families_total) as poverty_families_total,
            sum(pop_disability_total)   as pop_disability_total,
            sum(pop_not_well_eng_speak) as pop_not_well_eng_speak,
            sum(pop_without_car)        as pop_without_car,
-           8::int                        as resolution
+           8::int                      as resolution
     from us_census_tracts_stats
     group by 1
 );

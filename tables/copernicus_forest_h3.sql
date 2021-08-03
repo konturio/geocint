42e23753c3bf1007cf8b06b0fce0f701410c9f1d
Yevhen Kyslomed <ykyslomed@kontur.io>
2021-08-03 10:27:50 +0000

    Apply 1 suggestion(s) to 1 file(s)

--- tables/copernicus_forest_h3.sql
+++ tables/copernicus_forest_h3.sql
@@ -65,41 +65,36 @@ create table copernicus_forest_h3
 do
 $$
     declare
-        row         record;
-        col_jsonb   jsonb;
-        carry_jsonb jsonb;
-        cur_row     record;
-        cur_out     float;
-        res         integer;
-        carry       int;
+        columns   text[];
+        res       integer;
+        cur_row   jsonb;
+        carry     jsonb;
+        carry_out jsonb;
     begin
+        columns = '{forest_area, evergreen_needle_leaved_forest, shrubs, herbage, unknown_forest}';
         res = 8;
         while res > 0
             loop
-                carry = 0;
-                for cur_row in (select * from copernicus_forest_h3_in where resolution = res order by h3)
+                select jsonb_object_agg(column_name, 0) from unnest(columns) "column_name" into carry;
+                for cur_row in (select to_jsonb(r) from copernicus_forest_h3_in r where resolution = res order by h3)
                     loop
-                        -- create jsonb '{"column": ["resolution", "herbage", "shrublands", "area_km2", "forest_area", "h3"]}'
-                        select jsonb_build_object('column', jsonb_agg(key))
-                        into col_jsonb
-                        from (select distinct (json_each_text(row_to_json(test_copernicus_forest_h3))).key
-                              from test_copernicus_forest_h3) f;
-                        -- loop through array at "column" values
-                        for row in (select jsonb_array_elements(col_jsonb -> 'column') column_name)
-                            loop
-                                -- calculate carry for every column
-                                carry = carry + cur_row.column_name;
-                                cur_out = least(carry, cur_row.area_km2);
-                                carry = carry - cur_out;
-                                -- put carry into carry_jsonb and cur_out into jsonb too to insert their values into insert below
-                                if cur_out > 0 then
-                                    insert into copernicus_forest_h3 (h3, resolution, forest_area, area_km2)
-                                    values (cur_row.h3, cur_row.resolution, cur_out, cur_row.area_km2);
-                                end if;
-                            end loop;
-                        raise notice 'unprocessed carry %', carry;
-                        res = res - 1;
+                        select jsonb_object_agg(c.key, carry_value - carry_out_value),
+                               jsonb_object_agg(c.key, carry_out_value)
+                        from jsonb_each(carry) c,
+                             jsonb_each(cur_row) r,
+                             lateral (select c.value::float + r.value::float "carry_value") "carry_value",
+                             least(carry_value::float, (cur_row -> 'area_km2')::float) "carry_out_value"
+                        where c.key = r.key
+                        into carry, carry_out;
+
+                        if jsonb_path_exists(carry_out, '$.** ? (@ > 0)') then
+                            insert into copernicus_forest_h3
+                            select *
+                            from jsonb_populate_record(null::copernicus_forest_h3, cur_row || carry_out);
+                        end if;
                     end loop;
+                raise notice 'unprocessed carry %', carry;
+                res = res - 1;
             end loop;
     end;
 $$;

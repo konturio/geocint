469cb08221b9ac6705a40e0b310a7ea09d83d6b5
ykyslomed <^jklop23i>
2021-08-06 13:31:08 +0300

    2172 - reformat

--- tables/population_check_osm.sql
+++ tables/population_check_osm.sql
@@ -1,20 +1,21 @@
 drop table if exists population_check_osm;
 create table population_check_osm as (
     select osm_id,
-           b.tags ->> 'ISO3166-1:alpha3'                       "iso",
+           b.tags ->> 'ISO3166-1:alpha3'       "iso",
            name,
-           tags ->> 'name:en'                                  "name_en",
-           tags ->> 'population:date'                          "pop_date",
+           tags ->> 'name:en'                  "name_en",
+           tags ->> 'population:date'          "pop_date",
            osm_pop,
-           b.population                                          "kontur_pop",
+           b.population                        "kontur_pop",
            diff_pop,
-           diff_pop / (b.population + osm_pop)                   "index",
-           exists(select
-                  from hrsl_population_boundary h
-                  where h.iso = b.tags ->> 'ISO3166-1:alpha3') "has_hrsl_coverage"
+           diff_pop / (b.population + osm_pop) "index",
+           exists(
+                   select
+                   from hrsl_population_boundary h
+                   where h.iso = b.tags ->> 'ISO3166-1:alpha3'
+               )                               "has_hrsl_coverage"
     from kontur_boundaries b,
          parse_float(tags ->> 'population') osm_pop,
---          coalesce(b.population, 0) kontur_pop,
          abs(b.population - osm_pop) "diff_pop",
          ST_Area(geom::geography) "area"
     where osm_pop > 0

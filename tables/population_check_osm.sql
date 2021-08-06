15cf5a943ed711cad32b7203d36e07f58b6cea2b
ykyslomed <^jklop23i>
2021-08-06 12:50:21 +0300

    2172 - add population_date and comment

--- tables/population_check_osm.sql
+++ tables/population_check_osm.sql
@@ -1,18 +1,25 @@
 drop table if exists population_check_osm;
 create table population_check_osm as (
     select osm_id,
+           b.tags ->> 'ISO3166-1:alpha3'                       "iso",
            name,
-           kontur_pop                        "kontur_pop",
+           tags ->> 'name:en'                                  "name_en",
+           tags ->> 'population:date'                          "pop_date",
            osm_pop,
+           kontur_pop                                          "kontur_pop",
            diff_pop,
-           diff_pop / (kontur_pop + osm_pop) "index"
+           diff_pop / (kontur_pop + osm_pop)                   "index",
+           exists(select
+                  from hrsl_population_boundary h
+                  where h.iso = b.tags ->> 'ISO3166-1:alpha3') "has_hrsl_coverage"
     from kontur_boundaries b,
          parse_float(tags ->> 'population') osm_pop,
          coalesce(b.population, 0) kontur_pop,
          abs(kontur_pop - osm_pop) "diff_pop",
          ST_Area(geom::geography) "area"
     where osm_pop > 0
-      and gadm_level = 0
-      and area > 5161293.2 -- average hexagon area at 7 resolution
+      and admin_level = '2'
+      -- exclude boundaries with a small area (for example, the Vatican City)
+      and area > 737327.6 -- average hexagon area at 8 resolution
     order by "index" desc
 );
--- tables/population_check_un.sql
+++ tables/population_check_un.sql
@@ -9,21 +9,23 @@ create table population_check_un as (
              iso_codes i
         where u.iso = i.iso_num
           and u.variant_id = 2
-          and u.year = 2020
+          and u.year = date_part('year', current_date)
         order by 1)
     select un.code,
            un.country,
-           un.year                            "un_year",
-           un.pop                             "un_population",
-           k.population                       "kontur_population",
+           un.year                                                              "un_year",
+           un.pop                                                               "un_population",
+           k.population                                                         "kontur_population",
            diff_pop,
-           diff_pop / (un.pop + k.population) "index"
+           diff_pop / (un.pop + k.population)                                   "index",
+           exists(select from hrsl_population_boundary h where h.iso = un.code) "has_hrsl_coverage"
     from un,
          kontur_boundaries k,
          abs(un.pop - k.population) "diff_pop",
          ST_Area(geom::geography) "area"
     where k.gadm_level = 0
       and un.code = k.tags ->> 'ISO3166-1:alpha3'
-      and area > 5161293.2 -- average hexagon area at 7 resolution
-    order by index desc
+      -- exclude boundaries with a small area (for example, the Vatican City)
+      and area > 737327.6 -- average hexagon area at 8 resolution
+    order by diff_pop / (un.pop + k.population) desc
 );

1240374e8a6d91c52dddb209abfc120ce6518a81
trylinka <tatyanakarvat@gmail.com>
2021-08-11 13:28:32 +0200

    6735: Fix spreading of vaccine values across US counties

--- tables/covid19_vaccine_accept_us_counties_h3.sql
+++ tables/covid19_vaccine_accept_us_counties_h3.sql
@@ -1,28 +1,24 @@
+drop table if exists covid19_vaccine_accept_us_counties_h3_in;
+create table covid19_vaccine_accept_us_counties_h3_in as (
+    select distinct on (h3_polyfill((geom), 8)) h3_polyfill((geom), 8) as h3,
+           h3_to_geometry(h3_polyfill(geom, 8))                        as geom,
+           fips_code
+    from covid19_vaccine_accept_us_counties
+    order by 1
+);
+
+create index on covid19_vaccine_accept_us_counties_h3_in using gist(geom);
+
 drop table if exists covid19_vaccine_accept_us_counties_h3;
 create table covid19_vaccine_accept_us_counties_h3 as (
-    select h3_polyfill(geom, 8) as h3,
-           8                    as resolution,
-           sum(vaccine_value)   as vaccine_value
-    from covid19_vaccine_accept_us_counties
-    group by 1);
-
-alter table covid19_vaccine_accept_us_counties_h3
-    set (parallel_workers = 32);
-
-do
-$$
-    declare
-        res integer;
-    begin
-        res = 8;
-        while res > 0
-            loop
-                insert into covid19_vaccine_accept_us_counties_h3 (h3, resolution, vaccine_value)
-                select h3_to_parent(h3) as h3, (res - 1) as resolution, sum(vaccine_value) as vaccine_value
-                from covid19_vaccine_accept_us_counties_h3
-                where resolution = res
-                group by 1;
-                res = res - 1;
-            end loop;
-    end;
-$$;
+    select h3_polyfill((geom), 8)              as h3,
+           vaccine_value / h3_count            as vaccine_value,
+           8::int                              as resolution
+    from covid19_vaccine_accept_us_counties u,
+    lateral (select u.fips_code, count(h3) as h3_count
+             from covid19_vaccine_accept_us_counties_h3_in h
+             where ST_Intersects(h.geom, u.geom)
+             and h.geom && u.geom
+    group by 1) x
+    where x.fips_code = u.fips_code
+);

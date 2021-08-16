3ab4d8c673757b3a4ef15e04e121b19a657704e6
ykyslomed <^jklop23i>
2021-08-16 13:25:15 +0300

    6743 - rewrite query and remove area

--- tables/abu_dhabi_bivariate_pop_shops.sql
+++ tables/abu_dhabi_bivariate_pop_shops.sql
@@ -1,44 +1,30 @@
 drop table if exists abu_dhabi_bivariate_pop_shops;
 create table abu_dhabi_bivariate_pop_shops as (
-    with abu_dhabi_pop_shops as (
-        select h3,
-               coalesce(sum(shops), 0)      "shops",
-               coalesce(sum(population), 0) "population"
-        from (
-                 select h3_geo_to_h3(o.geog::geometry::box::point, 8) "h3",
-                        count(o)                                      "shops",
-                        null::double precision                        "population"
-                 from osm o,
-                      abu_dhabi_admin_boundaries b
-                 where ST_Intersects(b.geom, o.geog::geometry)
-                   and (
-                         (
-                                     o.tags ? 'amenity'
-                                 and o.tags ->> 'amenity' in
-                                     ('bar', 'biergarten', 'cafe', 'fast_food', 'food_court', 'ice_cream', 'pub',
-                                      'restaurant')
-                             ) or (
-                                     o.tags ? 'shop'
-                                 and o.tags ->> 'shop' in
-                                     ('convenience', 'general', 'greengrocer', 'kiosk', 'supermarket')
-                             )
-                     )
-                 group by 1
-                 union all
-                 select h3,
-                        null::integer "shops",
-                        population
-                 from kontur_population_h3 k,
-                      abu_dhabi_admin_boundaries b
-                 where resolution = 8
-                   and ST_Intersects(k.geom, ST_Transform(b.geom, 3857))
-                 order by h3
-             ) "z"
-        group by h3
+    with abu_dhabi_shops as (
+        select h3_geo_to_h3(o.geog::geometry::box::point, 8) "h3",
+               count(o)                                      "shops"
+        from osm o,
+             abu_dhabi_admin_boundaries b
+        where ST_Intersects(b.geom, o.geog::geometry)
+          and (
+                (
+                            o.tags ? 'amenity'
+                        and o.tags ->> 'amenity' in
+                            ('bar', 'biergarten', 'cafe', 'fast_food', 'food_court', 'ice_cream', 'pub',
+                             'restaurant')
+                    ) or (
+                            o.tags ? 'shop'
+                        and o.tags ->> 'shop' in
+                            ('convenience', 'general', 'greengrocer', 'kiosk', 'supermarket')
+                    )
+            )
+        group by 1
     )
-    select h3,
-           shops,
-           population,
-           chr(64 + ntile(3) over (order by shops)) || ntile(3) over (order by population) "bivariate_cell"
-    from abu_dhabi_pop_shops
+    select s.h3,
+           s.shops,
+           pop                                                                        "population",
+           chr(64 + ntile(3) over (order by s.shops)) || ntile(3) over (order by pop) "bivariate_cell"
+    from abu_dhabi_shops s
+             left outer join kontur_population_h3 k on (k.resolution = 8 and s.h3 = k.h3),
+         coalesce(k.population, 0) pop
 );

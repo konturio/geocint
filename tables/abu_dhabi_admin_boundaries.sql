692c035fe0e64a344080d3e938745e5576b44a07
ykyslomed <^jklop23i>
2021-08-16 21:38:14 +0300

    6743 - use ST_PointOnSurface instead of intersected area ratio

--- tables/abu_dhabi_admin_boundaries.sql
+++ tables/abu_dhabi_admin_boundaries.sql
@@ -7,7 +7,5 @@ create table abu_dhabi_admin_boundaries as (
     where o.tags @> '{"admin_level": "5"}' -- to use the index
       and o.osm_id = 4479763               -- osm_id of Abu-Dhabi boundary
       and o.osm_type = 'relation'          -- additional check of boundary type
-      and ST_Intersects(o.geog::geometry, g.geom)
-      and ST_Area(ST_Intersection(o.geog::geometry, g.geom)) / ST_Area(g.geom) > 0.5
+      and ST_Intersects(o.geog::geometry, ST_PointOnSurface(g.geom))
 );
-

82e37020ea333c9049d35fd654ed4b9c601771f0
Darafei Praliaskouski <me@komzpa.net>
2021-08-20 22:35:10 +0300

    6306 force parallel scan by making table contain more pages

--- tables/bivariate_indicators.sql
+++ tables/bivariate_indicators.sql
@@ -246,3 +246,15 @@ values ('days_maxwetbulb_over_32c_2c', 'Days above 32C wet-bulb, potential(2C)',
 update bivariate_indicators
 set is_base = true
 where param_id in ('population', 'total_building_count', 'area_km2', 'populated_area_km2','one');
+
+
+--- this is an ugly hack to enable Parallel Seq Scan on bivariate_indicators
+-- Postgres parallel seq scan works on page level, so we can't really get it to run more workers than there are
+-- pages in source table, so we make sure that the pages are filled in as sparsely as possible.
+alter table bivariate_indicators set (fillfactor = 10);
+alter table bivariate_indicators add column baloon text;
+alter table bivariate_indicators alter column baloon set storage external;
+alter table bivariate_indicators alter column copyrights set storage external;
+update bivariate_indicators set baloon = repeat(' ', 3000);
+vacuum full bivariate_indicators;
+alter table bivariate_indicators drop column baloon;

b6116709144a20dd2ff6d5fa9ac25067da3c5388
ykyslomed <ykyslomed@kontur.io>
2021-09-13 03:26:34 +0300

    6692 - build population density score

---
+++ tables/abu_dhabi_pds_bicycle_10min.sql
@@ -0,0 +1,9 @@
+drop table if exists abu_dhabi_pds_bicycle_10min;
+create table abu_dhabi_pds_bicycle_10min as (
+    select p1.id, p1.population, sum(p2.population) "pds", p1.geom
+    from abu_dhabi_buildings_population p1,
+         abu_dhabi_bicycle_isochrones isochrone,
+         abu_dhabi_buildings_population p2
+    where p1.id = isochrone.id
+    and ST_Intersects(isochrone.geom, p2.geom)
+);

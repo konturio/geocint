85f472666b380bcc60c7236d5b161eab900c064a
ykyslomed <ykyslomed@kontur.io>
2021-10-15 01:51:58 +0300

    6833 - fix hrsl_population_boundary

--- tables/hrsl_population_boundary.sql
+++ tables/hrsl_population_boundary.sql
@@ -1,61 +1,63 @@
 drop table if exists hrsl_population_boundary;
-
 create table hrsl_population_boundary as (
     with countries as (
-        select gid,
-               gid_0                   "iso",
-               name_0                  "name",
-               perimeter               "perimeter",
-               area                    "area",
-               ST_Subdivide(geom_4326) "geom"
-        from gadm_countries_boundary,
-             ST_Transform(geom, 4326) "geom_4326",
-             ST_Perimeter(geom_4326) "perimeter",
-             ST_Area(geom_4326) "area"
+        select tags ->> 'ISO3166-1:alpha3' "iso",
+               name,
+               ST_Area(geom)               "area",
+               geom                        "geom"
+        from kontur_boundaries
+        where admin_level = '2'
     ),
-         covered_by_rasters as (
-             select c.iso,
-                    c.name,
-                 /*
-                 "exacly_covered" is designed to improve query speed. if the number of intersected rasters with country
-                 multiplied by the minimum raster size (width or height) is greater than the perimeter of the entire
-                 country - the country fully contains at least one raster. but we do the raster count - 1 to exclude
-                 countries that are fully contained by the raster
-                 */
-                    c.perimeter < (count(r) - 1) * least(
-                            min(ST_PixelWidth(r.rast) * ST_Width(r.rast)),
-                            min(ST_PixelHeight(r.rast) * ST_Height(r.rast))
-                        ) "exacly_covered"
-             from countries c,
-                  hrsl_population_raster r
+         subdivided_country as (
+             select iso, name, area, ST_Subdivide(geom) "geom"
+             from countries
+         ),
+         subdivided_boundary as (
+             select ST_Subdivide(ST_Boundary(geom)) "geom"
+             from countries
+         ),
+         boundary_rasters as (
+             select distinct on (r.rid) r.rid, r.rast
+             from hrsl_population_raster r,
+                  subdivided_boundary c
              where ST_Intersects(c.geom, ST_ConvexHull(r.rast))
-             group by c.iso, c.name, c.perimeter
+             order by r.rid
+         ),
+         rasters_in_countries as (
+             select distinct on (c.iso, r.rid) c.iso, c.area, r.rid, r.rast
+             from subdivided_country c,
+                  hrsl_population_raster r
+             where ST_Intersects(ST_ConvexHull(r.rast), c.geom)
+             order by c.iso, r.rid
+         ),
+         covered_by_rasters as (
+             select r.iso, sum(ST_Area(ST_ConvexHull(r.rast))) / area "coverage"
+             from rasters_in_countries r
+             where r.rid not in (select b.rid from boundary_rasters b)
+             group by r.iso, r.area
          ),
          covered_by_pixels as (
-             select c.iso,
-                    c.name,
-                    sum((select count(p)
-                         from ST_PixelAsCentroids(r.rast, 1) p
-                         where ST_Intersects(c.geom, p.geom)))                            "pixels_within",
-                    sum(ST_Area(ST_Intersection(ST_ConvexHull(r.rast), c.geom))) / c.area "coverage"
-             from countries c,
-                  hrsl_population_raster r
-             where ST_Intersects(c.geom, ST_ConvexHull(r.rast))
-               and c.iso in (select cr.iso from covered_by_rasters cr where not cr.exacly_covered)
-             group by c.iso, c.name, c.area
+             select r.iso,
+                    sum(ST_Area(p.geom)) / r.area "coverage"
+             from rasters_in_countries r,
+                  ST_PixelAsPolygons(r.rast) p,
+                  countries c
+             where r.iso not in (select iso from covered_by_rasters where coverage > 0.01)
+               and r.iso = c.iso
+               and ST_Intersects(c.geom, ST_Centroid(p.geom))
+             group by r.iso, r.area
          )
-    select gid, iso, name, ST_Transform(geom, 3857) "geom"
-    from countries
-    where iso in (
+    select iso, name, geom
+    from subdivided_country c
+    where c.iso in (
         select iso
         from covered_by_rasters
-        where exacly_covered
+        where coverage > 0.01
         union all
         select iso
         from covered_by_pixels
-        where pixels_within > 10 -- excludes the Vatican, which is 100% covered and contains only 3 pixels
-          and coverage > 0.1
+        where coverage > 0.01
     )
 );
 
-create index hrsl_population_boundary_geom_idx on hrsl_population_boundary using gist (geom);
+create index on hrsl_population_boundary using gist (geom);

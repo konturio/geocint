c1a82df0e2a560e98a27c8693cba5d90b6ea25f2
ykyslomed <ykyslomed@kontur.io>
2021-10-21 16:05:45 +0300

    7591 - rename abu_dhabi_buildings_pds to abu_dhabi_buildings_population

--- tables/abu_dhabi_buildings_population.sql
+++ tables/abu_dhabi_buildings_population.sql
@@ -1,5 +1,5 @@
-drop table if exists abu_dhabi_buildings_pds;
-create table abu_dhabi_buildings_pds as (
+drop table if exists abu_dhabi_buildings_population;
+create table abu_dhabi_buildings_population as (
     with buildings_volume as (
         select b.id,
                b.building_height,
@@ -10,11 +10,11 @@ create table abu_dhabi_buildings_pds as (
     select b.id,
            b.building_height                             "height",
            round(b.volume)::integer                      "volume",
-           round(b.volume / v.volume * 1511768)::integer "pds", -- The number 1511768 is the estimated population of Abu Dhabi in 2021.
+           round(b.volume / v.volume * 1511768)::integer "population", -- The number 1511768 is the estimated population of Abu Dhabi in 2021.
            ST_Transform(b.geom, 3857)                    "geom"
     from buildings_volume b,
          (select sum(volume) "volume" from buildings_volume) v
     order by b.id
 );
 
-create index on abu_dhabi_buildings_pds using gist (geom);
+create index on abu_dhabi_buildings_population using gist (geom);
--- tables/abu_dhabi_pds_bicycle_10min.sql
+++ tables/abu_dhabi_pds_bicycle_10min.sql
@@ -1,9 +1,13 @@
 drop table if exists abu_dhabi_pds_bicycle_10min;
 create table abu_dhabi_pds_bicycle_10min as (
-    select p1.id, p1.height, sum(p2.volume) "volume", sum(p2.pds) "pds", ST_Transform(p1.geom, 4326) "geom"
-    from abu_dhabi_buildings_pds p1,
+    select p1.id,
+           p1.height,
+           sum(p2.volume)              "volume",
+           sum(p2.population)          "pds",
+           ST_Transform(p1.geom, 4326) "geom"
+    from abu_dhabi_buildings_population p1,
          abu_dhabi_isochrones_bicycle_10m isochrone,
-         abu_dhabi_buildings_pds p2
+         abu_dhabi_buildings_population p2
     where p1.id = isochrone.building_id
       and ST_Intersects(isochrone.geom, p2.geom)
     group by p1.id, p1.height, p1.geom

4869d4c63f3f2f5e2a3d21b8bb7c227b359d02ea
ykyslomed <ykyslomed@kontur.io>
2021-10-27 13:25:21 +0300

    fix HRSL boundary usage in population count

--- tables/population_grid_h3_r8.sql
+++ tables/population_grid_h3_r8.sql
@@ -1,13 +1,13 @@
 -- commented out WorldPop population dataset
-drop table if exists population_grid_h3_r8;
-create table population_grid_h3_r8 as (
+drop table if exists population_grid_h3_r8_in;
+create table population_grid_h3_r8_in as (
     select h3,
-           h3::geometry               as geom,
-           8                          as resolution,
-           null::float                as population,
-           coalesce(sum(ghs_pop), 0)  as ghs_pop,
-           sum(hrsl_pop)              as hrsl_pop,
-           sum(worldpop)              as worldpop
+           h3::geometry              as geom,
+           8                         as resolution,
+           null::float               as population,
+           coalesce(sum(ghs_pop), 0) as ghs_pop,
+           sum(hrsl_pop)             as hrsl_pop,
+           sum(worldpop)             as worldpop
     from (
              select h3,
                     population  as ghs_pop,
@@ -30,14 +30,24 @@ create table population_grid_h3_r8 as (
     group by 1
 );
 
-create index population_grid_h3_r8_geom_hrsl_pop_worldpop_idx on population_grid_h3_r8 using gist (geom, hrsl_pop, worldpop);
+create index on population_grid_h3_r8_in using gist (geom, hrsl_pop, worldpop);
 
-update population_grid_h3_r8 p
-set hrsl_pop = 0
-from hrsl_population_boundary b
-where ST_Intersects(ST_Transform(b.geom, 4326), p.geom)
-  and hrsl_pop is null;
-vacuum population_grid_h3_r8;
+drop table if exists population_grid_h3_r8;
+create table population_grid_h3_r8 as (
+    select h3,
+           p.geom,
+           resolution,
+           population,
+           ghs_pop,
+           case when b.geom is not null then coalesce(p.hrsl_pop, 0) end as hrsl_pop,
+           worldpop
+    from population_grid_h3_r8_in p
+             left outer join
+         hrsl_population_boundary b
+         on ST_Intersects(b.geom, p.geom)
+);
+
+drop table population_grid_h3_r8_in;
 
 -- update population_grid_h3_r8 p
 -- set worldpop = 0
@@ -45,8 +55,6 @@ vacuum population_grid_h3_r8;
 -- where ST_Intersects(ST_Transform(b.geom, 4326), p.geom)
 --   and worldpop is null;
 
-drop index if exists population_grid_h3_r8_geom_hrsl_pop_worldpop_idx;
-vacuum population_grid_h3_r8;
 update population_grid_h3_r8 p
 -- set population counts starting with more high resolution raster data (30m, 100m and whole planet)
 -- IMPORTANT: worldpop removed from coalesce

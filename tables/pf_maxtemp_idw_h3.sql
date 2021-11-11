15ce7097e5612a9f0e654e981ac7a07f801eec42
Pavel Pashagin <paolo.pash@gmail.com>
2021-11-11 17:15:20 +0000

    7824-dn-pf-layers-error-in-algorythm

--- tables/pf_maxtemp_idw_h3.sql
+++ tables/pf_maxtemp_idw_h3.sql
@@ -20,65 +20,68 @@ drop table if exists pf_maxtemp_h3_r4;
 create table pf_maxtemp_h3_r4 as (select distinct h3_geo_to_h3(geom, 4) as h3
                                   from pf_maxtemp_all);
 
--- generate H3 level 8 grid with data from original source
-drop table if exists pf_maxtemp_h3_r8;
-create table pf_maxtemp_h3_r8 as (select distinct h3_to_children(h3, 8)           as h3,
-                                                  h3_to_children(h3, 8)::geometry as geom
+-- generate H3 level 7 grid with data from original source. h8 takes more than 8 hours
+drop table if exists pf_maxtemp_h3_r7;
+create table pf_maxtemp_h3_r7 as (select distinct h3_to_children(h3, 7)           as h3,
+                                                  h3_to_children(h3, 7)::geometry as geom
                                   from pf_maxtemp_h3_r4);
-create index on pf_maxtemp_h3_r8 using gist (geom);
+create index on pf_maxtemp_h3_r7 using gist (geom);
 
 
 drop table if exists pf_maxtemp_idw_h3;
 create table pf_maxtemp_idw_h3
-(
-    h3                          h3index,
-    resolution                  float,
-    days_maxtemp_over_32c_1c    float,
-    days_maxtemp_over_32c_2c    float,
-    days_mintemp_above_25c_1c   float,
-    days_mintemp_above_25c_2c   float,
-    days_maxwetbulb_over_32c_1c float,
-    days_maxwetbulb_over_32c_2c float
-);
-
-
+as
 with nearest_points as (
     select h3,
            dist,
-           days_maxtemp_over_32c_1c    as z1,
-           days_maxtemp_over_32c_2c    as z2,
-           days_mintemp_above_25c_1c   as z3,
-           days_mintemp_above_25c_2c   as z4,
-           days_maxwetbulb_over_32c_1c as z5,
-           days_maxwetbulb_over_32c_2c as z6
-    from pf_maxtemp_all
-             CROSS JOIN LATERAL (
-        SELECT h3,
-               pf_maxtemp_h3_r8.geom                                   as h3_geom,
-               ST_Distance(pf_maxtemp_h3_r8.geom::geography, pf_maxtemp_all.geom::geography) AS dist
-        FROM pf_maxtemp_h3_r8
-        ORDER BY pf_maxtemp_all.geom <-> pf_maxtemp_h3_r8.geom
-        LIMIT 4
+           days_maxtemp_over_32c_1c,
+           days_maxtemp_over_32c_2c,
+           days_mintemp_above_25c_1c,
+           days_mintemp_above_25c_2c,
+           days_maxwetbulb_over_32c_1c,
+           days_maxwetbulb_over_32c_2c
+    from pf_maxtemp_h3_r7
+             cross join lateral (
+        select days_maxtemp_over_32c_1c,
+               days_maxtemp_over_32c_2c,
+               days_mintemp_above_25c_1c,
+               days_mintemp_above_25c_2c,
+               days_maxwetbulb_over_32c_1c,
+               days_maxwetbulb_over_32c_2c,
+               ST_Distance(pf_maxtemp_h3_r7.geom::geography, pf_maxtemp_all.geom::geography) AS dist
+        from pf_maxtemp_all
+        order by pf_maxtemp_all.geom <-> pf_maxtemp_h3_r7.geom
+        limit 4
         ) grid)
-insert
-into pf_maxtemp_idw_h3 (h3, resolution,
-                        days_maxtemp_over_32c_1c, days_maxtemp_over_32c_2c,
-                        days_mintemp_above_25c_1c, days_mintemp_above_25c_2c,
-                        days_maxwetbulb_over_32c_1c, days_maxwetbulb_over_32c_2c)
+
 select h3,
-       8::integer,
-       (SUM(z1 / dist) / SUM(1 / dist)) as d32_1,
-       (SUM(z2 / dist) / SUM(1 / dist)) as d32_2,
-       (SUM(z3 / dist) / SUM(1 / dist)) as n25_1,
-       (SUM(z4 / dist) / SUM(1 / dist)) as n25_2,
-       (SUM(z5 / dist) / SUM(1 / dist)) as b32_1,
-       (SUM(z6 / dist) / SUM(1 / dist)) as b32_2
+       7::integer as resolution,
+       (SUM(days_maxtemp_over_32c_1c / dist) / SUM(1 / dist)) as days_maxtemp_over_32c_1c,
+       (SUM(days_maxtemp_over_32c_2c / dist) / SUM(1 / dist)) as days_maxtemp_over_32c_2c,
+       (SUM(days_mintemp_above_25c_1c / dist) / SUM(1 / dist)) as days_mintemp_above_25c_1c,
+       (SUM(days_mintemp_above_25c_2c / dist) / SUM(1 / dist)) as days_mintemp_above_25c_2c,
+       (SUM(days_maxwetbulb_over_32c_1c / dist) / SUM(1 / dist)) as days_maxwetbulb_over_32c_1c,
+       (SUM(days_maxwetbulb_over_32c_2c / dist) / SUM(1 / dist)) as days_maxwetbulb_over_32c_2c
 from nearest_points
 group by h3;
 
-drop table if exists pf_maxtemp_h3_r8;
+drop table if exists pf_maxtemp_h3_r7;
 
-call generate_overviews('pf_maxtemp_idw_h3', '{days_maxtemp_over_32c_1c, days_maxtemp_over_32c_2c, days_mintemp_above_25c_1c, days_mintemp_above_25c_2c, days_maxwetbulb_over_32c_1c, days_maxwetbulb_over_32c_2c}'::text[], '{avg, avg, avg, avg, avg, avg}'::text[], 8);
+insert into pf_maxtemp_idw_h3 (h3, resolution,
+                               days_maxtemp_over_32c_1c, days_maxtemp_over_32c_2c,
+                               days_mintemp_above_25c_1c, days_mintemp_above_25c_2c,
+                               days_maxwetbulb_over_32c_1c, days_maxwetbulb_over_32c_2c)
+select h3_to_children(h3, 8) as h3,
+       8::integer            as resolution,
+       days_maxtemp_over_32c_1c,
+       days_maxtemp_over_32c_2c,
+       days_mintemp_above_25c_1c,
+       days_mintemp_above_25c_2c,
+       days_maxwetbulb_over_32c_1c,
+       days_maxwetbulb_over_32c_2c
+from pf_maxtemp_idw_h3;
+
+call generate_overviews('pf_maxtemp_idw_h3', '{days_maxtemp_over_32c_1c, days_maxtemp_over_32c_2c, days_mintemp_above_25c_1c, days_mintemp_above_25c_2c, days_maxwetbulb_over_32c_1c, days_maxwetbulb_over_32c_2c}'::text[], '{avg, avg, avg, avg, avg, avg}'::text[], 7);
 
 -- The output table contains a lot of interpolated values that are all-zero.
 -- Trim them out, we don't need to output cells for them.

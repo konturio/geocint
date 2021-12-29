962713b6e4acbfdea6ca8627a3e0a8a4e7abc194
Aleksandr Petrov <felispimeja@gmail.com>
2021-12-30 02:54:09 +0300

    Redesign gadm_boundaries table + create its deduplicated copy

--- tables/gadm_boundaries.sql
+++ tables/gadm_boundaries.sql
@@ -2,11 +2,22 @@
 drop table if exists gadm_boundaries;
 create table gadm_boundaries as
 select
-       (row_number() over())::int id, -- Add unique id column
-       gid_0::text,                   -- Level_0 GADM id
-       gid_1,                         -- Level_1 GADM id
-       gid_2,                         -- Level_2 GADM id
-       gid_3,                         -- Level_3 GADM id
+       (row_number() over())::int id,           -- Add unique id column
+       gid_0::text,                             -- Level_0 GADM id
+       gid_1,                                   -- Level_1 GADM id
+       gid_2,                                   -- Level_2 GADM id
+       gid_3,                                   -- Level_3 GADM id
+       case
+                when gadm_level = 0 then gid_0
+                when gadm_level = 1 then gid_1
+                when gadm_level = 2 then gid_2
+                when gadm_level = 3 then gid_3
+       end gid,                                 -- Feature's lowest level gid
+       case
+                when gadm_level = 1 then gid_0
+                when gadm_level = 2 then gid_1
+                when gadm_level = 3 then gid_2
+       end parent_gid,                           -- Feature's lowest level parent gid
        gadm_level,
        hasc::text,
        name::text,
@@ -17,7 +28,7 @@ from (
                 null::text  gid_2,
                 null::text  gid_3,
                 0::smallint gadm_level,
-                null::text  hasc,      -- GADM level_0 doesn't have HASC codes
+                null::text  hasc,                -- GADM level_0 doesn't have HASC codes
                 name_0      "name",
                 geom
          from gadm_level_0
@@ -53,8 +64,32 @@ from (
          from gadm_level_3
      ) a;
 
+-- Create indexes
+create index on gadm_boundaries (gid);
+create index on gadm_boundaries (parent_gid);
 create index on gadm_boundaries using gist (geom);
 
+
+-- Create deduplicated copy of GADM dataset
+drop table if exists gadm_deduplicated;
+create table gadm_deduplicated as
+select *
+from gadm_boundaries
+where parent_gid is null
+        or parent_gid not in (
+                -- Find duplicated features in GADM:
+                select g1.gid
+                from gadm_boundaries      g1
+                left join gadm_boundaries g2
+                        on g1.gid = g2.parent_gid
+                where g1.gadm_level < 3
+                group by g1.gid
+                having count(g2.*) = 1
+        );
+create index on gadm_deduplicated using gist(geom);
+
+
+-- Drop temporary tables
 drop table if exists gadm_level_0;
 drop table if exists gadm_level_1;
 drop table if exists gadm_level_2;

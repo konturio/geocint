b205076f93787403621a959e2df1216f9ddf6118
Rob Scobie <robert@kontur.io>
2022-01-17 10:10:56 -1000

    unified the case size in script

--- tables/facebookroads_cleaned.sql
+++ tables/facebookroads_cleaned.sql
@@ -1,16 +1,16 @@
-DROP TABLE IF EXISTS facebookroads_cleaned;
+drop table if exists facebookroads_cleaned;
 create table facebookroads_cleaned as
 select fr.*
-FROM facebook_roads fr --select facebookroads
-LEFT join lateral (
-	select st_buffer(st_collect(osm.geom)::geography, 10)::geometry geom
+from facebook_roads fr --select facebookroads
+left join lateral (
+	select st_buffer(ST_collect(osm.geom)::geography, 10)::geometry geom --selects geometries into a collection based on buffer size 10
 	from osm_roads osm
 	where ST_DWithin (fr.geom::geography, osm.geom::geography, 10)
 ) osm on true
 where st_length(st_intersection(fr.geom, osm.geom)::geography) < 0.5 * st_length(fr.geom::geography) --cacluate the length of intersection and make sure more than 50% is intersecting
 union all 
 select fr.*
-FROM facebook_roads fr
-LEFT JOIN osm_roads osm
-ON ST_DWithin (fr.geom::geography, osm.geom::geography, 10)
+from facebook_roads fr
+left join osm_roads osm
+on ST_DWithin (fr.geom::geography, osm.geom::geography, 10)
 where osm.osm_id is null;

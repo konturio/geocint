a929e7e2dee7ca7a4fc3b0a7132904f82bbfea24
Rob Scobie <robert@kontur.io>
2022-01-17 09:39:06 -1000

    fix facbook_roads_h3.sql

--- tables/facebook_roads_h3.sql
+++ tables/facebook_roads_h3.sql
@@ -3,5 +3,5 @@ create table facebook_roads_h3 as
 		(select 8::int as resolution,
 				h3_geo_to_h3(ST_PointOnSurface(geom)::point, 8)as h3,
 				sum(ST_Length(geom::geography)) as fb_roads_length
-		from facebookroads_cleaned as a 
+		from facebook_roads as a
 		group by h3);
--- tables/facebookroads_cleaned.sql
+++ tables/facebookroads_cleaned.sql
@@ -1,13 +1,8 @@
-create index if not exists osm_roads_geog_idx on osm_roads using gist((geom::geography));
-drop table if exists facebookroads_cleaned;
-create table facebookroads_cleaned as
-select f.*
-from facebook_roads f
-left join lateral (
-        select st_buffer(ST_collect(o.geom)::geography, 10) geog --selects geometries into a collection based on buffer size 10
-        from osm_roads o
-        where ST_DWithin (f.geom::geography, o.geom::geography, 10)
-) o2 on true
-where o2.geog is null  -- select all facebook roads that are not within 10 meters with osm roads
-        -- select facebook roads with less than 50% intersection length with 10m buffer from osm roads
-        or st_length(ST_intersection(f.geom::geography, o2.geog)) < 0.5 * st_length(f.geom::geography) 
+create index if not exists osm_roads_geom_idx on osm_roads using gist(geom);
+
+-- Delete all facebook roads that have more than 50% intersection with 10m buffer from Open Street Map roads
+explain
+delete from facebook_roads a
+using osm_roads b
+where st_intersects(a.geom, b.geom) and
+      st_length(ST_intersection(a.geom, st_buffer(b.geom::geography, 10)::geometry)) > 0.5 * st_length(a.geom);

4aa2647cc53a8d9e5ca584763007b5145be9be69
Aleksandr Petrov <felispimeja@gmail.com>
2022-01-21 13:54:54 +0300

    fix to clean facebook roads

--- tables/facebook_roads.sql
+++ tables/facebook_roads.sql
@@ -1,7 +1,4 @@
-create index if not exists osm_roads_geom_idx on osm_roads using gist(geom);
-
 -- Delete all facebook roads that have more than 50% intersection with 10m buffer from Open Street Map roads
-explain
 delete from facebook_roads a
 using osm_roads b
 where st_intersects(a.geom, b.geom) and

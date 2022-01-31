d80b64e56d58cdbb79122df7c0d80797478032d2
ykyslomed <ykyslomed@kontur.io>
2022-01-31 11:57:03 +0200

    8793 - set for null value 10×distance_to_closest_road_cell

--- tables/isodist_hospitals_h3.sql
+++ tables/isodist_hospitals_h3.sql
@@ -6,14 +6,19 @@ create table isodist_hospitals_h3_distinct as (
     group by h3, geom
     order by h3
 );
-create index on isodist_hospitals_h3_distinct using gist(geom);
+create index on isodist_hospitals_h3_distinct using gist (geom);
 
 drop table if exists isodist_hospitals_h3;
 create table isodist_hospitals_h3 as (
     select h3, 8 resolution, round(min(distance) / 1000)::integer distance
     from (
+             select h3, distance
+             from isodist_hospitals_h3_distinct
+             union all
              select g.h3, d.distance
              from osm_object_count_grid_h3 g
+                  left outer join isodist_hospitals_h3_distinct i
+                     on (g.h3 = i.h3)
                   cross join lateral (
                  select s.h3, s.distance + ST_Distance(g.h3::geography, s.h3::geography) distance
                  from isodist_hospitals_h3_distinct s
@@ -21,10 +26,21 @@ create table isodist_hospitals_h3 as (
                  limit 1
                  ) d
              where g.highway_length > 0
-             and g.resolution = 8
+               and g.resolution = 8
+               and i.h3 is null
              union all
-             select h3, distance
-             from isodist_hospitals_h3_distinct
+             select p.h3, d.distance
+             from population_grid_h3 p
+                  left outer join osm_object_count_grid_h3 g
+                     on (g.highway_length > 0 and g.resolution = 8 and g.h3 = p.h3)
+                  cross join lateral (
+                 select s.h3, s.distance + ST_Distance(p.h3::geography, s.h3::geography) * 10 distance
+                 from isodist_hospitals_h3_distinct s
+                 order by p.h3::geometry <-> s.geom
+                 limit 1
+                 ) d
+             where p.resolution = 8
+               and g.h3 is null
          ) f
     group by h3
 );

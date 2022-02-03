781eb83eabc59a77215413fe3b71ccfb437d943a
Aliaksandr Kalenik <kalenik.aliaksandr@gmail.com>
2022-02-03 22:03:59 +0300

    add note in start.sh

--- basemap/start.sh
+++ basemap/start.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
 
+
+# NOTE: with current Makefile structure there multiple directory in which write
+# operations are performed and need to be persisted across executions.
+# I can't wire single persisted volume into multiple paths and I don't won't
+# to introduce persisted volume per directory. Also I don't want to change
+# Makefile to have single output directory so for now I decided to symlink
+# directories that need to be persisted to single volume.
 mkdir data
 ln -s data /persisted-volume/data
 mkdir db

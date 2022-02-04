369e24567fe09c64e73f8fa0a429b3e2781d0932
Aliaksandr Kalenik <kalenik.aliaksandr@gmail.com>
2022-02-04 10:56:14 +0300

    fix note

--- basemap/push_basemap_docker_image.sh
+++ basemap/push_basemap_docker_image.sh
@@ -1,10 +1,7 @@
 #!/bin/bash
 set -e
 echo "PLEASE NOTE: you need to docker login with your own credentials into Kontur's nexus to push docker image"
-# NOTE: tile_genrator is used by both: main and basemap pipelines but
-# I don't won't to include parent directory into docker build context
-# so I decided to symlink tile_generator into basemap directory for now
-ln -s ../tile_generator ./tile_generator
-docker build -t nexus.kontur.io:8085/konturdev/build-basemap . -f dockerfile-basemap
+cp -r ../tile_generator tile_generator
+docker build -t nexus.kontur.io:8085/konturdev/build-basemap .
 docker push nexus.kontur.io:8085/konturdev/build-basemap
 
--- basemap/start.sh
+++ basemap/start.sh
@@ -1,17 +1,16 @@
 #!/bin/bash
 
-
 # NOTE: with current Makefile structure there multiple output directories
 # need to be persisted across executions.
 # I can't wire single persisted volume into multiple paths and I don't won't
 # to introduce persisted volume per directory. Also I don't want to change
 # Makefile to have single output directory so for now I decided to symlink
 # directories that need to be persisted to single volume.
-mkdir data
-ln -s data /persisted-volume/data
-mkdir db
-ln -s db /persisted-volume/db
-mkdir deploy
-ln -s deploy /persisted-volume/deploy
+mkdir -p /persisted-volume/data
+ln -s /persisted-volume/data data
+mkdir -p /persisted-volume/db
+ln -s /persisted-volume/db db
+mkdir -p /persisted-volume/deploy
+ln -s /persisted-volume/deploy deploy
 
-make -j data/basemap.mbtiles
+make deploy/s3/test/basemap.mbtiles

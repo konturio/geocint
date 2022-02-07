8f5f03f705cc0f27a6ad79a507e40e60a2d84ac8
Alexander Kalenik <kalenik.aliaksandr@gmail.com>
2022-02-07 06:47:45 +0000

    Basemap in kubernetes

---
+++ basemap/start.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# NOTE: with current Makefile structure there multiple output directories
+# need to be persisted across executions.
+# I can't wire single persisted volume into multiple paths and I don't won't
+# to introduce persisted volume per directory. Also I don't want to change
+# Makefile to have single output directory so for now I decided to symlink
+# directories that need to be persisted to single volume.
+mkdir -p /persisted-volume/data
+ln -s /persisted-volume/data data
+mkdir -p /persisted-volume/db
+ln -s /persisted-volume/db db
+mkdir -p /persisted-volume/deploy
+ln -s /persisted-volume/deploy deploy
+
+make deploy/s3/test/basemap.mbtiles

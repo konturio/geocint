06276d348a3857226364bd89fc9c415b0b8aac5d
avalasiuk <avalasiuk@kontur.io>
2022-02-22 11:03:00 +0300

    Add kontur_population_v3_h3.sql instruction

---
+++ tables/kontur_population_v3_h3.sql
@@ -0,0 +1,7 @@
+drop table if exists kontur_population_v3_h3;
+create table kontur_population_v3_h3 as (
+    select h3_geo_to_h3(ST_PointOnSurface(geom), 8) as h3,
+           8::integer                               as resolution,
+           population
+    from kontur_population_v3
+);

6db581ea5224ffff7eee1b17aab846598a5868bf
avalasiuk <avalasiuk@kontur.io>
2022-03-02 15:59:15 +0300

    Add dependencies in Makefile and ingest osm population data in population_grid_h3_r8

--- tables/population_grid_h3_r8.sql
+++ tables/population_grid_h3_r8.sql
@@ -7,19 +7,29 @@ create table population_grid_h3_r8_in as (
            null::float               as population,
            coalesce(sum(ghs_pop), 0) as ghs_pop,
            sum(hrsl_pop)             as hrsl_pop,
-           sum(worldpop)             as worldpop
+           --sum(worldpop)             as worldpop,
+           sum(prescale_pop)         as prescale_pop
     from (
              select h3,
                     population  as ghs_pop,
                     null::float as hrsl_pop,
-                    null::float as worldpop
+                    --null::float as worldpop
+                    null::float as prescale_pop
              from ghs_globe_population_grid_h3_r8
              union all
              select h3,
                     null::float as ghs_pop,
                     population  as hrsl_pop,
-                    null::float as worldpop
+                    --null::float as worldpop
+                    null::float as prescale_pop
              from hrsl_population_grid_h3_r8
+             union all
+             select h3,
+                    null::float as ghs_pop,
+                    null::float  as hrsl_pop,
+                    --null::float as worldpop
+                    population as prescale_pop
+             from prescale_to_osm_h3_r8
 --              union all
 --              select h3,
 --                     null::float as ghs_pop,
@@ -40,13 +50,15 @@ create table population_grid_h3_r8 as (
            population,
            ghs_pop,
            case when b.geom is not null then coalesce(p.hrsl_pop, 0) end as hrsl_pop,
-           worldpop
+           --worldpop
+           prescale_pop
     from population_grid_h3_r8_in p
              left outer join
          hrsl_population_boundary b
          on ST_Intersects(b.geom, p.geom)
 );
 
+drop table if exists prescale_to_osm_h3_r8;
 drop table population_grid_h3_r8_in;
 
 -- update population_grid_h3_r8 p
@@ -58,7 +70,7 @@ drop table population_grid_h3_r8_in;
 update population_grid_h3_r8 p
 -- set population counts starting with more high resolution raster data (30m, 100m and whole planet)
 -- IMPORTANT: worldpop removed from coalesce
-set population = coalesce(hrsl_pop, ghs_pop);
+set population = coalesce(prescale_pop, coalesce(hrsl_pop, ghs_pop));
 
 vacuum full analyze population_grid_h3_r8;
 create index on population_grid_h3_r8 using gist (geom, population);

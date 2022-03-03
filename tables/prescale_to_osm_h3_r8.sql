f385a095899b30ace1a684b537f05557934b712b
avalasiuk <avalasiuk@kontur.io>
2022-03-03 17:48:29 +0300

    Rewrite this file without CTE and altering prescale_to_osm_boundaries

--- tables/prescale_to_osm_h3_r8.sql
+++ tables/prescale_to_osm_h3_r8.sql
@@ -1,31 +1,22 @@
--- Add column with population divided into hexs
-alter table if exists prescale_to_osm_boundaries
-add column if not exists divided_population int;
-
--- Update table and set population divided into hexs
-with prep as (
-    select distinct on (h3)
-                        osm_id            as id
-                        h3_polyfill(geom) as h3
-)
-with prep_mid as (
-    select osm_id    as osm_id,
-           count(*)  as hexs_number
-    group by osm_id
-)
-update prescale_to_osm_boundaries
-	set divided_population = population/hexs_number;
-
-
--- Get all hexs on 8 resolution, which cowered prescale_to_osm_boundaries
+-- Get all hexs on 8 resolution, that cover prescale_to_osm_boundaries 
+-- Calculate population part per hex as divide source poly populatition 
+-- by the number of hexs, that cover it
 drop table if exists prescale_to_osm_h3_r8;
 create table prescale_to_osm_h3_r8 as (
     select distinct on (h3)
-           8                   as resolution, 
-           osm_id              as osm_id,
-           h3_polyfill(geom)   as h3
-           divided_population  as population
-    from prescale_to_osm_boundaries
+            8                      as resolution,
+            osm_id                 as osm_id,
+            h3_polyfill(geom)      as h3,            
+            population/count(h3_p) as divided_population        
+    from (
+        select distinct on (h3)
+            osm_id            as id,
+            population        as population,
+            geom              as geom,
+            h3_polyfill(geom) as h3_p
+        from prescale_to_osm_boundaries
+          ) squ
+    group by osm_id, population, geom
 );
 
 drop table if exists prescale_to_osm_boundaries;

3abc1d02c2a7c1d318934ba66211d178186eff0e
avalasiuk <avalasiuk@kontur.io>
2022-03-03 16:16:44 +0300

    Change POLYGON to geometry, use osm_admin_boundaries instead of osm

--- tables/changed_population.sql
+++ tables/changed_population.sql
@@ -1,14 +1,14 @@
 -- Add actual osm population column
 alter table if exists prescale_to_osm
 add column if not exists actual_pop bigint,
-add column if not exists geom geometry(POLYGON, 4326);
+add column if not exists geom geometry(geometry, 4326);
 
 -- Update prescale_to osm with actual population data
 update prescale_to_osm
 	set actual_pop = o.tags ->> 'population',
-	set geom = ST_Transform(geog::geometry, 4326)
-	from osm o
-	where prescale_to_osm.osm_id = o.ism_id;
+	set geom = o.geom
+	from osm_admin_boundaries o
+	where prescale_to_osm.osm_id = o.osm_id;
 
 -- Create table for storing cases with population that is different with last osm dump
 -- Also for cases wiht null geometry
@@ -19,7 +19,7 @@ create table changed_population (osm_type text,
 	                             right_population bigint, 
 	                             change_date date, 
 	                             actual_pop bigint, 
-	                             geom geometry(POLYGON, 4326));
+	                             geom geometry(geometry, 4326));
 
 -- Move cases with outdated population and null-geometry objects to new table for recheck
 with changes as (

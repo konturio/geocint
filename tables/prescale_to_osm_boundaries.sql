d330c1afc3ff14bd883aad2ce4e619a8b40ecb46
avalasiuk <avalasiuk@kontur.io>
2022-03-03 22:34:56 +0300

    Rewrite prescale_to_osm_boundaries.sql

--- tables/prescale_to_osm_boundaries.sql
+++ tables/prescale_to_osm_boundaries.sql
@@ -25,52 +25,48 @@ create index on prescale_to_osm using gist(geom);
 
 -- Create table with boundaries of polygons from prescale to osm
 drop table if exists prescale_to_osm_boundaries;
-create table prescale_to_osm_boundaries as (
-    select geom,
-           osm_type,
-           osm_id,
-           (case
-                when (tags ->> 'population') ~ E'^[[:digit:]]+([.][[:digit:]]+)?$'
-                    then (tags ->> 'population')::float
-                else null
-               end)                           as population,
-           (case
-                when (tags ->> 'admin_level') ~ E'^[[:digit:]]+([.][[:digit:]]+)?$'
-                    then (tags ->> 'admin_level')::float
-                else null
-               end)                           as admin_level
-    from osm_admin_boundaries
-    where ST_Dimension(geog::geometry) = 2
-      and tags ? 'population'
-      and tags ->> 'admin_level' is not null
-      and osm_id in (select osm_id from prescale_to_osm)
-    order by 1
-);
 
--- Insert all boundaries, which are included in goal polygons
-insert into table prescale_to_osm_boundaries (geom, osm_type, osm_id, population, admin_level)
-    select o.geom,
-           o.osm_type,
-           o.osm_id,
-           (case
-                when (tags ->> 'population') ~ E'^[[:digit:]]+([.][[:digit:]]+)?$'
-                    then (tags ->> 'population')::float
-                else null
-               end)                           as population,
-           (case
-                when (tags ->> 'admin_level') ~ E'^[[:digit:]]+([.][[:digit:]]+)?$'
-                    then (tags ->> 'admin_level')::float
-                else null
-               end)                           as admin_level
-    from osm_admin_boundaries o 
-    left join prescale_to_osm_boundaries p
-    on ST_Intersects(o.geom, p.geom)
-    where ST_Dimension(o.geog::geometry) = 2
-      and tags ? 'population'
-      and tags ->> 'admin_level' is not null
-      and tags ->> 'admin_level' < p.admin_level
-    order by 1
-);
+-- Get boundaries with admin_level for objects from prescale_to_osm
+with prep as (select p.geom,
+                     p.osm_id, 
+                     p.admin_level,
+                     p.tags
+                  from osm_admin_boundaries as o
+                  join prescale_to_osm  as p
+                  on o.osm_id = p.osm_id)
+-- Create CTE which include all boundaries from prep and low-level boundaries that them include
+with prep_mid as (select o.geom,
+                         o.osm_id, 
+                         o.admin_level,
+                         o.tags
+                      from osm_admin_boundaries o 
+                      join prep p
+                      on ST_Intersects(p.geom, ST_PointOnSurface(o.geom))
+                      where o.admin_level > p.admin_level
+                  union all
+                  select * 
+                      from prep
+)
+
+create table prescale_to_osm_boundaries as
+    select  geom,
+            osm_type,
+            osm_id, 
+            (case
+                 when (tags ->> 'population') ~ E'^[[:digit:]]+([.][[:digit:]]+)?$'
+                     then (tags ->> 'population')::float
+                 else null
+                end)                           as population,
+            (case
+                 when (tags ->> 'admin_level') ~ E'^[[:digit:]]+([.][[:digit:]]+)?$'
+                     then (tags ->> 'admin_level')::float
+                 else null
+                end)                           as admin_level
+            from prep_mid
+            where ST_Dimension(geom) = 2
+                  and tags ? 'population'
+                  and tags ->> 'admin_level' is not null
+            order by 1;
 
 create index on prescale_to_osm_boundaries using gist (geom);
 create index on prescale_to_osm_boundaries using gist (ST_PointOnSurface(geom));

39fe8f77ab7f8eb3d6abd43e76c45b00a057322c
Aliaksandr Kalenik <kalenik.aliaksandr@gmail.com>
2022-04-26 06:01:54 +0300

    remove population column from gist index for stat_h3 in insights-api

--- scripts/population_api_dump_footer.sql
+++ scripts/population_api_dump_footer.sql
@@ -1,4 +1,4 @@
-  CREATE INDEX stat_h3_geom_zoom_population_idx ON public.stat_h3__new USING gist (geom, zoom, population);
+  CREATE INDEX stat_h3_geom_zoom_population_idx ON public.stat_h3__new USING gist (zoom, geom);
   ALTER TABLE IF EXISTS public.stat_h3 RENAME TO stat_h3__old;
   ALTER TABLE IF EXISTS public.stat_h3__new RENAME TO stat_h3;
   DROP TABLE public.stat_h3__old;

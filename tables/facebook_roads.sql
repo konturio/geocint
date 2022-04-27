c4c0b9bb5bd85266fa1e26051ff0194c5cffee6a
Aliaksandr Kalenik <kalenik.aliaksandr@gmail.com>
2022-04-27 17:57:01 +0300

    facebook_roads.sql: add spec url and note about the reason why this filter should exist

--- tables/facebook_roads.sql
+++ tables/facebook_roads.sql
@@ -1,3 +1,6 @@
+-- Create new table facebook_roads selecting only features that have less than 50% intersection with 10m buffer from Open Street Map roads
+-- Facebook roads spec: https://github.com/facebookmicrosites/Open-Mapping-At-Facebook/wiki/Available-Countries
+-- Note: The point of our filter is to remove the newly mapped roads that were mapped in OSM after the dataset was published by FB.
 drop table if exists facebook_roads;
 create table facebook_roads as
 select
@@ -5,4 +8,11 @@ select
        f.highway_tag         as highway,
        f.geom
 from facebook_roads_in f
-where ST_NPoints(geom) = 2; -- ai roads are segments. osm roads are lines.
+left join osm_roads o
+        on ST_Intersects(f.geom, o.geom)
+             and ST_Length(
+             	ST_Intersection(
+             		f.geom, ST_Buffer(o.geom::geography, 10, 'endcap=flat join=bevel')::geometry
+             	    -- `endcap=flat join=bevel` lessens points in buffer geom
+             	)) > 0.5 * ST_Length(f.geom)
+where o.osm_id is null;

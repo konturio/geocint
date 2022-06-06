6f406bbe7979e23a089c93a26b6f8dc1d6228fcf
avalasiuk <avalasiuk@kontur.io>
2022-06-06 14:54:42 +0300

    Change scaling process

--- tables/prescale_to_osm_h3_r8.sql
+++ tables/prescale_to_osm_h3_r8.sql
@@ -15,12 +15,13 @@ create table prescale_to_osm_h3_r8 as
 with sum_population as (
         select
                 b.osm_id,
-
-                 -- Calculate intersection area for each h3 cell and boundary polygon                 
-                round(sum(h.population * (case
+                -- Calculate kontur population for each boundary from prescale table
+                -- We need for '+1' in the end to make sure special case of 
+                --"population sum in all hexagons was 0 but has to be not 0" is handled.               
+                coalesce(round(sum(h.population * (case
                                                when ST_Within(h.geom, b.geom) then 1
                                                else ST_Area(ST_Intersection(h.geom, b.geom)) / ST_Area(h.geom)
-                                          end))) as population
+                                          end))), 0) + 1 as population
         from prescale_to_osm_boundaries_3857 b
         join kontur_population_h3 h
                 on ST_Intersects(h.geom, b.geom)
@@ -31,11 +32,7 @@ with sum_population as (
 select
         b.geom,
         b.osm_id,
-        (case
-             when p.population is not null and p.population > 0
-                 then (b.population::float / p.population::float)
-             else 1::float
-        end)                as coefficient,
+        b.population::float / p.population::float as coefficient,
         b.admin_level
 from prescale_to_osm_boundaries_3857 b
 left join sum_population p using(osm_id);

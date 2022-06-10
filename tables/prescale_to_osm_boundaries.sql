d12368f3e073b8bbd6eb5dc29b1646ca062a68b6
avalasiuk <avalasiuk@kontur.io>
2022-06-10 14:24:00 +0300

    move to new branch, to make rebase simply

---
+++ tables/prescale_to_osm_boundaries.sql
@@ -0,0 +1,72 @@
+-- Update prescale_to osm with actual population data
+update prescale_to_osm
+    set actual_osm_pop = cast(o.tags ->> 'population' as bigint),
+        geom = o.geom
+    from osm_admin_boundaries o
+    where prescale_to_osm.osm_id = o.osm_id;
+
+-- Create table for storing cases with population that is different with last osm dump
+-- Also for cases wiht null geometry
+-- Move cases with outdated population and null-geometry objects to new table for recheck
+drop table if exists changed_population;
+
+create table changed_population as 
+    with changes as (
+        delete 
+            from prescale_to_osm 
+            where right_population <> actual_osm_pop
+            or geom is null
+            returning osm_type, osm_id, name, right_population, change_date, actual_osm_pop, geom
+    )
+    select * from changes;
+
+create index on prescale_to_osm using gist(geom);
+
+-- Create table with boundaries of polygons from prescale to osm
+drop table if exists prescale_to_osm_boundaries;
+
+-- Get boundaries with admin_level for objects from prescale_to_osm
+create table prescale_to_osm_boundaries as
+with prep as (select p.geom,
+                     p.osm_id, 
+                     o.admin_level,
+                     o.tags
+                  from osm_admin_boundaries as o
+                  join prescale_to_osm  as p
+                  on o.osm_id = p.osm_id),
+-- Create CTE which include all boundaries from prep and low-level boundaries that them include
+prep_mid as (select o.geom,
+                    o.osm_id, 
+                    o.admin_level,
+                    o.tags
+                from osm_admin_boundaries o 
+                join prep p
+                on ST_Intersects(p.geom, ST_PointOnSurface(o.geom))
+                where o.admin_level > p.admin_level
+            union all
+            select * 
+                from prep)
+select  geom,
+        osm_id, 
+        (case
+             when (tags ->> 'population') ~ E'^[[:digit:]]+([.][[:digit:]]+)?$'
+                 then (tags ->> 'population')::float
+             else null
+        end)                               as population,
+        (case
+             when (tags ->> 'admin_level') ~ E'^[[:digit:]]+([.][[:digit:]]+)?$'
+                 then (tags ->> 'admin_level')::float
+             else null
+        end)                               as admin_level
+from prep_mid
+where ST_Dimension(geom) = 2
+    and tags ? 'population'
+    and tags ->> 'admin_level' is not null
+order by 1;
+
+create index on prescale_to_osm_boundaries using gist (geom, ST_PointOnSurface(geom));
+
+drop table if exists prescale_to_osm;
+
+alter table prescale_to_osm_boundaries add column isdeg boolean,
+                                       add column pop_ulevel real;
---
+++ tables/prescale_to_osm_coefficient_table.sql
@@ -0,0 +1,50 @@
+-- Transform prescale_to_osm_boundaries table to 3857
+drop table if exists prescale_to_osm_boundaries_3857;
+create table prescale_to_osm_boundaries_3857 as 
+        select ST_Transform(geom, 3857) as geom,
+               osm_id,
+               population
+        from prescale_to_osm_boundaries;
+
+drop table if exists prescale_to_osm_boundaries;
+
+-- Calculate Kontur population for each boundary
+-- Also calculate scaled coefficient for population
+drop table if exists prescale_to_osm_coefficient_table_in;
+create table prescale_to_osm_coefficient_table_in as
+with sum_population as (
+        select
+                b.osm_id,
+                -- Calculate kontur population for each boundary from prescale table
+                -- We need for '+1' in the end to make sure special case of 
+                --"population sum in all hexagons was 0 but has to be not 0" is handled.               
+                coalesce(round(sum(h.population * (case
+                                               when ST_Within(h.geom, b.geom) then 1
+                                               else ST_Area(ST_Intersection(h.geom, b.geom)) / ST_Area(h.geom)
+                                          end))), 0) + 1 as population
+        from prescale_to_osm_boundaries_3857 b
+        join kontur_population_h3 h
+                on ST_Intersects(h.geom, b.geom)
+                        and h.resolution = 8
+                        and h.population > 0
+        group by 1
+)
+select
+        b.geom,
+        b.osm_id,
+        b.population::float / p.population::float as coefficient
+from prescale_to_osm_boundaries_3857 b
+left join sum_population p using(osm_id);
+
+drop table if exists prescale_to_osm_boundaries_3857;
+
+-- Transform prescale_to_osm_coefficient_table_in table to 4326
+drop table if exists prescale_to_osm_coefficient_table;
+create table prescale_to_osm_coefficient_table as
+        select ST_Transform(geom, 4326) as geom,
+               osm_id,
+               coefficient
+        from prescale_to_osm_coefficient_table_in;
+
+drop table if exists prescale_to_osm_coefficient_table_in;
+create index on prescale_to_osm_coefficient_table using gist(geom);

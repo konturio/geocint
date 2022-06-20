29cd40c258ad64b64f14c7423b1d9d2c48580809
Pavel Pashagin <paolo.pash@gmail.com>
2022-06-20 23:24:06 +0300

    fire_stations man_distance overviews are weightened by population

--- tables/isodist_fire_stations_h3_overview.sql
+++ tables/isodist_fire_stations_h3_overview.sql
@@ -1,14 +1,11 @@
 insert into isodist_fire_stations_h3 (h3, resolution, distance, man_distance)
-select p.h3,
-       p.resolution,
-       a.max_distance,
-       (p.population * a.max_distance / 1000)
-from (select h3_to_parent(h3) as h3,
-             max(distance)    as max_distance
-      from isodist_fire_stations_h3
-      where resolution = :seq_res
-      group by 1) a
+select h3_to_parent(i.h3) as h3,
+       :seq_res - 1       as resolution,
+       max(i.distance)    as distance,
+       (sum(distance * p.population)) / sum(population) as man_distance
+from isodist_fire_stations_h3 i
          join kontur_population_h3 p
-              on a.h3 = p.h3
-                  and p.resolution = :seq_res - 1
-;
+              on i.h3 = p.h3
+where i.resolution = :seq_res
+  and p.resolution = :seq_res
+group by 1;

9d348316dbcc8e2825f6844996e5cb22687a6c48
Pavel Pashagin <paolo.pash@gmail.com>
2022-07-10 11:55:03 +0300

    overview method ver4

--- tables/isodist_fire_stations_h3_overview.sql
+++ tables/isodist_fire_stations_h3_overview.sql
@@ -1,13 +1,13 @@
 insert into isodist_fire_stations_h3 (h3, resolution, distance, man_distance)
-select h3_to_parent(i.h3) as h3,
-       :seq_res - 1       as resolution,
-       max(i.distance)    as distance,
+select h3_to_parent(i.h3)                   as h3,
+       :seq_res - 1                         as resolution,
+       max(i.distance)                      as distance,
        --(sum(distance * p.population)) / sum(population) as man_distance
-       (sum(i.man_distance * p.population)) / sum(p.population) as man_distance
+       -- (sum(i.man_distance * p.population)) / sum(p.population) as man_distance
+       (sum(i.man_distance * p.population)) as man_distance
 from isodist_fire_stations_h3 i
          join kontur_population_h3 p
               on i.h3 = p.h3
 where i.resolution = :seq_res
   and p.resolution = :seq_res
 group by 1;
-

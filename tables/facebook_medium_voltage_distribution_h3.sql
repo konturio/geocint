2fcb9b24fdd2a03c7ac534c2853b0817a7bf78de
Pavel Pashagin <paolo.pash@gmail.com>
2022-07-28 15:13:24 +0000

    11234-wrong-min-value-for-medium-voltage-powerlines-distribution-layer

--- tables/facebook_medium_voltage_distribution_h3.sql
+++ tables/facebook_medium_voltage_distribution_h3.sql
@@ -1,20 +1,17 @@
 drop table if exists facebook_medium_voltage_distribution_h3;
 
-create table facebook_medium_voltage_distribution_h3(
-    h3 h3index primary key,
-    resolution smallint generated always as (h3_get_resolution(h3)) stored,
-    powerlines smallint not null default 1
+create table facebook_medium_voltage_distribution_h3
+(
+    h3         h3index,
+    resolution int,
+    powerlines int not null default 1
 );
 
 -- notice that source data has resolution ~7.5 so we have gaps on 8 level
 -- on conflict do nothing without distict is 20% slower.
-insert into facebook_medium_voltage_distribution_h3 (h3) (
-    select distinct
-        h3_geo_to_h3(ST_Point(lon, lat, 4326), resolution)
-    from
-        facebook_medium_voltage_distribution_in, generate_series(0, 8) as resolution
+insert into facebook_medium_voltage_distribution_h3 (h3, resolution, powerlines) (
+    select h3_geo_to_h3(ST_Point(lon, lat, 4326), 8),
+                    8::int,
+                    value
+    from facebook_medium_voltage_distribution_in
 );
-
--- h3 as pk is quite useless in folowing operations but it might be
--- a good point to start increasing stat_h3 performance.
--- stat_h3 is the only place where our table is used at the moment.

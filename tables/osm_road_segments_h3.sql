562fc5a8746b6fd0f9e8cd382cd3caa38d0d8d0e
ee.kazakov <ee.kazakov@gmail.com>
2022-10-01 16:35:06 +0200

    cast to point for h3_lat_lng_to_cell

--- tables/osm_road_segments_h3.sql
+++ tables/osm_road_segments_h3.sql
@@ -2,7 +2,7 @@ drop table if exists osm_road_segments_h3;
 create table osm_road_segments_h3 as (
     select
         8::int as resolution,
-        h3_lat_lng_to_cell(ST_StartPoint(seg_geom), 8) as h3,
+        h3_lat_lng_to_cell(ST_StartPoint(seg_geom)::point, 8) as h3,
         sum(length) as highway_length
     from osm_road_segments
     group by h3);

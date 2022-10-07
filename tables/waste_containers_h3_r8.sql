9998abbbbf19df65a037d0c199a5ba47f919f0b6
avalasiuk <avalasiuk@kontur.io>
2022-10-07 09:44:07 +0300

    Fix table name

--- tables/waste_containers_h3_r8.sql
+++ tables/waste_containers_h3_r8.sql
@@ -1,8 +1,8 @@
-drop table if exists waste_containers_h3_r8_in;
+drop table if exists waste_containers_h3_r8;
 
 -- according to research 75m is an optimal distance betweem thash bins
 -- 75 meters provide 99% coverage
-create table waste_containers_h3_r8_in as (
+create table waste_containers_h3_r8 as (
     select osm_id,
            h3_geo_to_h3(ST_Centroid(geog::geometry), 8) as h3
     from osm o
@@ -11,12 +11,12 @@ create table waste_containers_h3_r8_in as (
        or tags @> '{"amenity":"recycling"}'
 );
 
-drop table if exists waste_containers_h3_r8;
-create table waste_containers_h3_r8 as (
+drop table if exists waste_containers_h3;
+create table waste_containers_h3 as (
     select h3                 as h3,
            count(osm_id)      as number_of_waste_containers
-    from waste_containers_h3_r8_in
+    from waste_containers_h3_r8
     group by 1
 );
 
-drop table if exists waste_containers_h3_r8_in;
+drop table if exists waste_containers_h3_r8;

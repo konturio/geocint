1b32503b84f65f9e757e529f2697bba6e8d012d1
avalasiuk <avalasiuk@kontur.io>
2022-10-12 14:11:40 +0300

    Add overlay description

--- tables/bivariate_overlays.sql
+++ tables/bivariate_overlays.sql
@@ -142,8 +142,10 @@ select 10,
        'population',
        'area_km2',
        false,
-       E'description',
+       E'This map shows the coverage of a community with garbage containers. Green zones are located a short distance from the nearest garbage container.
+       The red areas are populated and need to build more waste containers or add them to the OpenStreetMap database if they already exist. \n\nLast updated  ' ||
+       json_extract_path_text(meta::json, 'data', 'timestamp', 'last') || E' \n\n',
        '[{"id":"A1","color":"rgb(232,232,157)"},{"id":"A2","color":"rgb(239,163,127)"},{"id":"A3","color":"rgb(228,26,28)"},
          {"id":"B1","color":"rgb(186,226,153)"},{"id":"B2","color":"rgb(161,173,88)"},{"id":"B3","color":"rgb(191,108,63)"},
          {"id":"C1","color":"rgb(90,200,127)"},{"id":"C2","color":"rgb(112,186,128)"},{"id":"C3","color":"rgb(83,152,106)"}]'
-;
+from osm_meta;;
--- tables/waste_containers_h3.sql
+++ tables/waste_containers_h3.sql
@@ -1,28 +1,4 @@
 drop table if exists waste_containers_h3_r8;
-
--- -- according to research 75m is an optimal distance betweem thash bins
--- -- 75 meters provide 99% coverage
--- create table waste_containers_h3_r8 as (
---     select osm_id,
---            h3_geo_to_h3(ST_Centroid(geog::geometry), 8) as h3
---     from osm o
---     where tags @> '{"amenity":"waste_basket"}'
---        or tags @> '{"amenity":"waste_disposal"}'
---        or tags @> '{"amenity":"recycling"}'
--- );
-
--- drop table if exists waste_containers_h3;
--- create table waste_containers_h3 as (
---     select h3                 as h3,
---            count(osm_id)      as number_of_waste_containers,
---            8                  as resolution
---     from waste_containers_h3_r8
---     group by 1
--- );
-
--- drop table if exists waste_containers_h3_r8;
-
-
 create table waste_containers_h3_r8 as (
     select osm_id                                       as osm_id,
            ST_Centroid(geog::geometry)                  as geom,

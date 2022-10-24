a343f01f8131fa47efbd8b925edd6df3a46fcfd6
avalasiuk <avalasiuk@kontur.io>
2022-10-24 17:37:52 +0300

    Move dithering to separate procedure

---
+++ procedures/dither_area_to_not_bigger_than_100pc_of_hex_area.sql
@@ -0,0 +1,44 @@
+-- dither areas to not be bigger than 100% of hexagon's area for every resolution
+drop procedure if exists dither_area_to_not_bigger_than_100pc_of_hex_area;
+create or replace procedure dither_area_to_not_bigger_than_100pc_of_hex_area(input_table text,
+                                                                             table_h3 text,                                                                             
+                                                                             columns text[],
+                                                                             resolution integer default 8)
+language plpgsql
+as
+
+$$
+    declare
+        res         integer := resolution;
+        cur_row     jsonb;
+        carry       jsonb;
+        carry_out   jsonb;
+    begin
+
+        while res >= 0
+            loop
+                select jsonb_object_agg(column_name, 0) from unnest(columns) "column_name" into carry;
+                for cur_row in (select to_jsonb(r) from input_table r where resolution = res order by h3)
+                    loop
+                        -- recursive сalculation carry value for every type of area
+                        select jsonb_object_agg(c.key, carry_value - carry_out_value),
+                               jsonb_object_agg(c.key, carry_out_value)
+                        from jsonb_each(carry) c,
+                             jsonb_each(cur_row) r,
+                             lateral (select c.value::float + r.value::float "carry_value") "carry_value",
+                             least(carry_value::float, (cur_row -> 'area_km2')::float) "carry_out_value"
+                        where c.key = r.key
+                        into carry, carry_out;
+
+                        -- insert new value when difference between forest and hexagon area area is bigger then zero
+                        if jsonb_path_exists(carry_out, '$.** ? (@ > 0)') then
+                            insert into table_h3
+                            select *
+                            from jsonb_populate_record(null::table_h3, cur_row || carry_out);
+                        end if;
+                    end loop;
+                raise notice 'unprocessed carry %', carry;
+                res = res - 1;
+            end loop;
+    end;
+$$;

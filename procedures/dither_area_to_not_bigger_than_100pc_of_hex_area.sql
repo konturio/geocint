45a7ea4577cd1401ce98284e359a5eeb5752071a
avalasiuk <avalasiuk@kontur.io>
2022-10-24 23:16:25 +0300

    Use multiple execute

--- procedures/dither_area_to_not_bigger_than_100pc_of_hex_area.sql
+++ procedures/dither_area_to_not_bigger_than_100pc_of_hex_area.sql
@@ -21,25 +21,26 @@ $$
             loop
                 select jsonb_object_agg(column_name, 0) from unnest(columns) "column_name" into carry;
 
-                execute 'for cur_row in (select to_jsonb(r) from '|| input_table || ' r where resolution = ' || res::text || ' order by h3)
+                execute 'for cur_row in (select to_jsonb(r) from '|| input_table || ' r where resolution = ' || res::text || ' order by h3)'
                     loop
+                        
                         -- recursive сalculation carry value for every type of area
-                        select jsonb_object_agg(c.key, carry_value - carry_out_value),
+                       execute 'select jsonb_object_agg(c.key, carry_value - carry_out_value),
                                jsonb_object_agg(c.key, carry_out_value)
                         from jsonb_each(carry) c,
                              jsonb_each(cur_row) r,
                              lateral (select c.value::float + r.value::float "carry_value") "carry_value",
                              least(carry_value::float, (cur_row -> ''area_km2'')::float) "carry_out_value"
                         where c.key = r.key
-                        into carry, carry_out;
+                        into carry, carry_out;'
 
                         -- insert new value when difference between forest and hexagon area area is bigger then zero 
-                        if  jsonb_path_exists(carry_out, ' || E'''$.** ? (@ > 0)'') then
+                       execute 'if  jsonb_path_exists(carry_out, ' || E'''$.** ? (@ > 0)'') then
                             insert into '|| table_h3 || '
                             select *
                             from jsonb_populate_record(null::' || table_h3 || ', cur_row || carry_out);
-                        end if;
-                    end loop';
+                        end if';
+                    end loop;
 
                 raise notice 'unprocessed carry %', carry;
                 res = res - 1;

e9c7e54c58f5a8e16d391bb05e6bfff807480889
lgudyma <lgudyma@kontur.io>
2022-11-13 19:18:39 +0200

    13250-create-test-export-dataset-from-geocint-as-proof-of-concept

--- scripts/map_action_export.sh
+++ scripts/map_action_export.sh
@@ -2,7 +2,7 @@
 PSQL_SELECT='psql -q -t -U gis -c'
 OUTDIR=data/out/map_action/
 
-layers=$(${PSQL_SELECT} "SELECT distinct ma_theme FROM lgudyma.map_action LIMIT 2;")
+layers=$(${PSQL_SELECT} "SELECT distinct ma_theme FROM lgudyma.map_action;")
 
 for layer in ${layers}; do
     ogr2ogr -f "ESRI Shapefile" ${OUTDIR}map_action_${layer}.shp PG:"dbname=gis" -sql "SELECT * FROM lgudyma.map_action WHERE ma_theme = '${layer}'"

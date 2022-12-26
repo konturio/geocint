95361b67213ba08ac00103d6d7dda02ffdf19c83
ee.kazakov <ee.kazakov@gmail.com>
2022-12-26 13:20:19 +0100

    change auth endpoints

--- scripts/events_client/servers.py
+++ scripts/events_client/servers.py
@@ -10,14 +10,14 @@ class EventsServers:
 STAGES = {
     'dev': EventsServers(
         api='https://test-apps02.konturlabs.com/events/v1',
-        auth='https://keycloak01.konturlabs.com/auth/realms/dev/protocol/openid-connect/token',
+        auth='https://dev-keycloak.k8s-01.konturlabs.com/auth/realms/dev/protocol/openid-connect/token',
     ),
     'test': EventsServers(
         api='https://test-apps.konturlabs.com/events/v1',
-        auth='https://keycloak01.konturlabs.com/auth/realms/test/protocol/openid-connect/token',
+        auth='https://test-keycloak.k8s-01.konturlabs.com/auth/realms/test/protocol/openid-connect/token',
     ),
     'prod': EventsServers(
         api='https://apps.kontur.io/events/v1',
-        auth='https://keycloak01.kontur.io/auth/realms/kontur/protocol/openid-connect/token',
+        auth='prod-keycloak.k8s-01.konturlabs.com/auth/realms/kontur/protocol/openid-connect/token',
     ),
 }

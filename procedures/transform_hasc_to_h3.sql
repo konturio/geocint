a856e7e7633ee1fce717f9022dedc36bcbcfc39a
avalasiuk <avalasiuk@kontur.io>
2023-03-09 22:06:40 +0300

    Add logging for missed hasc codes

--- procedures/transform_hasc_to_h3.sql
+++ procedures/transform_hasc_to_h3.sql
@@ -9,6 +9,7 @@ as
 $$
 declare
     res         integer := resolution;
+    input_table text:= input_hasc_table;
     item_list   text;
     column_list text;
 begin
@@ -26,6 +27,19 @@ begin
                  from kontur_boundaries k join ' || input_hasc_table || ' s
                  on s.' || hack_field_name ||' = k.hasc_wiki
                  where k.hasc_wiki in (select ' || hack_field_name ||' from ' || input_hasc_table || '))';
+
+        -- create if not exist table to store hascs that were missed in Kontur Boundaries
+        execute 'create table if not exists kontur_boundaries_hasc_codes_check (missed_hasc char(2),
+                                                                                source_of_missed_hasc text,
+                                                                                found_at TIMESTAMPTZ DEFAULT NOW())';
+
+        -- get missed hasc codes and insert to special table kontur_boundaries_hasc_codes_check
+        execute 'insert into kontur_boundaries_hasc_codes_check (missed_hasc, source_of_missed_hasc, found_at)
+                    (select distinct s.' || hack_field_name ||' as missed_hasc, 
+                                     ''' || input_table || ''' as source_of_missed_hasc,
+                                     NOW()                      as found_at
+                    from ' || input_hasc_table || ' s, kontur_boundaries k
+                    where s.' || hack_field_name ||' not in (select hasc_wiki from kontur_boundaries where hasc_wiki is not null))';
 
         
         execute 'drop table if exists ' || ouput_h3_table;

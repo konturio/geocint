7f5d5c45c5b547e82b2cb2c698dca90edef180a5
avalasiuk <avalasiuk@kontur.io>
2023-03-22 16:24:16 +0300

    Add kontur_population overlay

--- tables/bivariate_overlays.sql
+++ tables/bivariate_overlays.sql
@@ -193,3 +193,21 @@ select 11,
          {"id":"C1","color":"rgb(232,232,157)"},{"id":"C2","color":"rgb(229,154,55)"},{"id":"C3","color":"rgb(228,26,28)"}]',
        true
 from osm_meta;
+
+insert into bivariate_overlays (ord, name, x_numerator, x_denominator, y_numerator, y_denominator, active, description, colors, is_public)
+select 12,
+       'Kontur Population',
+       'population',
+       'area_km2',
+       'population',
+       'area_km2',
+       false,
+       E'This map shows the population density. \n\nLast updated  ' ||
+       json_extract_path_text(meta::json, 'data', 'timestamp', 'last') || E' \n\n',
+       '[{"id":"A1","color":"rgb(232,232,157)"},
+         {"id":"A2","color":"rgb(186,226,153)"},{"id":"B1","color":"rgb(186,226,153)"},
+         {"id":"A3","color":"rgb(90,200,127)"},{"id":"B2","color":"rgb(90,200,127)"},{"id":"C1","color":"rgb(90,200,127)"},
+         {"id":"B3","color":"rgb(112,186,128)"},{"id":"C2","color":"rgb(112,186,128)"},
+         {"id":"C3","color":"rgb(83,152,10)"}]',
+       true
+from osm_meta;

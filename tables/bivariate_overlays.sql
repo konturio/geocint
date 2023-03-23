bef2c087d8fef34a5c8223590058fe90e1f0cffa
avalasiuk <avalasiuk@kontur.io>
2023-03-23 17:35:11 +0300

    Fix legend for population overlay

--- tables/bivariate_overlays.sql
+++ tables/bivariate_overlays.sql
@@ -204,10 +204,8 @@ select 12,
        false,
        E'This map shows the population density. \n\nLast updated  ' ||
        json_extract_path_text(meta::json, 'data', 'timestamp', 'last') || E' \n\n',
-       '[{"id":"A1","color":"rgb(232,232,157)"},
-         {"id":"A2","color":"rgb(186,226,153)"},{"id":"B1","color":"rgb(186,226,153)"},
-         {"id":"A3","color":"rgb(90,200,127)"},{"id":"B2","color":"rgb(90,200,127)"},{"id":"C1","color":"rgb(90,200,127)"},
-         {"id":"B3","color":"rgb(112,186,128)"},{"id":"C2","color":"rgb(112,186,128)"},
-         {"id":"C3","color":"rgb(83,152,10)"}]',
+       '[{"id":"A1","color":"rgb(186,226,153)"},{"id":"A2","color":"rgb(90,200,127)"},{"id":"A3","color":"rgb(83,152,10)"},
+         {"id":"B1","color":"rgb(90,200,127)"},{"id":"B2","color":"rgb(90,200,127)"},{"id":"B3","color":"rgb(83,152,10)"},
+         {"id":"C1","color":"rgb(83,152,10)"},{"id":"C2","color":"rgb(83,152,10)"},{"id":"C3","color":"rgb(83,152,10)"}]',
        true
 from osm_meta;

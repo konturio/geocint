3bdf25e17b918b5f0fb26523da6dcbf321fe53dc
avalasiuk <avalasiuk@kontur.io>
2023-04-17 14:07:01 +0300

    Fix error with order by area(subdivide(geom))

--- tables/hdx_boundaries.sql
+++ tables/hdx_boundaries.sql
@@ -1,17 +1,22 @@
 -- Match high level kontur_boundaries with HASC codes by wikidata identifier
 drop table if exists hdx_boundaries;
 
-create table hdx_boundaries as (
-    select distinct on (hdx.hasc)
-        hdx.hasc                          as hasc,
-        kbnd.hasc_wiki                    as hasc_wiki,
-        coalesce(kbnd.name_en, kbnd.name) as name,
-        ST_Subdivide(kbnd.geom)           as geom
-    from
-        kontur_boundaries as kbnd
-    inner join hdx_locations_with_wikicodes as hdx
+create table hdx_boundaries_i as (
+    with input as (
+        select distinct on (hdx.hasc)
+                hdx.hasc                          as hasc,
+                kbnd.hasc_wiki                    as hasc_wiki,
+                coalesce(kbnd.name_en, kbnd.name) as name,
+                kbnd.geom                         as geom
+        from kontur_boundaries as kbnd
+        inner join hdx_locations_with_wikicodes as hdx
         on hdx.wikicode = kbnd.tags ->> 'wikidata'
-    order by hdx.hasc, ST_Area(geom) desc
+        order by hdx.hasc, ST_Area(geom) desc)
+    select hasc                   as hasc,
+           hasc_wiki              as hasc_wiki,
+           name                   as name,
+           ST_Subdivide(geom)     as geom
+    from input
 );
 
 create index on hdx_boundaries using gist(geom);

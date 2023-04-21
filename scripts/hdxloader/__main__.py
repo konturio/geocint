896aac939604ced874fcd2bfa6d9d24e9877bad8
avalasiuk <avalasiuk@kontur.io>
2023-04-21 16:48:13 +0300

    Add comments and fix deprecated functions and tags

---
+++ scripts/hdxloader/__main__.py
@@ -0,0 +1,265 @@
+import argparse
+import logging
+import os
+import uuid
+
+from hdx.facades.keyword_arguments import facade
+from hdx.utilities.easy_logging import setup_logging
+from hdx.data.dataset import Dataset
+
+from hdxloader.dataset import DatasetType
+from hdxloader.loader import create_datasets_for_all_hdx_countries, Loader, SCRIPT_NAME
+
+
+USER_AGENT = 'Kontur HDX Loader'
+
+
+def parse_args() -> argparse.Namespace:
+    parser = argparse.ArgumentParser(
+        'HDX Loader',
+        formatter_class=argparse.ArgumentDefaultsHelpFormatter,
+    )
+    sub_parsers = parser.add_subparsers(
+        title="Modes",
+        description="Choose what do you want to do.",
+        dest="mode",
+        required=True,
+    )
+    parent_parser = argparse.ArgumentParser(add_help=False)
+
+    # Add special parser for "update" mode
+    parser_update = sub_parsers.add_parser(
+        "update",
+        help="update dataset",
+        parents=[parent_parser],
+    )
+    parser_update.add_argument(
+        '-i', '--identifier',
+        help='Specify a dataset identifier.',
+        required=True,
+        type=str,
+    )
+    parser_update.add_argument(
+        '-u', '--update_with_file',
+        help='Specify a dataset identifier.',
+        required=True,
+        type=str,
+    )
+    parser_update.add_argument(
+        '-f', '--file_with_update',
+        help='file with updated information',
+        required=False,
+        type=str,
+    )
+    parser_update.add_argument(
+        '-n', '--tag_name',
+        help='tag that should be updated',
+        required=False,
+        type=str,
+    )
+    parser_update.add_argument(
+        '--tag_value',
+        help='new tag value',
+        required=False,
+        type=str,
+    )
+
+    # Add special parser for "load" mode
+    parser_load = sub_parsers.add_parser(
+        "load",
+        help="load resources",
+        parents=[parent_parser],
+    )
+    parser_load.add_argument(
+        '-d', '--data-directory',
+        help='Specify a path with data to load.',
+        required=True,
+        type=str,
+    )
+
+    # Add special parser for "create" mode
+    parser_create = sub_parsers.add_parser(
+        "create",
+        help="create datasets",
+        parents=[parent_parser],
+    )
+    parser_create.add_argument(
+        '-o', '--owner',
+        help='Who is the owner of these datasets?',
+        required=True,
+        type=str,
+    )
+    modes_parsers = [parser_create, parser_load, parser_update]
+
+    # Add general arguments to all modes
+    for sub_parser in modes_parsers:
+        sub_parser.add_argument(
+            '-t', '--dataset-type',
+            choices=list(DatasetType),
+            help='Specify a type of dataset you want to work with.',
+            required=False,
+            type=DatasetType,
+        )
+        auth_group = sub_parser.add_argument_group(
+            title='Authorization',
+        )
+        auth_group.add_argument(
+            '-k', '--key',
+            default='',
+            type=str,
+            required=False,
+            help=f'HDX API key. '
+                 f'Visit https://data.humdata.org/user/{os.getlogin()}/api-tokens to get one. '
+                 f'Omit it to use key from $HDX_KEY environment variable '
+                 f'(env available: {get_hdx_key_from_env() is not None}).',
+        )
+        auth_group.add_argument(
+            '-s', '--hdx-site',
+            choices=['stage', 'prod'],
+            default='stage',
+            type=str,
+            required=True,
+            help='Set HDX site. Use `stage` for testing. '
+                 'Notice that HDX keys are different for different sites'
+        )
+        misc_group = sub_parser.add_argument_group(
+            title='Misc',
+        )
+        misc_group.add_argument(
+            '-v', '--verbose',
+            action='store_true',
+            required=False,
+            help='Run in verbose mode.',
+        )
+        misc_group.add_argument(
+            '--no-dry-run',
+            action='store_true',
+            required=False,
+            help='Do actual work.',
+        )
+
+    return parser.parse_args()
+
+# get hdx key from environmental variable if -k, --key options will be omit
+def get_hdx_key_from_env():
+    return os.environ.get('HDX_KEY', None)
+
+def load(
+        data_directory: str,
+        dataset_type: DatasetType,
+        no_dry_run: bool = False,
+        **_kwargs
+):
+    loader = Loader(dataset_type, data_directory)
+    for filename in os.listdir(data_directory):
+        datasource_path = os.path.join(data_directory, filename)
+        if not loader.datasource_class.is_file_matching_by_regexp(datasource_path):
+            continue
+        resource = loader.datasource_class(datasource_path)
+        loader.add_datasource(resource)
+
+    if no_dry_run:
+        loader.upload(skip_validation=False)
+
+
+# create set of empty datasets
+def create_datasets(
+        dataset_type: DatasetType,
+        owner: str,
+        no_dry_run: bool = False,
+        **_kwargs
+):
+    new_datasets = create_datasets_for_all_hdx_countries(
+        dataset_type,
+        owner,
+    )
+    unique_id = str(uuid.uuid4())
+    if no_dry_run:
+        for dataset in new_datasets:
+            dataset.create_in_hdx(
+                remove_additional_resources=False,
+                hxl_update=False,
+                updated_by_script=SCRIPT_NAME,
+                batch=unique_id,
+                allow_no_resources=True,
+            )
+    else:
+        for dataset in new_datasets:
+            logging.info(dataset)
+
+def update_dataset(
+        dataset_identifier: str,
+        update_with_file: str,
+        file_with_update: str = '',
+        updated_tag: str = '',
+        updated_tag_value: str = '',
+        no_dry_run: bool = False,
+        **_kwargs
+):
+    # if True - do actual work, else - do nothing
+    if no_dry_run:
+        # get dataset by identifier
+        dataset_for_update = Dataset.read_from_hdx(dataset_identifier)
+        # update local version of dataset by one of several options
+        if update_with_file in ('json','yml','yaml','no_file'):
+            if update_with_file == 'json':
+                dataset_for_update.update_from_json(file_with_update)
+            elif update_with_file == 'yml' or update_with_file == 'yaml':
+                dataset_for_update.update_from_yaml(file_with_update)
+            elif update_with_file == 'no_file':
+                dataset_for_update.update({updated_tag : updated_tag_value})
+            # update dataset metadata on hdx server
+            dataset_for_update.update_in_hdx()
+        else:
+            print('Wrong value of --update-with-file attribute')
+
+
+def main():
+    args = parse_args()
+    setup_logging(
+        console_log_level='DEBUG' if args.verbose or not args.no_dry_run else 'INFO',
+        log_file=None,
+    )
+
+    if args.mode == 'load':
+        facade(
+            load,
+            data_directory=os.path.abspath(args.data_directory),
+            dataset_type=args.dataset_type,
+            no_dry_run=args.no_dry_run,
+            hdx_site=args.hdx_site,
+            user_agent=USER_AGENT,
+            hdx_read_only=False,
+            hdx_key=args.key or get_hdx_key_from_env(),
+        )
+    elif args.mode == 'create':
+        facade(
+            create_datasets,
+            dataset_type=args.dataset_type,
+            owner=args.owner,
+            no_dry_run=args.no_dry_run,
+            hdx_site=args.hdx_site,
+            user_agent=USER_AGENT,
+            hdx_read_only=False,
+            hdx_key=args.key or get_hdx_key_from_env(),
+        )
+    elif args.mode == 'update':
+        facade(
+            update_dataset,
+            dataset_identifier=args.identifier,
+            update_with_file=args.update_with_file,
+            file_with_update=args.file_with_update,
+            updated_tag=args.tag_name,
+            updated_tag_value=args.tag_value,
+            no_dry_run=args.no_dry_run,
+            hdx_site=args.hdx_site,
+            user_agent=USER_AGENT,
+            hdx_read_only=False,
+            hdx_key=args.key or get_hdx_key_from_env(),
+        )
+    else:
+        raise RuntimeError
+
+
+if __name__ == '__main__':
+    main()
---
+++ scripts/hdxloader/datasource.py
@@ -0,0 +1,184 @@
+import abc
+import datetime
+import logging
+import os
+import re
+import subprocess as sp
+import tarfile
+import zipfile
+
+from typing import Optional, Tuple, Union
+
+from hdx.data.resource import Resource
+
+from hdxloader.dataset import DatasetType
+
+
+class Datasource(abc.ABC):
+    def __init__(self, dataset_path: str):
+        if tarfile.is_tarfile(dataset_path) or zipfile.is_zipfile(dataset_path):
+            raise ValueError(
+                f'Datasource {dataset_path} must be initialized for non-archived data.'
+            )
+        self._dataset_path = dataset_path
+
+    @property
+    def path(self) -> str:
+        return self._dataset_path
+
+    @abc.abstractmethod
+    def convert_to_resource_and_upload(self) -> Resource:
+        pass
+
+    @classmethod
+    @abc.abstractmethod
+    def is_file_matching_by_regexp(cls, file_path: str) -> bool:
+        pass
+
+# TO DO
+# Now this class use some hardcoded things, like:
+# 1. Hardcoded S3 bucket
+# 2. Hardcoded aws cli profile - geocint-pipeline-sender
+# 3. Hardcoded Amazon S3 as cloud storage
+# 4. Hardcoded format for uploaded files - gpkg
+# Looks like we should move these things to special configuration file or console line attributes
+class CountryDatasource(Datasource, abc.ABC):
+    FILENAMEREGEX = None
+    ALPHA2REGEX = re.compile(r'[A-Z]{2}')
+
+    def __init__(self, dataset_path: str):
+        super().__init__(dataset_path)
+
+        self._alpha2: Optional[str] = None
+        self._date: Optional[datetime.date] = None
+        self._extract_iso3_and_date_from_path()
+
+        self._url: Optional[str] = None
+
+    def __repr__(self):
+        return f'CountryDatasource("{self._dataset_path}")'
+
+    @classmethod
+    def is_file_matching_by_regexp(cls, file_path: str) -> bool:
+        return bool(cls.FILENAMEREGEX.match(file_path))
+
+    @property
+    def alpha2(self) -> str:
+        return self._alpha2
+
+    @alpha2.setter
+    def alpha2(self, value: str):
+        if not isinstance(value, str):
+            raise TypeError(f'Unknown type for Alpha2 code: {type(value)}')
+        if not self.ALPHA2REGEX.match(value):
+            raise ValueError(f'Wrong Alpha2 code format: {value}')
+        self._alpha2 = value
+
+    @property
+    def date(self) -> Optional[datetime.date]:
+        return self._date
+
+    @date.setter
+    def date(self, value: Union[str, datetime.date]):
+        if isinstance(value, str):
+            try:
+                date = datetime.datetime.strptime(value, '%Y%m%d').date()
+            except ValueError as e:
+                raise ValueError(f'{e}\nWrong format for date: {value}. Must match `YYYYMMDD`') \
+                    from e
+        else:
+            date = value
+
+        if not isinstance(date, datetime.date):
+            raise TypeError(f'Wrong type for date: {type(date)}')
+
+        self._date = date
+
+    def _extract_iso3_and_date_from_path(self) -> Tuple[str, str]:
+        filename = os.path.basename(self._dataset_path)
+        match = self.FILENAMEREGEX.match(filename)
+        assert match, f'Unable to parse ISO3 and date from file path: {filename}'
+
+        self.date = match.group('date')
+        self.alpha2 = match.group('alpha2')
+        return match.group('date'), match.group('alpha2')
+
+    def _generate_overview(self):
+        pass
+
+    def _pack_and_upload_to_s3(self):
+        def _parse_direct_link_from_s3_url(url_: str) -> str:
+            s3_regexp = re.compile(r'(s3://)?(?P<bucket>[^/]+)/(?P<key>.+)')
+            match = s3_regexp.match(url_)
+            assert match, f'Unparsable url: {url_}'
+            return f"https://{match.group('bucket')}.s3.amazonaws.com/{match.group('key')}"
+
+        self._url = None
+        archive_path = self._dataset_path + '.gz'
+
+        try:
+            pigz_cmd = [
+                'pigz',
+                '-k', '--best',
+                self._dataset_path,
+            ]
+            logging.debug(' '.join(pigz_cmd))
+            sp.check_call(pigz_cmd)
+
+            assert os.path.exists(archive_path), \
+                f'Could not find archived dataset: {archive_path}'
+
+            archive_name = os.path.basename(archive_path)
+
+            s3_archive_uri = \
+                f's3://geodata-eu-central-1-kontur-public/kontur_datasets/{archive_name}'
+            s3_cmd = [
+                'aws', 's3', 'cp',
+                archive_path,
+                s3_archive_uri,
+                '--profile', 'geocint_pipeline_sender',
+                '--acl', 'public-read',
+            ]
+            logging.debug(' '.join(s3_cmd))
+            sp.check_call(s3_cmd)
+            direct_link = _parse_direct_link_from_s3_url(s3_archive_uri)
+            self._url = direct_link
+
+        finally:
+            if os.path.exists(archive_path):
+                os.remove(archive_path)
+
+        return self._url
+
+    def convert_to_resource_and_upload(self) -> Resource:
+        url = self._pack_and_upload_to_s3()
+        resource = Resource({
+            'description': f'Release {self.date}',
+            'format': 'gpkg',
+            'name': os.path.basename(self._dataset_path),
+            'url': url,
+        })
+
+        return resource
+
+
+class CountryPopulationDensityFor400mH3Hexagons(CountryDatasource):
+    FILENAMEREGEX = re.compile(r'.*kontur_population_(?P<alpha2>[A-Z]{2})_(?P<date>\d{8})\.gpkg')
+
+    def __repr__(self):
+        return f'CountryPopulationDensityFor400mH3Hexagons("{self._dataset_path}")'
+
+
+class CountryAdministrativeDivisionWithAggregatedPopulation(CountryDatasource):
+    FILENAMEREGEX = re.compile(r'.*kontur_boundaries_(?P<alpha2>[A-Z]{2})_(?P<date>\d{8})\.gpkg')
+
+    def __repr__(self):
+        return f'CountryAdministrativeDivisionWithAggregatedPopulation("{self._dataset_path}")'
+
+
+DATASETTYPE_TO_DATASOURCE = {
+    DatasetType.CountryAdministrativeDivisionWithAggregatedPopulation:
+        CountryAdministrativeDivisionWithAggregatedPopulation,
+    DatasetType.CountryPopulationDensityFor400mH3Hexagons:
+        CountryPopulationDensityFor400mH3Hexagons,
+}

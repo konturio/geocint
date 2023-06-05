529da2dc3abc8179cf09c0945ab5768f698426a7
avalasiuk <avalasiuk@kontur.io>
2023-06-05 19:03:25 +0300

    Wrap vacuum with set max parallel worker wrappers to avoid error with shared memory limit and move it to separate file to run in the same context and be able to run vacuum otside of transaction

---
+++ basemap/scripts/insert_topology_boundary.sql
@@ -0,0 +1,17 @@
+insert into planet_osm_line (osm_id, 
+	                         boundary, 
+	                         admin_level, 
+	                         maritime, 
+	                         way, 
+	                         tags) 
+    select -osm_id as osm_id, 
+           'administrative' as boundary, 
+           admin_level, 
+           nullif(maritime, false), 
+           geom as way, 
+           hstore('name:'||default_language, name) as tags limit 5
+    from kontur_topology_boundary;
+
+set max_parallel_maintenance_workers = 0;
+vacuum planet_osm_line;
+set max_parallel_maintenance_workers = 2;

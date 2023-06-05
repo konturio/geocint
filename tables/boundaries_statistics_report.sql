e8873ea316b73dbdfc57c869941ef0ecb69594ec
avalasiuk <avalasiuk@kontur.io>
2023-06-05 18:46:38 +0300

    Fix small issues

--- tables/boundaries_statistics_report.sql
+++ tables/boundaries_statistics_report.sql
@@ -6,7 +6,7 @@ create table boundaries_statistics_report_in as (
 	       c.osm_type,
 	       c.location,
 	       c.population,
-	       string_agg('href_['||h.id::text||'](https://tasks.hotosm.org/projects/'||h.id::text||')',',') as projects,
+	       string_agg('href_['||h.id::text||'](https://tasks.hotosm.org/projects/'||h.id::text||')',', ') as projects,
 	       st_transform(c.geom, 3857)                                                               as geom
 	from kontur_boundaries_export c left join hot_projects h on st_intersects(c.geom, h.geom)
 	where c.osm_admin_level::integer <= 5
@@ -44,14 +44,15 @@ drop table if exists boundaries_geom_prepared;
 drop table if exists boundaries_statistics_report_mid;
 create table boundaries_statistics_report_mid as (
 	select s.osm_id,
-	       max(hazardous_days_count)                                                                                      as hazardous_days_count,
-	       sum(s.populated_area_km2)                                                                                      as populated_area_km2,
+	       max(hazardous_days_count)                                                                                      as hazardous_days_count,	       
+           sum(s.populated_area_km2)                                                                                      as populated_area_km2,
 	       sum(s.populated_area_km2*(1-sign(s.highway_length)))                                                           as roads_unmapped_km2,
 	       sum(s.population * (1 - sign(s.building_count)))                                                               as people_without_osm_buildings,
 	       sum(s.population * (1 - sign(s.highway_length)))                                                               as people_without_osm_roads,
 	       sum(s.population * (1 - sign(s.count)))                                                                        as people_without_osm_objects,
-	       sum(s.populated_area_km2 * (1 - sign(s.count)))                                                                as populated_area_with_no_osm_objects_km2,
-	       sum(s.populated_area_km2 * (1 - sign(s.building_count)))                                                       as populated_area_with_no_osm_buildings_km2
+	       coalesce((sum(s.populated_area_km2*(1-sign(s.building_count)))/ nullif(sum(s.populated_area_km2), 0))* 100, 0) as buildings_unmapped_percentage,
+           coalesce((sum(s.populated_area_km2*(1-sign(s.highway_length)))/ nullif(sum(s.populated_area_km2), 0))* 100, 0) as roads_unmapped_percentage,
+           coalesce((sum(s.populated_area_km2 * (1 - sign(s.count))) / nullif(sum(populated_area_km2), 0))* 100, 0)       as osm_gaps_percentage
 	from boundaries_statistics_prepared s
 	group by 1
 );
@@ -71,9 +72,9 @@ create table boundaries_statistics_report as (
             c.people_without_osm_roads                                            as "People with no OSM roads",
             c.people_without_osm_objects                                          as "People with no OSM objects",
             trunc(cast(c.populated_area_km2 as numeric), 3)                       as "Populated area km2",
-            trunc(cast(c.populated_area_with_no_osm_objects_km2 as numeric), 3)   as "Populated area with no OSM objects km2",
-            trunc(cast(c.populated_area_with_no_osm_buildings_km2 as numeric), 3) as "Populated area with no OSM buildings km2",
-            trunc(cast(c.roads_unmapped_km2 as numeric), 3)                       as "Populated area with no OSM roads km2",
+            trunc(cast(c.osm_gaps_percentage as numeric), 3)                      as "Populated area with no OSM objects %",
+            trunc(cast(c.buildings_unmapped_percentage as numeric), 3)            as "Populated area with no OSM buildings %",
+            trunc(cast(c.roads_unmapped_percentage as numeric), 3)                as "Populated area with no OSM roads %",
             c.hazardous_days_count                                                as "Hazardous days count",
             b.projects                                                            as "HOT Projects",
             b.location,

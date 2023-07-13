93d36b75e0275e8d41a72d411d9322fd0100dff7
avalasiuk <avalasiuk@kontur.io>
2023-07-13 13:19:44 +0300

    Use distance between centroids to find optimal zoomingbox

--- tables/hdx_boundaries_iso3_bbox.sql
+++ tables/hdx_boundaries_iso3_bbox.sql
@@ -7,18 +7,28 @@ drop table if exists hdx_boundaries_iso3_bbox;
 create table hdx_boundaries_iso3_bbox as
 with cnt_polygons as (select  code,
                               hasc_wiki,
-                                st_union(geom) as geom
-                        from hdx_boundaries,
-                             hdx_locations_with_wikicodes as hc
-                        where hasc_wiki = hc.hasc
-                        group by code, hasc_wiki)
+                              st_union(geom) as geom
+                      from hdx_boundaries,
+                           hdx_locations_with_wikicodes as hc
+                      where hasc_wiki = hc.hasc
+                      group by code, hasc_wiki)
 
-select code,
-       replace(replace(replace(replace((case when st_length(ST_LongestLine(st_envelope(geom),st_envelope(geom))) > 
-                                             st_length(ST_LongestLine(st_shiftlongitude(st_envelope(geom)),st_shiftlongitude(st_envelope(geom))))
-                                             and code != 'fra'
-                                then box2d(st_shiftlongitude(geom))
-                                else box2d(geom)
-                end)::text,'(','='),')',''), 'BOX', 'bbox'),' ',',') as bbox
+select  code,
+        replace(
+            replace(
+                replace(
+                    replace(
+                        (case 
+                            when ST_Distance(ST_Centroid(st_envelope(geom)),
+                                             ST_Centroid(geom::geography)::geometry) > 
+                                 ST_Distance(ST_Centroid(st_envelope(ST_ShiftLongitude(geom))), 
+                                             ST_Union(ST_Centroid(geom::geography)::geometry, 
+                                                      ST_ShiftLongitude(ST_Centroid(geom::geography)::geometry)))                                             
+                            then box2d(st_shiftlongitude(geom))
+                            else box2d(geom)
+                        end)::text,'(','='),
+                    ')',''),
+                'BOX', 'bbox'),
+            ' ',',') as bbox
 from cnt_polygons
 group by 1,geom;

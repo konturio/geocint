96f85d1aff4cdf5b1840a019f595c70081148ee0
avalasiuk <avalasiuk@kontur.io>
2023-10-10 19:51:50 +0300

    Remove duplicate description

--- tables/bivariate_overlays.sql
+++ tables/bivariate_overlays.sql
@@ -39,7 +39,7 @@ select 2,
        'population',
        'area_km2',
        false,
-       E'This map shows whether all buildings are mapped in OpenStreetMap. Building completeness is calculated as the ratio of OpenStreetMap building count to AI estimated total building count (OpenStreetMap + Microsofot + Copernicus + Geoalert + Kontur). \n\nLast updated  ' ||
+       E'This map shows whether all buildings are mapped in OpenStreetMap. Building completeness is calculated as the ratio of OpenStreetMap building count to AI estimated total building count. \n\nLast updated  ' ||
        json_extract_path_text(meta::json, 'data', 'timestamp', 'last') || E' \n\n',
        '[{"id":"A1","color":"rgb(232,232,157)"},{"id":"A2","color":"rgb(239,163,127)"},{"id":"A3","color":"rgb(228,26,28)"},
          {"id":"B1","color":"rgb(186,226,153)"},{"id":"B2","color":"rgb(161,173,88)"},{"id":"B3","color":"rgb(191,108,63)"},

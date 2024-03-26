b7473fdf2247c78101aee7e727b1484bad6f83a2
avalasiuk <avalasiuk@kontur.io>
2024-03-26 15:32:40 +0100

    clean existed indicators before run

--- scripts/clean.sql
+++ scripts/clean.sql
@@ -1,3 +1,7 @@
 drop table if exists z8_grid;
 drop table if exists z_grid;
 drop table if exists osm_quality_bivariate_tiles;
+
+drop table if exists insights_api_indicators_list_test;
+drop table if exists insights_api_indicators_list_prod;
+drop table if exists insights_api_indicators_list_dev;

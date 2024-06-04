750302387d66d99f5cb8feb60f3237c643c30006
avalasiuk <avalasiuk@kontur.io>
2024-06-04 15:09:12 +0200

    Shift multiple_citizenship values from 0-2 to 1-3 to avoid zero values displaying error

--- tables/bivariate_indicators.sql
+++ tables/bivariate_indicators.sql
@@ -479,9 +479,9 @@ values ('years_to_naturalisation', 'The Legal residence before naturalization', 
         '[["good"], ["bad"]]'::jsonb, 'The duration of legal residence before a national of a foreign state, without any cultural, historical, or marriage ties or connections to the state in question, can request citizenship under that states naturalization laws.', 'World', 'static', 'years', TRUE, '📊');
 
 insert into bivariate_indicators (param_id, param_label, copyrights, direction, description, coverage, update_frequency, unit_id, is_public, emoji)
-values ('multiple_citizenship', 'Possibility to have dual multiple citizenship', jsonb_build_array(
+values ('multiple_citizenship', 'Possibility to have multiple citizenship', jsonb_build_array(
         'This dataset use material from the Wikipedia article https://en.wikipedia.org/wiki/Naturalization, which is released under the https://en.wikipedia.org/wiki/Wikipedia:Text_of_the_Creative_Commons_Attribution-ShareAlike_4.0_International_License.'),
-        '[["bad"],["good"]]'::jsonb, 'Possibility to have dual (multiple) citizenship: 0 - no, 1 - with restrictions regulated by local legislation, 2 - dual (multiple) citizenship is allowed.', 'World', 'static', 'other', FALSE, '🛂');
+        '[["bad"],["good"]]'::jsonb, 'Possibility to have dual (multiple) citizenship: 1 - no, 2 - with restrictions regulated by local legislation, 3 - dual (multiple) citizenship is allowed.', 'World', 'static', 'other', FALSE, '🛂');
 
 -- set indicator is_base to become denominators
 update bivariate_indicators

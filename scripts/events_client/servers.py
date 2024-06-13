7631b004d1ad3633200939c4255c1a3b2c80f85c
Aliaksandr Kolesen <aliaksandr.kolesen@gmail.com>
2024-06-13 20:41:36 +0200

    test: swtich to k8s urls

--- scripts/events_client/servers.py
+++ scripts/events_client/servers.py
@@ -13,7 +13,7 @@ STAGES = {
         auth='https://dev-keycloak.k8s-01.konturlabs.com/realms/dev/protocol/openid-connect/token',
     ),
     'test': EventsServers(
-        api='https://test-apps.konturlabs.com/events/v1',
+        api='https://test-event-api.k8s-01.konturlabs.com/events/v1',
         auth='https://test-keycloak.k8s-01.konturlabs.com/realms/test/protocol/openid-connect/token',
     ),
     'prod': EventsServers(

5896bf530992f0b8642d706f76d63aeb91d6768e
Andrew <33183728+frolui@users.noreply.github.com>
2024-07-16 12:18:53 +0200

    18919-pharmacies-h3-layer (#146)
    * Add osm pharmacy count layer
    
    * Simplify code
    
    * Fix where condition for osm_pharmacy
    
    * Add oam data input targets to clean

---
+++ tables/osm_pharmacy.sql
@@ -0,0 +1,14 @@
+drop table if exists osm_pharmacy;
+create table osm_pharmacy as (
+    select  osm_type,
+            osm_id,
+            geog::geometry as geom,
+            tags ->> 'operator' as operator,
+            tags ->> 'opening_hours' as opening_hours,
+            tags
+    from osm o
+    where tags @> '{"amenity":"pharmacy"}' 
+          or tags @> '{"shop":"chemist"}'
+          or tags @> '{"healthcare":"pharmacy"}'
+    order by _ST_SortableHash(geog::geometry)
+);

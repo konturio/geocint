3d8f105a174cc05b786929da0a42c2ef6cfa408b
avalasiuk <avalasiuk@kontur.io>
2024-09-18 01:35:58 +0200

    Add separate calculation for two clusters

--- tables/poles_for_sensors_placement_v4.sql
+++ tables/poles_for_sensors_placement_v4.sql
@@ -30,7 +30,8 @@ create table proposed_centroids as (
 drop table if exists gat_cand_in;
 create table gat_cand_in as (
     select a.* 
-    from (select source as source, id as id, geom from gat_poles union all select 'centroid' as source, n + 10000 as id, st_transform(st_setsrid(geom,3857),4326) geom from prop_cent_filtered) a, gatlinburg g where st_within(a.geom,g.geom)
+    -- from (select source as source, id as id, geom from gat_poles union all select 'centroid' as source, n + 10000 as id, st_transform(st_setsrid(geom,3857),4326) geom from prop_cent_filtered) a, gatlinburg g where st_within(a.geom,g.geom)
+    from (select source as source, id as id, geom from gat_poles) a, gatlinburg g where st_within(a.geom,g.geom) -- only poles
 );
 
 -- calculate costs for each pole
@@ -44,7 +45,9 @@ create table gat_cand as (
            g.geom::geography as geog           
     from gat_cand_in g,
          gat_stat c
-    where ST_Intersects(st_transform(g.geom,3857),c.geom)
+    -- where st_dwithin(st_transform(g.geom,3857), c.geom, 1608) -- 1 mile zone
+    where st_dwithin(st_transform(g.geom,3857), c.geom, 4824) -- 2 mile zone
+    -- where st_dwithin(st_transform(g.geom,3857), c.geom, 3216) and not st_dwithin(st_transform(g.geom,3857), c.geom, 1608)
     group by 1,2,4,6
 );
 
@@ -68,6 +71,74 @@ begin
 
         update gat_cand_copy set dist_network = st_distance(gat_cand_copy.geog,k.geog) 
             from (select st_collect(geog::geometry)::geography as geog from proposed_points) k where st_dwithin(gat_cand_copy.geog,k.geog,3216);       
+
+        counter := counter + 1;
+    end loop;
+end;
+$$;
+
+
+
+
+
+
+
+
+
+
+drop table if exists two_clusters_4326;
+create table two_clusters_4326 as (select cost, cid, st_transform(geom, 4326) as geom from two_clusters);
+create index on two_clusters_4326 using gist(geom);
+
+drop table if exists gat_clusters_poles;
+create table gat_clusters_poles as (
+    select a.* , g,cid as cid
+    -- from (select source as source, id as id, geom from gat_poles union all select 'centroid' as source, n + 10000 as id, st_transform(st_setsrid(geom,3857),4326) geom from prop_cent_filtered) a, gatlinburg g where st_within(a.geom,g.geom)
+    from (select source as source, id as id, geom 
+          from gat_poles) a, two_clusters_4326 t, gatlinburg g 
+          where st_intersects(a.geom,t.geom) and st_within(a.geom,g.geom) -- only poles
+);
+
+-- calculate costs for each pole
+drop table if exists gat_clusters_cand;
+create table gat_clusters_cand as (
+    select source,
+           id, 
+           sum(c.cost) as cost,
+           null::numeric as dist_network,
+           row_number() over (partition by cid order by sum(cost) desc) rank,
+           cid,
+           g.geom::geography as geog
+    from gat_clusters_poles g,
+         gat_stat c
+    -- where st_dwithin(st_transform(g.geom,3857), c.geom, 1608) -- 1 mile zone
+    where st_dwithin(st_transform(g.geom,3857), c.geom, 4824) -- 2 mile zone
+    -- where st_dwithin(st_transform(g.geom,3857), c.geom, 3216) and not st_dwithin(st_transform(g.geom,3857), c.geom, 1608)
+    group by 1,2,4,6,7
+);
+
+update gat_clusters_cand set dist_network = st_distance(gat_clusters_cand.geog,k.geog) from (select geog, cid from gat_clusters_cand where rank = 1) k where st_dwithin(gat_clusters_cand.geog,k.geog,3216) and gat_clusters_cand.cid = k.cid ;
+
+drop table if exists gat_clusters_cand_copy;
+create table gat_clusters_cand_copy as (select * from gat_clusters_cand);
+
+drop table if exists clusters_proposed_points;
+create table clusters_proposed_points as (select * from gat_clusters_cand where rank = 1);
+
+do
+$$
+declare 
+    counter integer := 1;
+begin 
+    while counter < 13 loop --(select count(*) from gpranked where rank is null and cost > 0) loop
+        raise notice 'Counter %', counter; 
+
+        -- insert into clusters_proposed_points (select source, id, cost, dist_network, counter + 1, cid, geog from gat_clusters_cand_copy where dist_network > 1608 order by cost desc limit 1);
+
+        insert into clusters_proposed_points (select source, id, cost, dist_network, counter + 1, cid, geog from (select *, rank() OVER (PARTITION BY cid ORDER BY cost desc) as upd_rank from gat_clusters_cand where dist_network > 1608) a where upd_rank = 1);
+
+        update gat_clusters_cand_copy set dist_network = st_distance(gat_clusters_cand_copy.geog,k.geog) 
+            from (select st_collect(geog::geometry)::geography as geog, cid from clusters_proposed_points group by cid) k where st_dwithin(gat_clusters_cand_copy.geog,k.geog,3216) and gat_clusters_cand_copy.cid = k.cid;       
 
         counter := counter + 1;
     end loop;

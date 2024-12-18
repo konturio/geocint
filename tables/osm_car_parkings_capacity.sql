4d8495882c2a59dca41a689b4562e2bfa0ed440a
avalasiuk <avalasiuk@kontur.io>
2024-12-18 15:45:31 +0100

    Update calculation process to distribute capacity across hexagons proportionally to the area/length of the intersecting segment

--- tables/osm_car_parkings_capacity.sql
+++ tables/osm_car_parkings_capacity.sql
@@ -21,6 +21,7 @@ create table osm_car_parkings_capacity_in as (
     order by 1, 2, ST_Dimension(ST_Normalize(geog::geometry)) desc
 );
 
+-- use number of dimensions to define which type of regression should be applied
 drop table if exists osm_car_parkings_capacity_mid1;
 create table osm_car_parkings_capacity_mid1 as (
     select osm_id,
@@ -44,19 +45,18 @@ create table osm_car_parkings_capacity_mid1 as (
 
 -- calculate regression coefficients for area and length
 drop table if exists osm_car_parkings_capacity;
-with ar as (select regr_slope(capacity, area)       as slope,
-                   regr_intercept(capacity, area)   as intercept
-            from osm_car_parkings_capacity_mid1
-            where area is not null
-                  and capacity is not null),
-     lr as (select regr_slope(capacity, length)     as slope,
-                   regr_intercept(capacity, length) as intercept
-            from osm_car_parkings_capacity_mid1
-            where length is not null
-                  and capacity is not null)
--- calculate where it doesn't exists
-select osm_id       as osm_id,
-       osm_type     as osm_type,
+with area_regr as (select regr_slope(capacity, area)         as slope,
+                          regr_intercept(capacity, area)     as intercept
+                   from osm_car_parkings_capacity_mid1
+                   where area is not null
+                         and capacity is not null),
+     length_regr as (select regr_slope(capacity, length)     as slope,
+                            regr_intercept(capacity, length) as intercept
+                     from osm_car_parkings_capacity_mid1
+                     where length is not null
+                           and capacity is not null)
+select osm_id                                                as osm_id,
+       osm_type                                              as osm_type,
        ndimension,
        case
            when capacity is null and ndimension = 2
@@ -64,115 +64,21 @@ select osm_id       as osm_id,
            when capacity is null and ndimension = 1
                 then coalesce(length * lr.slope + lr.intercept, 0)
            else capacity
-       end          as capacity,
+       end                                                   as capacity,
        ST_Buffer(
                  case 
+                     -- we need it to prevent broken burref of closed linear parkings
                      when ndimension = 1 then ST_ConvexHull(ST_Normalize(geog::geometry))::geography
                      else geog
-                 end, 700 ) as buffered_geom,
-       ST_Normalize(geog::geometry) as geom
+                 end, 700 )                                  as buffered_geom,
+       ST_Normalize(geog::geometry)                          as geom
 into osm_car_parkings_capacity
-from ar,
-     lr,
+from area_regr   as ar,
+     length_regr as lr,
      osm_car_parkings_capacity_mid1;
-
--- drop table if exists osm_car_parkings_capacity_in;
--- drop table if exists osm_car_parkings_capacity_mid1;
-
-
-drop table if exists osm_car_parkings_capacity_h3_in;
-create table osm_car_parkings_capacity_h3_in as (
-    select h3,
-           h3_cell_to_boundary_geometry(h3) as geom
-    from (select h3_polygon_to_cells(buffered_geom, 8) as h3
-        from osm_car_parkings_capacity
-        where ndimension > 0 and capacity > 1) as sq
-);
-
-create index on osm_car_parkings_capacity_h3_in using gist(geom);
 
 create index on osm_car_parkings_capacity using gist(geom);
 
-
-select count(*) from (select osm_id, osm_type from  osm_car_parkings_capacity_h3_in a,
-                osm_car_parkings_capacity b
-          where ST_Intersects(a.geom, b.geom)
-                and b.ndimension > 0
-                and capacity > 1 group by osm_id, osm_type) a;
-
-select count(*) from (select osm_id, osm_type from  osm_car_parkings_capacity b
-          where  b.ndimension > 0
-                and capacity > 1 group by osm_id, osm_type) a;
-
-
-
-
-
-
-
-
-
-
-
-
-
-drop table if exists osm_car_parkings_capacity_h3;
-create table osm_car_parkings_capacity_h3 as (
-    select h3,
-           sum(osm_car_parkings_capacity) as osm_car_parkings_capacity,
-           resolution
-    from ((select a.h3,
-                 sum(case
-                         when ndimension = 2
-                             then round(capacity::float*ST_Area(ST_Intersection(a.geom, b.geom))/ST_Area(b.geom))
-                             else round(capacity::float*ST_Length(ST_Intersection(a.geom, b.geom))/ST_Length(b.geom))
-                     end)                                               as osm_car_parkings_capacity,
-                 8::integer                                             as resolution
-          from  osm_car_parkings_capacity_h3_in a,
-                osm_car_parkings_capacity b
-          where ST_Intersects(a.geom, b.geom)
-                and b.ndimension > 0
-                and capacity > 1
-          group by 1)
-          union all
-          (select h3_lat_lng_to_cell(ST_PointOnSurface(geom)::point, 8) as h3,
-                 sum(capacity)                                          as osm_car_parkings_capacity,
-                 8::integer                                             as resolution
-          from osm_car_parkings_capacity
-          where ndimension = 0
-                or capacity <= 1
-          group by 1)) sq
-    group by 1,3
-);
-
-
-
-
-drop table if exists osm_car_parkings_capacity_h32;
-create table osm_car_parkings_capacity_h32 as (
-    select h3,
-           sum(osm_car_parkings_capacity) as osm_car_parkings_capacity,
-           resolution
-    from ((select a.h3,
-                 sum(case
-                         when ndimension = 2
-                             then capacity::float*ST_Area(ST_Intersection(a.geom, b.geom))/ST_Area(b.geom)
-                             else capacity::float*ST_Length(ST_Intersection(a.geom, b.geom))/ST_Length(b.geom)
-                     end)                                               as osm_car_parkings_capacity,
-                 8::integer                                             as resolution
-          from  osm_car_parkings_capacity_h3_in a,
-                osm_car_parkings_capacity b
-          where ST_Intersects(a.geom, b.geom)
-                and b.ndimension > 0
-                and capacity > 1
-          group by 1)
-          union all
-          (select h3_lat_lng_to_cell(ST_PointOnSurface(geom)::point, 8) as h3,
-                 sum(capacity)                                          as osm_car_parkings_capacity,
-                 8::integer                                             as resolution
-          from osm_car_parkings_capacity
-          where ndimension = 0
-                or capacity <= 1
-          group by 1)) sq
-    group by 1,3
-);
+-- drop temporary tables
+drop table if exists osm_car_parkings_capacity_in;
+drop table if exists osm_car_parkings_capacity_mid1;

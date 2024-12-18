184e83784739ab97b9a9dd517fe112b5a4c1fc61
avalasiuk <avalasiuk@kontur.io>
2024-12-18 12:33:56 +0100

    Add missed comma

--- tables/osm_heritage_sites_h3.sql
+++ tables/osm_heritage_sites_h3.sql
@@ -2,7 +2,7 @@ drop table if exists osm_heritage_sites_h3;
 create table osm_heritage_sites_h3 as (
     select l.h3                                 as h3,
            count(k.*)::float                    as osm_heritage_sites_count,
-           min(k.heritage_admin_level)::float   as min_osm_heritage_admin_level
+           min(k.heritage_admin_level)::float   as min_osm_heritage_admin_level,
            8::integer                           as resolution
     from osm_heritage_sites k,
          land_polygons_h3_r8 l

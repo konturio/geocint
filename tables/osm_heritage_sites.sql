7f010df6b5f9d0ee598a9424c6983b3e80b20dec
avalasiuk <avalasiuk@kontur.io>
2024-12-19 20:12:04 +0100

    Remove case statement for non-integer values

--- tables/osm_heritage_sites.sql
+++ tables/osm_heritage_sites.sql
@@ -3,19 +3,10 @@ create table osm_heritage_sites as (
     select distinct on (osm_id, osm_type)
         osm_type,
         osm_id,
-        case
-            when tags ->> 'heritage' = 'UNESCO' then 1
-            when tags ->> 'heritage' = 'federal' then 2
-            when tags ->> 'heritage' = 'regional' then 4
-            when tags ->> 'heritage' ~ '^\d+(;\d+)+$' then (
-                select min(cast(value as integer))
-                from regexp_split_to_table(tags ->> 'heritage', ';') as value
-            )
-            else parse_integer(tags ->> 'heritage')
-        end                                                          as heritage_admin_level,
-        tags ->> 'heritage:operator'                                 as heritage_operator,
+        parse_integer(tags ->> 'heritage')              as heritage_admin_level,
+        tags ->> 'heritage:operator'                    as heritage_operator,
         tags,
-        ST_Transform(ST_Normalize(geog::geometry),3857)              as geom
+        ST_Transform(ST_Normalize(geog::geometry),3857) as geom
     from osm o
     where tags ? 'heritage'
           and tags ->> 'heritage' != 'no'

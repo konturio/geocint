68659d0e7cef3fdb02f0601e67938d70fcba86ef
avalasiuk <avalasiuk@kontur.io>
2025-02-03 09:20:03 +0100

    Add building parts to levels calculation

--- tables/osm_building_levels_h3.sql
+++ tables/osm_building_levels_h3.sql
@@ -1,21 +1,35 @@
-drop table if exists osm_building_levels_h3_in;
-create table osm_building_levels_h3_in as (
-    select h3_lat_lng_to_cell(ST_PointOnSurface(geom)::point, 8) as h3, -- here we use input geometry in EPSG:4326
-           coalesce(max(levels), ceil(max(height)/3), null) as max_levels,
-           coalesce(avg(levels), ceil(avg(height)/3), null) as avg_levels
-    from osm_buildings
-    where levels < 165 or (height < 840 and levels is null)
-    group by 1
+drop table if exists osm_buildings_parts_in;
+create table osm_buildings_parts_in as (
+    select osm_type,
+           osm_id,
+           geog::geometry                            as geom,
+           parse_integer(tags ->> 'building:levels') as levels,
+           parse_float(tags ->> 'height')            as height,
+           tags
+    from osm o
+    where tags ? 'building:part'
 );
 
 drop table if exists osm_building_levels_h3;
 create table osm_building_levels_h3 as (
-    select h3 as h3,
-           8  as resolution,
-           max_levels,
-           avg_levels
-    from osm_building_levels_h3_in
-    where max_levels is not null 
+    select h3,
+           coalesce(max(levels), ceil(max(height)/3), null) as max_levels,
+           coalesce(avg(levels), ceil(avg(height)/3), null) as avg_levels,
+           8 as resolution
+    from (select h3_lat_lng_to_cell(ST_PointOnSurface(geom)::point, 8) as h3,
+                 levels,
+                 height
+          from osm_buildings
+          where (levels < 165 and levels >= 0) or (height < 840 and levels is null and height >= 0)
+          union all
+          select h3_lat_lng_to_cell(ST_PointOnSurface(geom)::point, 8) as h3,
+                  levels,
+                  height
+          from osm_buildings_parts_in
+          where (levels < 165 and levels >= 0) or (height < 840 and levels is null and height >= 0))
+    group by 1
 );
 
-drop table if exists osm_building_levels_h3_in;
+drop table if exists osm_buildings_parts_in;
+
+call generate_overviews('osm_building_levels_h3', '{max_levels,avg_levels}'::text[], '{max,avg}'::text[], 8);

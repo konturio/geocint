efa2e85ee4f6805637ac5bfe823250ff8f740826
avalasiuk <avalasiuk@kontur.io>
2025-02-10 18:04:17 +0100

    Add main dependency calculation - population high res

---
+++ tables/population_grid_h3_r11.sql
@@ -0,0 +1,48 @@
+-- commented out WorldPop population dataset
+drop table if exists population_grid_h3_r11_in;
+create table population_grid_h3_r11_in as (
+    select h3,
+           h3::geometry              as geom,
+           11                        as resolution,
+           null::float               as population,
+           coalesce(sum(ghs_pop), 0) as ghs_pop,
+           sum(hrsl_pop)             as hrsl_pop
+    from (
+             select h3,
+                    population  as ghs_pop,
+                    null::float as hrsl_pop
+             from ghs_globe_population_grid_h3_r11
+             union all
+             select h3,
+                    null::float as ghs_pop,
+                    population  as hrsl_pop
+             from hrsl_population_grid_h3_r11
+         ) z
+    group by 1
+);
+
+create index on population_grid_h3_r11_in using gist (geom, hrsl_pop);
+
+drop table if exists population_grid_h3_r11;
+create table population_grid_h3_r11 as (
+    select h3,
+           p.geom,
+           resolution,
+           population,
+           ghs_pop,
+           case when b.geom is not null then coalesce(p.hrsl_pop, 0) end as hrsl_pop
+    from population_grid_h3_r11_in p
+             left outer join
+         hrsl_population_boundary b
+         on ST_Intersects(b.geom, p.geom)
+);
+
+drop table population_grid_h3_r11_in;
+
+update population_grid_h3_r11 p
+-- set population counts starting with more high resolution raster data (30m, 100m and whole planet)
+set population = coalesce(hrsl_pop, ghs_pop);
+
+vacuum full analyze population_grid_h3_r11;
+create index on population_grid_h3_r11 using gist (geom, population);
+create index on population_grid_h3_r11 using btree (h3);

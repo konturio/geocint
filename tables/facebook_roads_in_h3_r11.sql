cee54f4e64c86ea6924ab85c649237e2c1edd516
avalasiuk <avalasiuk@kontur.io>
2025-02-10 18:24:40 +0100

    Replace missed 8 with 11

--- tables/facebook_roads_in_h3_r11.sql
+++ tables/facebook_roads_in_h3_r11.sql
@@ -1,5 +1,5 @@
 drop table if exists facebook_roads_in_h3_r11;
-create table facebook_roads_in_h3_r8  as (
+create table facebook_roads_in_h3_r11  as (
     select 11::int                                              as resolution,
            h3_lat_lng_to_cell(ST_StartPoint(s.geom)::point, 11) as h3,
            sum(ST_Length(s.geom::geography))                    as fb_roads_in_length

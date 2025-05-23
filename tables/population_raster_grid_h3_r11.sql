8ea517e3200217081b8e23a9446e94e9d432b7a2
avalasiuk <avalasiuk@kontur.io>
2025-05-23 09:45:02 +0200

    Update population raster grid expression

--- tables/population_raster_grid_h3_r11.sql
+++ tables/population_raster_grid_h3_r11.sql
@@ -1,9 +1,8 @@
-
 drop table if exists :population_raster_grid_h3_r11;
 create table :population_raster_grid_h3_r11 as (
     select h3,
-           11        as resolution,
-           sum(sum)  as population
+           sum(value)  as population,
+           11          as resolution
     from (select (h3_raster_agg_to_h3(rast, 11)).* from :population_raster) z
     group by 1
 );

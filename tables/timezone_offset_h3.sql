e6ab14df20080fd760e0308977935080e7307f52
avalasiuk <avalasiuk@kontur.io>
2025-06-04 12:34:23 +0200

    Fix unit timezone not supported for type timestamp without time zone error

--- tables/timezone_offset_h3.sql
+++ tables/timezone_offset_h3.sql
@@ -1,22 +1,31 @@
 -- timezone offsets from OSM timezone tag
-
 drop table if exists timezone_points;
-create table timezone_points as (
-    select ST_PointOnSurface(geog::geometry) as geom,
-           tags->>'timezone' as tz
-    from osm
-    where tags ? 'timezone'
-);
+
+create table timezone_points as
+select
+    ST_PointOnSurface(geog::geometry) as geom,
+    tags ->> 'timezone'               as tz
+from osm
+where tags ? 'timezone';
 
 drop table if exists timezone_offset_h3;
-create table timezone_offset_h3 as (
-    select h3_lat_lng_to_cell(geom::point, 8) as h3,
-           avg(extract(timezone from now() at time zone tz)/3600.0) as utc_offset,
-           8 as resolution
-    from timezone_points
-    where tz is not null
-    group by 1
-);
+
+create table timezone_offset_h3 as
+select
+    h3_lat_lng_to_cell(geom::point, 8) as h3,
+    avg(
+        extract(timezone
+                from (now() at time zone 'UTC') at time zone
+                    case
+                        when tz = 'Europe/Pristina' then 'Europe/Belgrade'
+                        else tz
+                    end
+        ) / 3600.0
+    ) as utc_offset,
+    8 as resolution
+from timezone_points
+where tz is not null
+group by 1;
 
 create index on timezone_offset_h3(h3);
 

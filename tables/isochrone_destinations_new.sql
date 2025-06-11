c820fad3050b26f2b2f869ba1f4a2164d4bc41d6
avalasiuk <avalasiuk@kontur.io>
2025-06-11 18:26:53 +0200

    Update source table and function call

--- tables/isochrone_destinations_new.sql
+++ tables/isochrone_destinations_new.sql
@@ -1,18 +1,18 @@
 drop table if exists isochrone_destinations_new;
 create table isochrone_destinations_new as (
     select osm_id,
-           tags ->> 'amenity'             "type",
+           tags ->> 'amenity'                     as type,
            tags,
-           ST_Centroid(geog::geometry) as geom
+           ST_Centroid(geog::geometry)::geography as geog
     from osm o
     where tags @> '{"amenity":"fire_station"}'
        or tags @> '{"amenity":"hospital"}'
        or tags @> '{"amenity":"charging_station"}'
     union all
     select osm_id,
-           'bomb_shelter' as type,
+           'bomb_shelter'                         as type,
            tags,
-           ST_Centroid(geog::geometry) as geom
+           ST_Centroid(geog::geometry)::geography as geog
     from osm o
     where tags @> '{"building":"bunker"}'
        or tags @> '{"military":"bunker"}'

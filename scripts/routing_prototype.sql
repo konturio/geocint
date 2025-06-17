529a815b335b828a712400019274624e1baa6b7c
Nadia <hh.sleeping@gmail.com>
2025-06-17 21:56:33 +0400

    Apply suggestions from code review
    Co-authored-by: Darafei Praliaskouski <darafei@kontur.io>

--- scripts/routing_prototype.sql
+++ scripts/routing_prototype.sql
@@ -54,7 +54,7 @@ danger_zones(h3, danger_estimate) as (
     select population.h3, 1000
     from stat_h3_geom st
     join population on (population.original_h3 = st.h3), event
-    where st_intersects(st.geom, event.geom)
+    where ST_Intersects(st.geom, event.geom)
         and resolution = :population_resolution
 ),
 passable_roads (h3, passability) as (
@@ -91,7 +91,7 @@ select
     d1.h3 source_h3,
     h3index_to_bigint(d2.h3) as target,
     d2.h3 target_h3,
-    st_makeline(
+    ST_MakeLine(
         h3_cell_to_lat_lng(d1.h3)::geometry,
         h3_cell_to_lat_lng(d2.h3)::geometry) geom,
     case
@@ -153,7 +153,7 @@ select
     r.start_vid path_start,
     r.end_vid path_end,
     -- for equal (source, target) tuple there can be 2 distinct geometries depending on edge orientation in current path
-    ST_SetSRID(st_makeline(
+    ST_SetSRID(ST_MakeLine(
         h3_cell_to_lat_lng(case node when source then source_h3 else target_h3 end)::geometry,
         h3_cell_to_lat_lng(case node when source then target_h3 else source_h3 end)::geometry), 4326) geom,
     start_h3.population as population_in_danger,

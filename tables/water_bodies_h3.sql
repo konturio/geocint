3e39a2a2dbb45f90a5e6aceb08a4781da39017e6
avalasiuk <avalasiuk@kontur.io>
2025-06-23 18:22:18 +0200

    Calculate geometry for elevation layer

--- tables/water_bodies_h3.sql
+++ tables/water_bodies_h3.sql
@@ -28,8 +28,8 @@ create index on water_bodies_h3_in using gist(geom);
 
 drop table if exists water_bodies_h3;
 create table water_bodies_h3 as (
-    select i.h3                                                   as h3,
-           ST_Transform(h3_cell_to_boundary_geometry(i.h3), 3857) as geom
+    select i.h3                               as h3,
+           h3_cell_to_boundary_geometry(i.h3) as geom
     from water_bodies_h3_in i, 
          water_bodies_in p
     where ST_Intersects(i.geom, p.geom3857)
--- tables/water_level_idw_h3.sql
+++ tables/water_level_idw_h3.sql
@@ -2,6 +2,17 @@
 -- base data: water_bodies_h3 joined with gebco_h3
 -- result: average water elevation per cell in meters
 
+drop table if exists temp_gebco_with_geom;
+create table temp_gebco_with_geom as (
+	select h3, 
+	       avg_elevation_gebco,
+	       h3_cell_to_boundary_geometry(i.h3) as geom
+	from gebco_h3
+);
+
+create index on temp_gebco_with_geom using gist(geom);
+create index on temp_gebco_with_geom using btree(h3);
+
 -- table with water level known values
 drop table if exists water_bodies_elevation_h3;
 create table water_bodies_elevation_h3 as (
@@ -9,19 +20,18 @@ create table water_bodies_elevation_h3 as (
            w.geom,
            g.avg_elevation_gebco as water_elevation
     from water_bodies_h3 w
-         join gebco_h3 g using (h3)
+         join temp_gebco_with_geom g using (h3)
 );
 create index on water_bodies_elevation_h3 using gist(geom);
 
 -- interpolate for all gebco cells
-
 drop table if exists water_level_idw_h3;
 create table water_level_idw_h3 as
 with nearest as (
     select g.h3,
            n.water_elevation,
            st_distance(g.geom::geography, n.geom::geography) as dist
-    from gebco_h3 g
+    from temp_gebco_with_geom g
          cross join lateral (
              select water_elevation, geom
              from water_bodies_elevation_h3
@@ -38,3 +48,5 @@ group by h3;
 create index on water_level_idw_h3 (h3);
 
 drop table if exists water_bodies_elevation_h3;
+
+call generate_overviews('water_bodies_elevation_h3', '{water_elevation}'::text[], '{avg}'::text[], 8);
